43a0ecb171ea7f99edc5557bacbe12b1b5b298eb
Andre Wolden <wolden.andre@gmail.com>
2019-05-29 12:23:37 +0200

    fjerner litt react boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="ella_blunk.svg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          sosialhjelp-innsyn
         </p>
         <a
           className="App-link"
