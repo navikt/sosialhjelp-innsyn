07f6f8c872126d008fc04e7a1144e07bed5be463
Flemming, Thomas <thomas.flemming@gmail.com>
2019-06-17 18:06:18 +0200

    Hode, fot og noen nav-frontend-moduler

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
+import Lenkepanel from 'nav-frontend-lenkepanel';
+import Lesmerpanel from 'nav-frontend-lesmerpanel';
 import './App.less';
+import AppBanner from "./components/appBanner/AppBanner";
+import BrodsmuleSti from "./components/brodsmuleSti/BrodsmuleSti";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-        <img src="ella_blunk.svg" className="App-logo" alt="logo" />
-        <p>
-          sosialhjelp-innsyn
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </div>
-  );
-}
+	return (
+		<div className="informasjon-side">
+			<AppBanner/>
+
+			<div className="blokk-center">
+				<BrodsmuleSti/>
+				<Lenkepanel href="#todo" tittelProps="normaltekst" border>
+					Lenkepanel test
+				</Lenkepanel>
+
+				<Lesmerpanel intro={<span>Les mer panel med introduksjonstekst her som <a href="todo#">kan være HTML</a></span>} border>
+					<div>
+						<p style={{"marginTop": 0}}>
+							Noe tekst som økonomisk sosialhjelp. Du bestemmer selv om du vil bruke sykmeldingen eller avbryte den. Du kan også jobbe i kombinasjon med sykmelding. Det kommer an på hva sykdommen din tillater og hva det er praktisk mulig å få til på arbeidsplassen.
+						</p>
+						<p>
+							Greit å vite: Arbeidsgiveren har plikt til å legge til rette for at du kan jobbe helt eller delvis selv om du er syk.
+						</p>
+					</div>
+				</Lesmerpanel>
+
+				<p style={{textAlign: "center"}}>
+					<img src="ella_blunk.svg" className="App-logo" alt="logo"/>
+				</p>
+
+			</div>
+		</div>
+	);
+};
 
 export default App;
