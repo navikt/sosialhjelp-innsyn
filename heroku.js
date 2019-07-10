b7e780efbf9b804992a9aa860175bd96fa82e628
siljee <silje.hansen3@nav.no>
2019-07-10 09:06:19 +0200

    Oppdaterer alle pather fra soknadsosialhjelp til sosialhjelp

--- heroku.js
+++ heroku.js
@@ -4,20 +4,20 @@ var path = require('path')
 const port = process.env.PORT
 
 app.get("*/static/*", (req, res) => {
-  //let path = req.params[0].replace("/soknadsosialhjelp/", "");
-  console.log(`static ${req.path}`)
+  //let path = req.params[0].replace("/sosialhjelp/", "");
+  console.log(`request path: ${req.path}. serving: ${path.resolve(__dirname, "build/static", req.params[1])}`)
   //console.dir(req)
-  res.sendFile(path.resolve(__dirname, "static", req.params[1]))
+  res.sendFile(path.resolve(__dirname, "build/static", req.params[1]))
 })
 
-// app.get("/soknadsosialhjelp/statisk/*", (req, res) => {
+// app.get("/sosialhjelp/statisk/*", (req, res) => {
 //   console.log(`statisk ${req.path}`)
-//   res.sendFile(path.resolve(__dirname, "soknadsosialhjelp/statisk", req.params[0]))
+//   res.sendFile(path.resolve(__dirname, "sosialhjelp/statisk", req.params[0]))
 // })
 
 app.get("*", (req, res) => {
   console.log(`route  ${req.path}`)
-  res.sendFile(path.resolve(__dirname, "index.html"))
+  res.sendFile(path.resolve(__dirname, "build/index.html"))
 })
 
 app.listen(port, (req, res) => {
