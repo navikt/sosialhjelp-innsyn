3ca56b8af178cb473c66645147d75f793d34f067
Andre Wolden <wolden.andre@gmail.com>
2019-07-11 10:28:06 +0200

    endrer ApiBaseUrl for kjøring lokalt med TestApplication running på backend

--- src/utils/restUtils.ts
+++ src/utils/restUtils.ts
@@ -7,7 +7,7 @@ export function erDev(): boolean {
 
 export function getApiBaseUrl(): string {
     if (erDev()) {
-        return "http://localhost:8080/sosialhjelp/innsyn-api/api/v1/innsyn"; // /1234/saksStatus
+        return "http://localhost:8080/soknadsosialhjelp/innsyn-api/api/v1/innsyn"; // /1234/saksStatus
     } else {
         return "/sosialhjelp/innsyn-api/api/v1/innsyn";
     }
