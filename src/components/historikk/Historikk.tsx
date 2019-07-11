90c16601f9968508987930b6699ad51d864e9a95
Thomas Flemming <thomas.flemming@gmail.com>
2019-07-11 13:23:20 +0200

    sorter historikk kronologisk

--- src/components/historikk/Historikk.tsx
+++ src/components/historikk/Historikk.tsx
@@ -1,46 +1,52 @@
 import React from 'react';
-import { Panel } from "nav-frontend-paneler";
+import {Panel} from "nav-frontend-paneler";
 import {Normaltekst, Systemtittel, Element} from "nav-frontend-typografi";
 import "./historikk.less";
 import {Hendelse} from "../../redux/innsynsdata/innsynsdataReducer";
 import EksternLenke from "../eksternLenke/EksternLenke";
-import {FormattedDate, FormattedTime, FormattedMessage} from "react-intl";
+import {FormattedMessage} from "react-intl";
+import DatoOgKlokkeslett from "../tidspunkt/DatoOgKlokkeslett";
 
 interface Props {
-	hendelser: null|Hendelse[];
+    hendelser: null | Hendelse[];
+}
+
+function sorterHendelserKronologisk(hendelser: Hendelse[]): Hendelse[] {
+    return hendelser.sort((a: Hendelse, b: Hendelse) => {
+        let c = new Date(a.tidspunkt);
+        let d = new Date(b.tidspunkt);
+        return c > d ? -1 : c < d ? 1 : 0;
+    });
 }
 
 const Historikk: React.FC<Props> = ({hendelser}) => {
-	if (hendelser === null) {
-		return null;
-	}
-	return (<>
-			<Panel className="panel-luft-over">
-				<Systemtittel><FormattedMessage id="historikk.tittel" /></Systemtittel>
-			</Panel>
-			<Panel className="panel-glippe-over">
-				<ul className="historikk">
-					{hendelser.map((hendelse: Hendelse, index) => {
-						// const tidspunkt = new Date(hendelse.tidspunkt).toLocaleDateString();
-						return (
-							<li key={index}>
-								<Element>
-									<FormattedDate value={new Date(hendelse.tidspunkt)}/>
-									&nbsp;klokken&nbsp;
-									<FormattedTime value={new Date(hendelse.tidspunkt)}/>
-								</Element>
-
-								<Normaltekst>{hendelse.beskrivelse}</Normaltekst>
-								{hendelse.filUrl && (
-									<EksternLenke href={"url_todo_" + hendelse.filUrl}>Se vedtaksbrev</EksternLenke>
-								)}
-							</li>
-						);
-					})}
-				</ul>
-			</Panel>
-		</>
-	);
+    if (hendelser === null) {
+        return null;
+    }
+    const sorterteHendelser = sorterHendelserKronologisk(hendelser);
+    return (<>
+            <Panel className="panel-luft-over">
+                <Systemtittel><FormattedMessage id="historikk.tittel"/></Systemtittel>
+            </Panel>
+            <Panel className="panel-glippe-over">
+                <ul className="historikk">
+                    {sorterteHendelser.map((hendelse: Hendelse, index: number) => {
+                        return (
+                            <li key={index}>
+                                <Element>
+                                    <DatoOgKlokkeslett tidspunkt={hendelse.tidspunkt}/>
+                                </Element>
+                                <Normaltekst>{hendelse.beskrivelse}</Normaltekst>
+                                {hendelse.filUrl && (
+                                    <EksternLenke href={"url_todo_" + hendelse.filUrl}>Se vedtaksbrev</EksternLenke>
+                                )}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </Panel>
+        </>
+    );
 };
 
 export default Historikk;
