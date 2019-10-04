060a62c3a2d5d2c28e24ab161f6a07cec0f0f9f7
Thomas Flemming <thomas.flemming@gmail.com>
2019-10-04 14:44:23 +0200

    Justere CSS på ArkfanePanel og Historikk

--- src/components/oppgaver/VedleggModal.tsx
+++ src/components/oppgaver/VedleggModal.tsx
@@ -1,28 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import BildeModal from "../bildeModal/BildeModal";
-// eslint-disable-next-line
-import { pdfjs, Document, Page } from "react-pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+/*
+ * For å kommentere inn pdf støtte igjen:
+ *   npm install --save react-pdf
+ *   npm install --save-dev @types/react-pdf
+ */
+
+// eslint-disable-next-line
+// import { pdfjs, Document, Page } from "react-pdf";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const VedleggModal: React.FC<{ file: File, synlig: boolean, onRequestClose: () => void }> = ({file, synlig, onRequestClose}) => {
     const fileExtension = file.name.replace(/^.*\./, '');
     const isImage = fileExtension.match(/jpe?g|png/i) !== null;
-    const isPDF = fileExtension.match(/pdf/i) !== null;
-    const [numPages, setNumPages] = useState(1);
-    const pageNumber = 1;
-    // const [pageNumber, setPageNumber] = useState(1);
 
+    // const isPDF = fileExtension.match(/pdf/i) !== null;
+    // const [numPages, setNumPages] = useState(1);
+    // const pageNumber = 1;
+    // const [pageNumber, setPageNumber] = useState(1);
     // const onLoadPDF = (pdf: any) => {
     //     setNumPages(pdf.numPages);
     // };
-
-    useEffect(() => {
-        return () => {
-            console.log('will unmount');
-            setNumPages(0);
-        }
-    }, []);
+    // useEffect(() => {
+    //     return () => {
+    //         setNumPages(0);
+    //     }
+    // }, []);
 
     return (
         <BildeModal
@@ -43,19 +48,19 @@ const VedleggModal: React.FC<{ file: File, synlig: boolean, onRequestClose: () =
                         alt={file.name}
                     />
                 )}
-                {isPDF && (
-                    <div style={{overflow: "scroll"}}>
-                        <Document
-                            className="vedlegg_pdf_document"
-                            options={{zoom: "page-fit"}}
-                            file={ URL.createObjectURL(file)}
-                            onLoadSuccess={(pdf: any ) => setNumPages(pdf.numPages)}
-                        >
-                            <Page pageNumber={pageNumber} />
-                        </Document>
-                        <p>Side {pageNumber} av {numPages}</p>
-                    </div>
-                )}
+                {/*{isPDF && (*/}
+                {/*    <div style={{overflow: "scroll"}}>*/}
+                {/*        <Document*/}
+                {/*            className="vedlegg_pdf_document"*/}
+                {/*            options={{zoom: "page-fit"}}*/}
+                {/*            file={ URL.createObjectURL(file)}*/}
+                {/*            onLoadSuccess={(pdf: any ) => setNumPages(pdf.numPages)}*/}
+                {/*        >*/}
+                {/*            <Page pageNumber={pageNumber} />*/}
+                {/*        </Document>*/}
+                {/*        <p>Side {pageNumber} av {numPages}</p>*/}
+                {/*    </div>*/}
+                {/*)}*/}
             </div>
         </BildeModal>
 
