475870a3995b00520e0c67a5eb22fc7e86c65a46
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2019-10-21 13:54:07 +0200

    Fikser heroku bygging 4.

--- set-resource-path.sh
+++ set-resource-path.sh
@@ -2,5 +2,5 @@
 
 # Run `heroku labs:enable runtime-dyno-metadata -a $APP_NAME` to make $HEROKU_APP_NAME available
 if [[ -n "$HEROKU_APP_NAME" && "$HEROKU_APP_NAME" != "sosialhjelp-innsyn" ]]; then
-    sed -i 's!sosialhjelp/innsyn!'$HEROKU_APP_NAME'/sosialhjelp/innsyn!g' index.html
+    sed -i 's!sosialhjelp/innsyn!'$HEROKU_APP_NAME'/sosialhjelp/innsyn!g' build/index.html
 fi
