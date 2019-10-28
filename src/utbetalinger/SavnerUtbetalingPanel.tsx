0037c60b269cab7bb301764dc4fbd532f5bf2391
Thomas Flemming <thomas.flemming@gmail.com>
2019-10-28 13:14:22 +0100

    Boks med informasjon om det er noe utbetalinger bruker savner

---
+++ src/utbetalinger/SavnerUtbetalingPanel.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {Element, Normaltekst} from "nav-frontend-typografi";
+import InfoIkon from "../components/ikoner/InfoIkon";
+
+const SavnerUtbetalingPanel: React.FC = () => {
+
+    return (
+        <div className="savner_utbetaling_panel">
+            <span style={{
+                display: "block",
+                float: "left",
+                marginLeft: "-2.5rem"
+            }}>
+                <InfoIkon/>
+            </span>
+            <Element>Er det en utbetaling du savner?</Element>
+            <br/>
+            <Normaltekst>
+                Utbetalingsoversikten er under utvikling, vi kan for øyeblikket derfor bare vise:
+            </Normaltekst>
+            <ul>
+                <li>
+                    <Normaltekst>
+                        utbetalinger fra digitale søknader, men ikke fra papirsøknader
+                    </Normaltekst>
+                </li>
+                <li>
+                    <Normaltekst>
+                        utbetalinger som er opp til ett år gamle, eldre enn dette blir slettet
+                    </Normaltekst>
+                </li>
+                <li>
+                    <Normaltekst>
+                        utbetalinger fra sosialhjelpssøknader, andre utbetalinger finner du her
+                    </Normaltekst>
+                </li>
+            </ul>
+        </div>
+
+    );
+};
+
+export default SavnerUtbetalingPanel;
