16f6827ab54507a46fb522f58e76e2055f4c0c91
martintveter <41987225+martintveter@users.noreply.github.com>
2019-10-29 13:45:05 +0100

    Fikser path for vedleggsopplasting ved å endre `innsynssdataUrl` til å inkludere `/innsyn/` som prefiks (#36)

--- src/redux/innsynsdata/innsynsDataActions.ts
+++ src/redux/innsynsdata/innsynsDataActions.ts
@@ -3,12 +3,12 @@ import {fetchToJson, HttpStatus, REST_STATUS} from "../../utils/restUtils";
 import {InnsynsdataSti, oppdaterInnsynsdataState, settRestStatus} from "./innsynsdataReducer";
 import {history} from "../../configureStore";
 
-export const innsynssdataUrl = (fiksDigisosId: string, sti: string): string => `/${fiksDigisosId}/${sti}`;
+export const innsynsdataUrl = (fiksDigisosId: string, sti: string): string => `/innsyn/${fiksDigisosId}/${sti}`;
 
 export function hentInnsynsdata(fiksDigisosId: string|string, sti: InnsynsdataSti, visFeilSide?: boolean) {
     return (dispatch: Dispatch) => {
         dispatch(settRestStatus(sti, REST_STATUS.PENDING));
-        let url = "/innsyn" + innsynssdataUrl(fiksDigisosId, sti);
+        let url = innsynsdataUrl(fiksDigisosId, sti);
         fetchToJson(url).then((response: any) => {
             dispatch(oppdaterInnsynsdataState(sti, response));
             dispatch(settRestStatus(sti, REST_STATUS.OK));
