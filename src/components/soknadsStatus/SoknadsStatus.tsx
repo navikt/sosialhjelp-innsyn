89d384e70f38b4a55956a4057a77902b80a46f12
Andre Wolden <wolden.andre@gmail.com>
2019-11-25 14:46:58 +0100

    fikser styling

--- src/components/soknadsStatus/SoknadsStatus.tsx
+++ src/components/soknadsStatus/SoknadsStatus.tsx
@@ -4,7 +4,7 @@ import {Element, EtikettLiten, Innholdstittel, Normaltekst} from "nav-frontend-t
 import DokumentMottatt from "../ikoner/DokumentMottatt";
 import DokumentElla from "../ikoner/DocumentElla";
 import "./soknadsStatus.less";
-import { SaksStatusState, SaksStatus, VedtakFattet} from "../../redux/innsynsdata/innsynsdataReducer";
+import {SaksStatusState, SaksStatus, VedtakFattet} from "../../redux/innsynsdata/innsynsdataReducer";
 import EksternLenke from "../eksternLenke/EksternLenke";
 import {FormattedMessage} from "react-intl";
 import Lastestriper from "../lastestriper/Lasterstriper";
@@ -13,135 +13,141 @@ import DatoOgKlokkeslett from "../tidspunkt/DatoOgKlokkeslett";
 import DokumentSendt from "../ikoner/DokumentSendt";
 
 export enum SoknadsStatusEnum {
-	SENDT = "SENDT",
-	MOTTATT = "MOTTATT",
-	UNDER_BEHANDLING = "UNDER_BEHANDLING",
-	FERDIGBEHANDLET = "FERDIGBEHANDLET",
-	BEHANDLES_IKKE = "BEHANDLES_IKKE"
+    SENDT = "SENDT",
+    MOTTATT = "MOTTATT",
+    UNDER_BEHANDLING = "UNDER_BEHANDLING",
+    FERDIGBEHANDLET = "FERDIGBEHANDLET",
+    BEHANDLES_IKKE = "BEHANDLES_IKKE"
 }
+
 export enum SaksStatusEnum {
-	BEHANDLES_IKKE = "BEHANDLES IKKE",
-	IKKE_INNSYN = "IKKE INNSYN"
+    BEHANDLES_IKKE = "BEHANDLES IKKE",
+    IKKE_INNSYN = "IKKE INNSYN"
 }
 
 interface StatusDetalj {
-	beskrivelse: string;
-	status: string;
-	kommentarer?: React.ReactNode|string;
+    beskrivelse: string;
+    status: string;
+    kommentarer?: React.ReactNode | string;
 }
 
 interface Props {
-	status: string|null|SoknadsStatusEnum;
-	sak: null|SaksStatusState[];
-	leserData: boolean;
+    status: string | null | SoknadsStatusEnum;
+    sak: null | SaksStatusState[];
+    leserData: boolean;
 }
 
 const SoknadsStatus: React.FC<Props> = ({status, sak, leserData}) => {
-	const antallSaksElementer: number = sak ? sak.length : 0;
+    const antallSaksElementer: number = sak ? sak.length : 0;
 
-	return (
-		<Panel className={"panel-uthevet " + (antallSaksElementer > 0 ? "panel-uthevet-luft-under" : "")}>
-			<div className="tittel_og_ikon">
-				{leserData && (
-					<Lastestriper linjer={1}/>
-				)}
-				{status === SoknadsStatusEnum.SENDT && (
-					<>
-						<Innholdstittel><FormattedMessage id="status.sendt" /></Innholdstittel>
-						<DokumentSendt />
-					</>
-				)}
-				{status === SoknadsStatusEnum.MOTTATT && (
-					<>
-						<Innholdstittel><FormattedMessage id="status.mottatt" /></Innholdstittel>
-						<DokumentMottatt />
-					</>
-				)}
-				{status === SoknadsStatusEnum.UNDER_BEHANDLING && (
-					<>
-						<Innholdstittel><FormattedMessage id="status.under_behandling" /></Innholdstittel>
-						<DokumentElla />
-					</>
-				)}
-				{status === SoknadsStatusEnum.FERDIGBEHANDLET && (
-					<>
-						<Innholdstittel><FormattedMessage id="status.ferdigbehandlet" /></Innholdstittel>
-						<DokumentOk />
-					</>
-				)}
-				{status === SoknadsStatusEnum.BEHANDLES_IKKE && (
-					<>
-						<Innholdstittel><FormattedMessage id="status.behandles_ikke" /></Innholdstittel>
-						<DokumentOk />
-					</>
-				)}
-			</div>
+    return (
+        <Panel className={"panel-uthevet " + (antallSaksElementer > 0 ? "panel-uthevet-luft-under" : "")}>
+            <div className="tittel_og_ikon">
+                {leserData && (
+                    <Lastestriper linjer={1}/>
+                )}
+                {status === SoknadsStatusEnum.SENDT && (
+                    <>
+                        <Innholdstittel><FormattedMessage id="status.sendt"/></Innholdstittel>
+                        <DokumentSendt/>
+                    </>
+                )}
+                {status === SoknadsStatusEnum.MOTTATT && (
+                    <>
+                        <Innholdstittel><FormattedMessage id="status.mottatt"/></Innholdstittel>
+                        <DokumentMottatt/>
+                    </>
+                )}
+                {status === SoknadsStatusEnum.UNDER_BEHANDLING && (
+                    <>
+                        <Innholdstittel><FormattedMessage id="status.under_behandling"/></Innholdstittel>
+                        <DokumentElla/>
+                    </>
+                )}
+                {status === SoknadsStatusEnum.FERDIGBEHANDLET && (
+                    <>
+                        <Innholdstittel><FormattedMessage id="status.ferdigbehandlet"/></Innholdstittel>
+                        <DokumentOk/>
+                    </>
+                )}
+                {status === SoknadsStatusEnum.BEHANDLES_IKKE && (
+                    <>
+                        <Innholdstittel><FormattedMessage id="status.behandles_ikke"/></Innholdstittel>
+                        <DokumentOk/>
+                    </>
+                )}
+            </div>
 
-			{status === SoknadsStatusEnum.BEHANDLES_IKKE && antallSaksElementer === 0 && (
-				<div className="status_detalj_panel status_detalj_panel_luft_under">
-					<Element><FormattedMessage id="saker.default_tittel" /></Element>
-					<div className="panel-glippe-over">
-						<Normaltekst>
-							<FormattedMessage id="status.behandles_ikke_ingress" />
-						</Normaltekst>
-					</div>
-				</div>
-			)}
+            {status === SoknadsStatusEnum.BEHANDLES_IKKE && antallSaksElementer === 0 && (
+                <div className="status_detalj_panel status_detalj_panel_luft_under">
+                    <Element><FormattedMessage id="saker.default_tittel"/></Element>
+                    <div className="panel-glippe-over">
+                        <Normaltekst>
+                            <FormattedMessage id="status.behandles_ikke_ingress"/>
+                        </Normaltekst>
+                    </div>
+                </div>
+            )}
 
-			{sak && sak.map((statusdetalj: SaksStatusState, index: number) => {
-				let saksStatus = statusdetalj.status.replace(/_/g,' ');
-				saksStatus =	saksStatus === "FERDIGBEHANDLET" ? "FERDIG BEHANDLET" : saksStatus;
-				const kanVises: boolean = statusdetalj.status !== SaksStatus.IKKE_INNSYN;
-				return (
-					<div className="status_detalj_panel" key={index}>
-						<div className="status_detalj_panel__tittel">
-							<Element>{statusdetalj.tittel}</Element>
-						</div>
-						{kanVises
+            {sak && sak.map((statusdetalj: SaksStatusState, index: number) => {
+                let saksStatus = statusdetalj.status.replace(/_/g, ' ');
+                saksStatus = saksStatus === "FERDIGBEHANDLET" ? "FERDIG BEHANDLET" : saksStatus;
+                const kanVises: boolean = statusdetalj.status !== SaksStatus.IKKE_INNSYN;
+                return (
+                    <div className="status_detalj_panel" key={index}>
+                        <div className={"status_detalj_linje"}>
+                            <div className="status_detalj_panel__tittel">
+                                <Element>{statusdetalj.tittel}</Element>
+                            </div>
+                            {kanVises
                             && saksStatus !== SaksStatusEnum.IKKE_INNSYN
                             && saksStatus !== SaksStatusEnum.BEHANDLES_IKKE
                             && status !== SoknadsStatusEnum.BEHANDLES_IKKE
-                            &&(
-							<div className="status_detalj_panel__status">
-								<EtikettLiten>{saksStatus}</EtikettLiten>
-							</div>
-						)}
+                            && (
+                                <div className="status_detalj_panel__status">
+                                    <EtikettLiten>{saksStatus}</EtikettLiten>
+                                </div>
+                            )}
+                        </div>
 
-						{statusdetalj.vedtaksfilUrlList && statusdetalj.vedtaksfilUrlList.map((hendelse: VedtakFattet, index: number) => (
-							<div className="status_detalj_panel__kommentarer" key={index}>
-								<EksternLenke href={"" + hendelse.vedtaksfilUrl} target="_blank">
-									Vedtak
-									(<DatoOgKlokkeslett bareDato={true} tidspunkt={hendelse.dato}/>)
-								</EksternLenke>
-							</div>
-						))}
-						{statusdetalj.melding && statusdetalj.melding.length > 0 && (
-							<div className="panel-glippe-over">
-								<Normaltekst>
-									{statusdetalj.melding}
-								</Normaltekst>
-							</div>
-						)}
-						{(saksStatus === SaksStatusEnum.BEHANDLES_IKKE || status === SoknadsStatusEnum.BEHANDLES_IKKE) && (
-						<div className="panel-glippe-over">
-							<Normaltekst>
-								<FormattedMessage id="status.behandles_ikke_ingress" />
-							</Normaltekst>
-						</div>
-						)}
-						{saksStatus === SaksStatusEnum.IKKE_INNSYN && (
-						<div className="panel-glippe-over">
-							<Normaltekst>
-								<FormattedMessage id="status.ikke_innsyn_ingress" />
-							</Normaltekst>
-						</div>
-						)}
-					</div>
-				)
-			})}
+                        {statusdetalj.melding && statusdetalj.melding.length > 0 && (
+                            <div className="panel-glippe-over">
+                                <Normaltekst>
+                                    {statusdetalj.melding}
+                                </Normaltekst>
+                            </div>
+                        )}
+                        {(saksStatus === SaksStatusEnum.BEHANDLES_IKKE || status === SoknadsStatusEnum.BEHANDLES_IKKE) && (
+                            <div className="panel-glippe-over">
+                                <Normaltekst>
+                                    <FormattedMessage id="status.behandles_ikke_ingress"/>
+                                </Normaltekst>
+                            </div>
+                        )}
+                        {saksStatus === SaksStatusEnum.IKKE_INNSYN && (
+                            <div className="panel-glippe-over">
+                                <Normaltekst>
+                                    <FormattedMessage id="status.ikke_innsyn_ingress"/>
+                                </Normaltekst>
+                            </div>
+                        )}
 
-		</Panel>
-	);
+                        {statusdetalj.vedtaksfilUrlList && statusdetalj.vedtaksfilUrlList.map((hendelse: VedtakFattet, index: number) => (
+                            <div className={"status_detalj_linje"}>
+                                <div className="status_detalj_panel__kommentarer" key={index}>
+                                    <EksternLenke href={"" + hendelse.vedtaksfilUrl} target="_blank">
+                                        Vedtak
+                                        (<DatoOgKlokkeslett bareDato={true} tidspunkt={hendelse.dato}/>)
+                                    </EksternLenke>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )
+            })}
+
+        </Panel>
+    );
 };
 
 export default SoknadsStatus;
