1edbd7d82c63d0f69655ab3e635ba7d04be40924
Andre Wolden <wolden.andre@gmail.com>
2019-11-25 09:45:09 +0100

    mottar vedtaksinfo for en sak fra innsyn-api og benytter det til å bestemme om vilkar panelet skal vises eller ikke

--- src/components/vilkar/VilkarUtils.ts
+++ src/components/vilkar/VilkarUtils.ts
@@ -1,14 +1,24 @@
-import {SaksStatusState, SaksStatus} from "../../redux/innsynsdata/innsynsdataReducer";
+import {SaksStatus, SaksStatusState, UtfallVedtak, Vedtak} from "../../redux/innsynsdata/innsynsdataReducer";
 
 export const getSkalViseVilkarView = (innsynSaksStatusStateListe: SaksStatusState[]): boolean => {
     if (innsynSaksStatusStateListe && Array.isArray(innsynSaksStatusStateListe)) {
-        let innvilgedeSaker: SaksStatusState[] = innsynSaksStatusStateListe.filter((s: SaksStatusState) => s.status === SaksStatus.FERDIGBEHANDLET);
-        if (innvilgedeSaker.length > 0) {
+        let ferdigBehandledeSaker: SaksStatusState[] = innsynSaksStatusStateListe.filter((s: SaksStatusState) => {
+            return s.status === SaksStatus.FERDIGBEHANDLET
+        });
+        let saksStatusStateListeMedInnvilgetEllerDelvisInnvilgetVedtak: SaksStatusState[] = ferdigBehandledeSaker.filter((ferdigBehandledeSak: SaksStatusState) => {
+            let sisteVedtakMedUtfallErInnvilgetEllerDelvisInnvilget = false;
+            ferdigBehandledeSak.vedtaksListe.forEach((vedtak: Vedtak) => {
+                if (vedtak.utfall && (vedtak.utfall === UtfallVedtak.DELVIS_INNVILGET || vedtak.utfall === UtfallVedtak.INNVILGET)){
+                    sisteVedtakMedUtfallErInnvilgetEllerDelvisInnvilget = true;
+                } else if ( vedtak.utfall && (vedtak.utfall === UtfallVedtak.AVVIST || vedtak.utfall === UtfallVedtak.AVSLATT)){
+                    sisteVedtakMedUtfallErInnvilgetEllerDelvisInnvilget = false;
+                }
+            });
+            return sisteVedtakMedUtfallErInnvilgetEllerDelvisInnvilget;
+        });
+        if (saksStatusStateListeMedInnvilgetEllerDelvisInnvilgetVedtak.length > 0) {
             return true;
-        } else {
-            return false;
         }
-    } else {
-        return false;
     }
+    return false;
 };
