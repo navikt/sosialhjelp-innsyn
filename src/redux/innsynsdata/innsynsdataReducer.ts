8f434517771b4d87eaa443b38b0a2cdbeddf872b
Andre Wolden <wolden.andre@gmail.com>
2019-12-09 12:42:52 +0100

    mottar link tekst fra backend sammen med url

--- src/redux/innsynsdata/innsynsdataReducer.ts
+++ src/redux/innsynsdata/innsynsdataReducer.ts
@@ -135,7 +135,12 @@ export interface Status {
 export interface Hendelse {
     tidspunkt: string;
     beskrivelse: string;
-    filUrl: null | string;
+    filUrl: null | UrlResponse;
+}
+
+export interface UrlResponse {
+    linkTekst: string,
+    link: string
 }
 
 export interface VedtakFattet {
