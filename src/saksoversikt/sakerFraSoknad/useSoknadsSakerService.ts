cce151af609becd98b35f1a93249fd461581a4eb
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2019-12-18 10:42:50 +0100

    GCP test URL fix mot soknaden

--- src/saksoversikt/sakerFraSoknad/useSoknadsSakerService.ts
+++ src/saksoversikt/sakerFraSoknad/useSoknadsSakerService.ts
@@ -20,6 +20,9 @@ const useSoknadsSakerService = () => {
     if(erDev()) {
         url = "http://localhost:8181" + url;
     }
+    if (window.location.origin.indexOf("innsyn.dev-nav.no") >= 0) {
+        url = window.location.origin.replace("innsyn.dev-nav.no", "soknad-api.dev-nav.no") + url;
+    }
     useEffect(() => {
         fetch(url)
             .then(response => response.json())
