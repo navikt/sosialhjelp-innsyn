e138e4a73b9b9820c51f66490303af41aa9d42c8
siljee <silje.hansen3@nav.no>
2020-06-23 20:07:35 +0200

    Inkluderer console logger fra craco (med farger)

--- start.js
+++ start.js
@@ -2,11 +2,23 @@ var os = require('os');
 var exec = require('child_process').exec;
 
 var runString = "";
-console.log("Du kjører på OS: " + os.type())
+console.log("Ditt OS er " + os.type())
 if (os.type() === 'Windows_NT')
-    runString = "set PORT=3002 && craco start";
+    runString = "set PORT=3002 && craco start --color";
 else
-    runString = "PORT=3002 craco start";
+    runString = "PORT=3002 craco start --color";
 
-exec(runString);
-console.log("Starter med commando: " + runString);
+var cracoStart = exec(runString);
+console.log("Starter med commando: " + runString + "\n");
+
+cracoStart.stdout.on('data', function (data) {
+    console.log(data.toString());
+});
+
+cracoStart.stderr.on('data', function (data) {
+    console.log('ERROR: ' + data.toString());
+});
+
+cracoStart.on('exit', function (code) {
+    console.log('child process exited with code ' + code.toString());
+});
