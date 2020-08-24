057ebe7c0ef24aaaf04dbbd7648e337d2606ea64
siljee <silje.hansen3@nav.no>
2020-08-24 08:40:51 +0200

    Ta i bruk nytt endepunkt for raskere utbetalings-sjekk på landingssiden (#168)

---
+++ src/utbetalinger/service/useUtbetalingerExistsService.ts
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+import {fetchToJson, REST_STATUS} from "../../utils/restUtils";
+import {ServiceHookTypes} from "../../utils/ServiceHookTypes";
+import {logErrorMessage} from "../../redux/innsynsdata/loggActions";
+
+const useUtbetalingerExistsService = (month: number) => {
+    const [result, setResult] = useState<ServiceHookTypes<boolean>>({
+        restStatus: REST_STATUS.PENDING,
+    });
+
+    useEffect(() => {
+        const url = "/innsyn/utbetalinger/exists";
+        setResult({restStatus: REST_STATUS.PENDING});
+        fetchToJson(url + "?month=" + month)
+            .then((response: any) => {
+                setResult({restStatus: REST_STATUS.OK, payload: response});
+            })
+            .catch((error: any) => {
+                logErrorMessage(error.message, error.navCallId);
+                setResult({restStatus: REST_STATUS.FEILET, error});
+            });
+    }, [month]);
+    return result;
+};
+
+export default useUtbetalingerExistsService;
