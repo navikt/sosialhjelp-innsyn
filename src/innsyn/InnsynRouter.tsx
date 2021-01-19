5c0bfc3f7080475707c8dbd79d1478107926fe43
martintveter <41987225+martintveter@users.noreply.github.com>
2021-01-19 14:47:29 +0100

    Revert "Fjerner demo-side. (#225)"
    This reverts commit e526e3847d2ae21dd9bb784e221e2e56c96c85b9.

--- src/innsyn/InnsynRouter.tsx
+++ src/innsyn/InnsynRouter.tsx
@@ -3,6 +3,7 @@ import AppBanner from "../components/appBanner/AppBanner";
 import {ConnectedRouter} from "connected-react-router";
 import {history} from "../configureStore";
 import {Route, Switch} from "react-router";
+import InnsynDemoMeny from "./statiskeDemoSider/InnsynDemoMeny";
 import SaksStatus from "./SaksStatus";
 import Linkside from "../components/linkside/Linkside";
 import SideIkkeFunnet from "../components/sideIkkeFunnet/SideIkkeFunnet";
@@ -19,6 +20,7 @@ const InnsynRouter: React.FC = () => {
                 <div className={"blokk-center " + (ekstraSpaltebredde ? "blokk-center--wide" : "")}>
                     <Switch>
                         <Route exact path="/innsyn/utbetaling" component={Utbetalinger} />
+                        <Route exact path="/innsyn/demo" component={InnsynDemoMeny} />
                         <Route exact path="/innsyn/:soknadId/status" component={SaksStatus} />
                         <Route exact path="/innsyn/link" component={Linkside} />
                         <Route component={SideIkkeFunnet} />
---
+++ src/innsyn/statiskeDemoSider/InnsynDemoMeny.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Panel from "nav-frontend-paneler";
+import {Innholdstittel, Normaltekst, Sidetittel} from "nav-frontend-typografi";
+import {getAbsoluteBasename} from "../../configureStore";
+import {getApiBaseUrlForSwagger} from "../../utils/restUtils";
+import Lenke from "nav-frontend-lenker";
+
+const InnsynDemoMeny: React.FC = () => {
+    return (
+        <Panel>
+            <Sidetittel>Sosialhjelp innsyn</Sidetittel>
+            <Normaltekst>Innsyn for sosialsøkere. Data som vises er testdata fra backend.</Normaltekst>
+            <br />
+
+            <Innholdstittel>Endre innsynsdata med swagger</Innholdstittel>
+            <Normaltekst>
+                For å teste innsynsløsning, kan man laste opp innsynsdata som JSON til API via swaggersidene slik:
+            </Normaltekst>
+            <br />
+            <ol className="typo-normal">
+                <li>
+                    Gå til <Lenke href={getApiBaseUrlForSwagger()}>swagger</Lenke>
+                </li>
+                <li>
+                    Velg <b>digisos-api-controller</b> og tilhørende POST-kall, og deretter <b>'Try it out'</b>
+                </li>
+                <li>Lim inn dine testdata som jsonDigisosSoker (digisos-soker.json)</li>
+                <li>Velg en 'soknadId' for dine testdata</li>
+                <li>
+                    Gå til <Lenke href={"/" + getAbsoluteBasename() + "/innsyn/soknadId/status"}>status-siden</Lenke>
+                </li>
+                <li>
+                    Endre 'soknadId' i url til å matche din soknadId fra steg 4 for å se innsynsvisningen med dine
+                    testdata
+                </li>
+            </ol>
+            <br />
+        </Panel>
+    );
+};
+
+export default InnsynDemoMeny;
