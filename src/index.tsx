d532baf59fc934cb8e28200c2e97b480b2ab2f63
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-01-19 14:39:09 +0100

    Fikser feil som ga error i konsollet hver gang modal ble åpnet eller lukket

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,14 @@ import ReactDOM from "react-dom";
 import "./index.less";
 import App from "./App";
 import "formdata-polyfill";
+import Modal from "react-modal";
 
 window.onerror = (errorMessage, url, line, column, error) => {
     // TODO implementere clientlogger, f.eks. slik:
     // store.dispatch(loggException(errorMessage.toString(), url, line, column, error));
     console.warn("TODO: Implementer clientlogger");
 };
+
+Modal.setAppElement("#root");
 
 ReactDOM.render(<App />, document.getElementById("root"));
