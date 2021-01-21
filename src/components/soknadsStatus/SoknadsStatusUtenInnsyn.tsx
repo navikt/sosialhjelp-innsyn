bb48c502d7deb8f81e21b7c749660bcf66e22df4
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-01-21 16:21:21 +0100

    Små fixes etter PR-feedback

--- src/components/soknadsStatus/SoknadsStatusUtenInnsyn.tsx
+++ src/components/soknadsStatus/SoknadsStatusUtenInnsyn.tsx
@@ -10,24 +10,17 @@ import {REST_STATUS, skalViseLastestripe} from "../../utils/restUtils";
 import DokumentSendt from "../ikoner/DokumentSendt";
 import DatoOgKlokkeslett from "../tidspunkt/DatoOgKlokkeslett";
 
-interface Props {
+const SoknadsStatusUtenInnsyn = (props: {
     restStatus: REST_STATUS;
     tidspunktSendt: string | null;
     navKontor: string | null;
     filUrl: UrlResponse | null;
-}
-
-const SoknadsStatusUtenInnsyn: React.FC<Props> = ({restStatus, tidspunktSendt, navKontor, filUrl}) => {
-    let formatertDato =
-        tidspunktSendt === null ? null : (
-            <DatoOgKlokkeslett bareDato={true} tidspunkt={tidspunktSendt} brukKortMaanedNavn={true} />
-        );
-
+}) => {
     return (
         <Panel className={"panel-uthevet"}>
             <div className="tittel_og_ikon">
-                {skalViseLastestripe(restStatus) && <Lastestriper linjer={1} />}
-                {restStatus !== REST_STATUS.FEILET && (
+                {skalViseLastestripe(props.restStatus) && <Lastestriper linjer={1} />}
+                {props.restStatus !== REST_STATUS.FEILET && (
                     <>
                         <Innholdstittel>Søknaden er sendt</Innholdstittel>
                         <DokumentSendt />
@@ -36,11 +29,13 @@ const SoknadsStatusUtenInnsyn: React.FC<Props> = ({restStatus, tidspunktSendt, n
             </div>
 
             <div className="status_detalj_panel_info_alert_luft_under">
-                {formatertDato && navKontor && filUrl && (
+                {props.tidspunktSendt && props.navKontor && props.filUrl && (
                     <Normaltekst>
-                        Sendt den {formatertDato} til {navKontor}{" "}
-                        <EksternLenke href={filUrl.link} target="_blank">
-                            {filUrl.linkTekst}
+                        Sendt den{" "}
+                        <DatoOgKlokkeslett bareDato={true} tidspunkt={props.tidspunktSendt} brukKortMaanedNavn={true} />
+                        til {props.navKontor}{" "}
+                        <EksternLenke href={props.filUrl.link} target="_blank">
+                            {props.filUrl.linkTekst}
                         </EksternLenke>
                     </Normaltekst>
                 )}
