8d7130a80c3cd6ec6901d11df06c182fe9d26402
Ida Onshus <25682301+idaons@users.noreply.github.com>
2021-01-28 13:28:26 +0100

    skiller på css mellom Feilside og SideIkkeFunnet

--- src/components/sideIkkeFunnet/SideIkkeFunnet.tsx
+++ src/components/sideIkkeFunnet/SideIkkeFunnet.tsx
@@ -5,25 +5,25 @@ import "./SideIkkeFunnet.less";
 
 const SideIkkeFunnet: React.FC<{}> = () => {
     return (
-        <div className="feilside skjema-content">
-            <div className="feilside__ikon">
+        <div className="sideIkkeFunnet skjema-content">
+            <div className="sideIkkeFunnet__ikon">
                 <UtropstegnSirkelGraIkon />
             </div>
-            <Innholdstittel className="feilside__tittel">"OOPS, NOE GIKK GALT"</Innholdstittel>
-            <div className="feilside__innhold">Vi fant ikke siden du prøvde å åpne</div>
+            <Innholdstittel className="sideIkkeFunnet__tittel">"OOPS, NOE GIKK GALT"</Innholdstittel>
+            <div className="sideIkkeFunnet__innhold">Vi fant ikke siden du prøvde å åpne</div>
 
-            <ul className="feilside__link-liste">
-                <li className="feilside__link">
+            <ul className="sideIkkeFunnet__link-liste">
+                <li className="sideIkkeFunnet__link">
                     <a href="http://www.nav.no" className="lenke">
                         Gå til forsiden nav.no
                     </a>
                 </li>
-                <li className="feilside__link">
+                <li className="sideIkkeFunnet__link">
                     <a href="https://www.nav.no/no/Ditt+NAV" className="lenke">
                         Gå til Ditt NAV
                     </a>
                 </li>
-                <li className="feilside__link">
+                <li className="sideIkkeFunnet__link">
                     <a
                         href="https://www.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Klage+ris+og+ros/Feil+og+mangler+paa+navno"
                         className="lenke"
