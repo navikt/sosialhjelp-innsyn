e4a7f3112f4b79b5d9d056574fbe108f0727bc32
Ida Onshus <25682301+idaons@users.noreply.github.com>
2021-01-28 09:55:17 +0100

    rydder

--- src/components/driftsmelding/Driftsmelding.tsx
+++ src/components/driftsmelding/Driftsmelding.tsx
@@ -16,47 +16,39 @@ const DriftsmeldingAlertstripe: React.FC<{}> = () => {
 
     const driftsmelding: Driftsmelding = getDriftsmeldingByKommuneResponse(kommuneResponse);
     const tidspunkt = (
-        <>
-            <Element>
-                <DatoOgKlokkeslett
-                    bareDato={false}
-                    tidspunkt={
-                        kommuneResponse ? (kommuneResponse.tidspunkt ? kommuneResponse.tidspunkt.toString() : "") : ""
-                    }
-                />
-            </Element>
-        </>
+        <Element>
+            <DatoOgKlokkeslett
+                bareDato={false}
+                tidspunkt={
+                    kommuneResponse ? (kommuneResponse.tidspunkt ? kommuneResponse.tidspunkt.toString() : "") : ""
+                }
+            />
+        </Element>
     );
 
     switch (driftsmelding.type) {
         case DriftsmeldingTypeKeys.DRIFTSMELDING_ETTERSENDELSE_DEAKTIVERT: {
             return (
-                <>
-                    <AlertStripe type="feil">
-                        {tidspunkt}
-                        <FormattedMessage id={driftsmelding.textKey} />
-                    </AlertStripe>
-                </>
+                <AlertStripe type="feil">
+                    {tidspunkt}
+                    <FormattedMessage id={driftsmelding.textKey} />
+                </AlertStripe>
             );
         }
         case DriftsmeldingTypeKeys.DRIFTSMELDING_INNSYN_DEAKTIVERT: {
             return (
-                <>
-                    <AlertStripe type="feil">
-                        {tidspunkt}
-                        <FormattedMessage id={driftsmelding.textKey} />
-                    </AlertStripe>
-                </>
+                <AlertStripe type="feil">
+                    {tidspunkt}
+                    <FormattedMessage id={driftsmelding.textKey} />
+                </AlertStripe>
             );
         }
         case DriftsmeldingTypeKeys.DRIFTSMELDING_INNSYN_OG_ETTERSENDELSE_DEAKTIVERT: {
             return (
-                <>
-                    <AlertStripe type="feil">
-                        {tidspunkt}
-                        <FormattedMessage id={driftsmelding.textKey} />
-                    </AlertStripe>
-                </>
+                <AlertStripe type="feil">
+                    {tidspunkt}
+                    <FormattedMessage id={driftsmelding.textKey} />
+                </AlertStripe>
             );
         }
         case DriftsmeldingTypeKeys.DRIFTSMELDING_INGEN: {
--- src/components/sideIkkeFunnet/SideIkkeFunnet.tsx
+++ src/components/sideIkkeFunnet/SideIkkeFunnet.tsx
@@ -5,36 +5,34 @@ import "./SideIkkeFunnet.less";
 
 const SideIkkeFunnet: React.FC<{}> = () => {
     return (
-        <>
-            <div className="feilside skjema-content">
-                <div className="feilside__ikon">
-                    <UtropstegnSirkelGraIkon />
-                </div>
-                <Innholdstittel className="feilside__tittel">"OOPS, NOE GIKK GALT"</Innholdstittel>
-                <div className="feilside__innhold">Vi fant ikke siden du prøvde å åpne</div>
-
-                <ul className="feilside__link-liste">
-                    <li className="feilside__link">
-                        <a href="http://www.nav.no" className="lenke">
-                            Gå til forsiden nav.no
-                        </a>
-                    </li>
-                    <li className="feilside__link">
-                        <a href="https://www.nav.no/no/Ditt+NAV" className="lenke">
-                            Gå til Ditt NAV
-                        </a>
-                    </li>
-                    <li className="feilside__link">
-                        <a
-                            href="https://www.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Klage+ris+og+ros/Feil+og+mangler+paa+navno"
-                            className="lenke"
-                        >
-                            Meld fra om feil
-                        </a>
-                    </li>
-                </ul>
+        <div className="feilside skjema-content">
+            <div className="feilside__ikon">
+                <UtropstegnSirkelGraIkon />
             </div>
-        </>
+            <Innholdstittel className="feilside__tittel">"OOPS, NOE GIKK GALT"</Innholdstittel>
+            <div className="feilside__innhold">Vi fant ikke siden du prøvde å åpne</div>
+
+            <ul className="feilside__link-liste">
+                <li className="feilside__link">
+                    <a href="http://www.nav.no" className="lenke">
+                        Gå til forsiden nav.no
+                    </a>
+                </li>
+                <li className="feilside__link">
+                    <a href="https://www.nav.no/no/Ditt+NAV" className="lenke">
+                        Gå til Ditt NAV
+                    </a>
+                </li>
+                <li className="feilside__link">
+                    <a
+                        href="https://www.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Klage+ris+og+ros/Feil+og+mangler+paa+navno"
+                        className="lenke"
+                    >
+                        Meld fra om feil
+                    </a>
+                </li>
+            </ul>
+        </div>
     );
 };
 
