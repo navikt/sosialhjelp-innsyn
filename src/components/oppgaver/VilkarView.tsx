591a0d461e23199b56bf88dc4a35bf03267c1ee9
Line Moseng <line.moseng@nav.no>
2021-07-05 13:49:49 +0200

    Visning av vilkår

--- src/components/oppgaver/VilkarView.tsx
+++ src/components/oppgaver/VilkarView.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Vilkar} from "../../redux/innsynsdata/innsynsdataReducer";
+import {Element, Normaltekst} from "nav-frontend-typografi";
 
 interface Props {
     vilkar: Vilkar;
@@ -7,8 +8,17 @@ interface Props {
 
 export const VilkarView: React.FC<Props> = ({vilkar}) => {
     return (
-        <div>
-            <div className={"oppgaver_detaljer luft_over_1rem"}>{vilkar.tittel}</div>
+        <div className={"oppgaver_detaljer luft_over_1rem"}>
+            <div className={"oppgave-detalj-overste-linje"}>
+                <div className={"tekst-wrapping"}>
+                    <Element>{vilkar.tittel}</Element>
+                </div>
+                {vilkar.beskrivelse && (
+                    <div className={"tekst-wrapping"}>
+                        <Normaltekst className="luft_over_4px">{vilkar.beskrivelse}</Normaltekst>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
