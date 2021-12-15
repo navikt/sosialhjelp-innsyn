1f8a2ba9fdc2fe6bb9fd44a4271b80ed540aa03e
LovConst <68546812+LovConst@users.noreply.github.com>
2021-12-15 14:58:12 +0100

    Update tekster.ts

--- src/tekster/tekster.ts
+++ src/tekster/tekster.ts
@@ -1,90 +1,86 @@
 export const tekster = {
     nb: {
         "historikk.tittel": "Historikk",
-        "historikk.se_vedtaksbrev": "Vis brevet",
+        "historikk.se_vedtaksbrev": "Vis svar på søknaden",
 
         "status.sendt": "Søknaden er sendt",
         "status.mottatt": "Søknaden er mottatt",
         "status.under_behandling": "Søknaden er under behandling",
-        "status.ferdigbehandlet": "Søknaden er ferdig behandlet",
-        "status.behandles_ikke": "Søknaden er ferdig behandlet",
+        "status.ferdigbehandlet": "Søknaden er ferdigbehandlet",
+        "status.behandles_ikke": "Søknaden er ferdigbehandlet",
         "status.soknad_behandles_ikke_ingress":
-            "Vi kan ikke vise behandlingsstatus på nett. Det kan for eksempel være fordi søknaden blir behandlet sammen med en annen søknad eller fordi du har gitt beskjed om at du likevel ikke ønsket å søke. Har du spørsmål kan du ta kontakt med NAV-kontoret ditt.",
+            "Vi kan ikke vise status på søknaden din på nav.no. Det kan være fordi søknaden din blir behandlet sammen med en annen søknad du har sendt, eller fordi du har trukket tilbake søknaden. Har du spørsmål kan du ta kontakt med NAV-kontoret ditt.",
         "status.sak_behandles_ikke_ingress":
-            "Vi kan ikke vise behandlingsstatus på nett. Det kan for eksempel være fordi saken blir behandlet sammen med en annen søknad eller fordi du har gitt beskjed om at du likevel ikke ønsket å søke. Har du spørsmål kan du ta kontakt med NAV-kontoret ditt.",
+            "Vi kan ikke vise status på søknaden din på nav.no. Det kan være fordi søknaden din blir behandlet sammen med en annen søknad du har sendt, eller fordi du har trukket tilbake søknaden. Har du spørsmål kan du ta kontakt med NAV-kontoret ditt.",
         "status.ikke_innsyn_ingress":
-            "Saken din vil bli behandlet, men vi kan ikke vise behandlingsstatus på nett. Har du spørsmål kan du ta kontakt med NAV-kontoret ditt.",
+            "Søknaden din vil bli behandlet, men vi kan ikke vise status på søknaden din på nav.no. Har du spørsmål kan du ta kontakt med NAV-kontoret ditt.",
 
         "saksStatus.under_behandling": "Under behandling",
-        "saksStatus.ferdig_behandlet": "Ferdig behandlet",
+        "saksStatus.ferdig_behandlet": "Ferdigbehandlet",
         "saksStatus.kan_ikke_vise_status": "Kan ikke vise status",
 
         "tidspunkt.klokken": "klokken",
         "saker.oppgave": "Du har oppgaver",
         "saker.default_tittel": "Søknad om økonomisk sosialhjelp",
 
-        "oppgaver.vilkar.tittel": "Vedtaket ditt kan inneholde vilkår eller dokumentasjonskrav",
+        "oppgaver.vilkar.tittel": "Du kan ha vilkår eller dokumentasjonskrav",
         "oppgaver.vilkar.tittel.tekst":
             "Du må følge disse for å sørge for at du får utbetalt riktig beløp til riktig tid.",
         "oppgaver.vilkar": "Vilkår",
         "oppgaver.vilkar.beskrivelse":
-            "Les vedtaket ditt for å se om du har vilkår. Dette er aktiviteter du må gjøre som en del av oppfølgingen din fra NAV.",
+            "Les svaret på søknaden din for å se om du har vilkår. Dette er aktiviteter du må gjøre som en del av oppfølgingen din fra NAV.",
         "oppgaver.vilkar.dokumentasjonskrav": "Dokumentasjonskrav",
         "oppgaver.vilkar.dokumentasjonskrav.beskrivelse":
-            "Les vedtaket ditt for å se om vi trenger mer dokumentasjon fra deg. Dette kan du sende til oss i “Dine vedlegg” fanen under.",
+            "Les svaret på søknaden din for å se om vi trenger flere opplysninger fra deg. Dette kan du sende til oss i «Dine vedlegg» fanen under.",
 
         "oppgaver.dine_oppgaver": "Dine oppgaver",
-        "oppgaver.maa_sende_dok_veileder": "Du må sende dokumentasjon",
-        "oppgaver.maa_sende_dok": "Du må sende inn dokumentasjon",
-        "oppgaver.innsendelsesfrist": "Frist for innlevering er {innsendelsesfrist}",
-        "oppgaver.innsendelsesfrist_passert": "Frist for innlevering var {innsendelsesfrist}",
+        "oppgaver.maa_sende_dok_veileder": "Du må sende opplysninger",
+        "oppgaver.maa_sende_dok": "Du må sende opplysninger",
+        "oppgaver.innsendelsesfrist": "Fristen din for å sende dem er {innsendelsesfrist}",
+        "oppgaver.innsendelsesfrist_passert": "Frist for å sende dem var {innsendelsesfrist}",
         "oppgaver.veileder_trenger_mer":
-            "Vi trenger mer dokumentasjon for å behandle søknaden din. " +
-            "Hvis du ikke leverer dokumentasjonen innen fristen, blir " +
-            "søknaden behandlet med den informasjonen vi har. " +
-            "Har du levert dokumentasjonen til NAV-kontoret, kan du se bort fra denne meldingen.",
+            "Vi trenger flere opplysninger om deg for å behandle søknaden din. " +
+            "Har du levert opplysningene til NAV-kontoret, kan du se bort fra denne meldingen.",
         "oppgaver.last_opp_vedlegg_ikke":
-            "Last opp vedlegg du ikke lastet opp da du sendte søknaden. " +
-            "Vi anbefaler at du ettersender vedlegg så snart som mulig og helst innen 14 dager. " +
-            "Hvis du ikke leverer dokumentasjonen, blir søknaden behandlet med den informasjonen vi " +
-            "har. " +
-            "Har du levert dokumentasjonen til NAV-kontoret, kan du se bort fra denne meldingen.",
-        "oppgaver.hjelp_last_opp": "Hjelp til å sende dokumentasjon?",
-        "oppgaver.informasjon.modal.overskrift": "Trenger du hjelp til å sende dokumentasjon?",
-        "oppgaver.informasjon.modal.bolk1.tittel": "Sende dokumentasjon på papir",
+            "Her ser du hvilke opplysninger du ikke sendte sammen med søknaden. " +
+            "Vi anbefaler at du sender opplysningene så snart som mulig, og helst innen 14 dager. " +
+            "Har du levert opplysningene til NAV-kontoret, kan du se bort fra denne meldingen.",
+        "oppgaver.hjelp_last_opp": "Hjelp til å sende opplysninger?",
+        "oppgaver.informasjon.modal.overskrift": "Trenger du hjelp til å sende opplysninger?",
+        "oppgaver.informasjon.modal.bolk1.tittel": "Sende opplysninger du har på papir",
         "oppgaver.informasjon.modal.bolk1.avsnitt1":
-            "Har du dokumentasjon på papir er det best å skanne dokumentene. Skannede dokumenter er av bedre kvalitet og lettere for NAV-kontoret å lese.",
+            "Har du opplysninger på papir, kan du skanne de før du sender det digitalt. Ved å skanne opplysningene blir kvaliteten bedre, og det blir lettere for NAV-kontoret å lese de.",
         "oppgaver.informasjon.modal.bolk1.avsnitt2":
-            "Med en skanne app kan du bruke kameraet på mobiltelefonen din til å skanne dokumenter. Du kan samle flere papirsider til ett digitalt dokument, for eksempel husleiekontrakten din.",
+            "Med en skanne app kan du bruke kameraet på mobiltelefonen din til å skanne opplysningene. Du kan samle flere papirsider til ett digitalt vedlegg, for eksempel husleiekontrakten din.",
         "oppgaver.informasjon.modal.bolk1.avsnitt3":
-            "Du kan også ta bilde av dokumentasjon du har på papir med mobiltelefonen din. Bildene kan du legge ved når du sender dokumentasjon fra mobilen. Hvis du bruker datamaskin, kan du sende bildene til deg selv på e-post.",
-        "oppgaver.informasjon.modal.bolk2.tittel": "Sende dokumentasajon fra andre nettsider",
+            "Du kan også ta bilde av opplysninger du har på papir med mobiltelefonen din. Bildene kan du legge ved når du sender vedlegg fra mobilen. Hvis du bruker datamaskin, kan du sende bildene til deg selv på e-post først, og så laste de opp på nav.no.",
+        "oppgaver.informasjon.modal.bolk2.tittel": "Sende opplysninger fra andre nettsider",
         "oppgaver.informasjon.modal.bolk2.avsnitt1":
-            "Hvis du skal dokumentere noe fra en annen nettside, kan du laste ned innholdet hvis det er mulig. Du bør unngå å ta bilde av PC-skjermen med et kamera. For eksempel er skattemeldingen, vedtak fra Lånekassen og kontoutskrifter fra noen banker tilgjengelig for nedlastning som PDF.",
-        "oppgaver.informasjon.modal.bolk3.tittel": "Sørg for at dokumentasjonen du sender kan leses",
+            "Hvis du skal sende opplysninger du har fra en annen nettside, kan du laste ned innholdet hvis det er mulig. For eksempel er skattemeldingen, brev fra Lånekassen og kontoutskrifter fra noen banker tilgjengelig for nedlastning som PDF. Du bør unngå å ta bilde av PC-skjermen med et kamera.",
+        "oppgaver.informasjon.modal.bolk3.tittel": "Sørg for at opplysningene du sender kan leses",
         "oppgaver.informasjon.modal.bolk3.avsnitt1":
-            "Det er viktig at du sjekker at dokumentene du laster opp er fullstendige og kan leses på skjermen. Hvis det er vanskelig å lese dokumentasjonen, så kan du bli bedt om å sende den på nytt.",
-        "oppgaver.informasjon.modal.bolk4.tittel": "Godtar vi ikke dokumentet du prøver å sende inn?",
+            "Det er viktig at du sjekker at opplysningene du laster opp er fullstendige og kan leses på skjermen. Hvis det er vanskelig å lese opplysningene, så kan du bli bedt om å sende de på nytt.",
+        "oppgaver.informasjon.modal.bolk4.tittel": "Godtar vi ikke vedlegget du prøver å sende inn?",
         "oppgaver.informasjon.modal.bolk4.avsnitt1":
-            "Det er noen krav til hvilke dokumenter vi godtar. Vennligst sjekk at dokumentet ikke:",
-        "oppgaver.informasjon.modal.bolk4.liste1": "er over 10 MB pr. dokument",
-        "oppgaver.informasjon.modal.bolk4.liste2": "inneholder andre tegn enn tall og bokstaver i filnavnet",
-        "oppgaver.informasjon.modal.bolk4.liste3": "er andre filtyper enn PDF, JPEG eller PNG",
-        "oppgaver.neste_frist": "Neste frist for innlevering er {innsendelsesfrist}",
-        "oppgaver.neste_frist_passert": "Frist for innlevering var for {antall_dager} siden, den {innsendelsesfrist}",
+            "Har du problemer med å sende inn vedlegg, kan du gjerne sjekke om vedlegget:",
+        "oppgaver.informasjon.modal.bolk4.liste1": "er under 10 MB per dokument",
+        "oppgaver.informasjon.modal.bolk4.liste2": "er av filtypen PDF, JPEG eller PNG",
+        "oppgaver.informasjon.modal.bolk4.liste3": "kun inneholder bokstaver og tall i filnavnet",
+        "oppgaver.neste_frist": "Neste frist for å sende dem er {innsendelsesfrist}",
+        "oppgaver.neste_frist_passert": "Frist for å levere opplysningene var for {antall_dager} siden, den {innsendelsesfrist}",
         "oppgaver.send_knapp_tittel": "Send vedlegg",
         "oppgaver.ingen_oppgaver": "Du har ingen oppgaver.",
         "oppgaver.beskjed": "Du vil få beskjed hvis det er noe du må gjøre.",
-        "oppgaver.andre_dokumenter": "Har du andre dokumenter du vil sende?",
-        "oppgaver.andre_dokumenter_beskjed": 'Du kan sende dokumenter under fanen "Dine vedlegg" nedenfor.',
+        "oppgaver.andre_dokumenter": "Har du andre opplysninger du vil sende?",
+        "oppgaver.andre_dokumenter_beskjed": 'Du kan sende opplysninger under fanen "Dine vedlegg" nedenfor.',
 
-        "vilkar.du_har_vilkar": "Vilkår for søknaden din",
+        "vilkar.du_har_vilkar": "Vilkår for stønaden din",
         "vilkar.veileder_trenger_mer":
-            "Du har {antallVilkar} vilkår. Vilkår er aktiviter som du må gjøre som en del av oppfølgingen din fra NAV.",
+            "Du har {antallVilkar} vilkår. Vilkår er aktiviter du må gjøre som en del av oppfølgingen din fra NAV.",
 
-        "dokumentasjonkrav.dokumentasjon_stonad": "Dokumentasjon for stønaden din",
+        "dokumentasjonkrav.dokumentasjon_stonad": "Opplysninger for stønaden din",
         "dokumentasjonkrav.veileder_trenger_mer":
-            "Du må levere {antall} {dokumenter}. Dokumentasjonen kan være knyttet til vilkårene dine eller det kan være økonomiske opplysninger NAV trenger for å utbetale stønaden.",
+            "Du må levere {antall} {dokumenter}. Opplysningene kan være knyttet til vilkårene dine eller det kan være økonomiske opplysninger NAV trenger for å utbetale stønaden.",
 
         "vedlegg.minst_ett_vedlegg": "Du må først velge vedlegg du vil sende.",
 
@@ -99,13 +95,13 @@ export const tekster = {
         "vedlegg.ulovlig_filnavn_feilmelding": "Filnavn kan kun inneholde a-å, 0-9 og tegnene - _ . , ( )",
         "vedlegg.ulovlig_filstorrelse_feilmelding": "Filer må være under 10 Mb.",
         "vedlegg.ulovlig_storrelse_av_alle_valgte_filer":
-            "Vi kan ikke motta dokumentene dine hvis de tilsammen er over 150 Mb.",
+            "Vi kan ikke motta vedleggene dine hvis de til sammen er over 150 Mb.",
         "vedlegg.velg_fil": "Velg vedlegg",
         "vedlegg.fjern": "Fjern",
         "vedlegg.tittel": "Dine vedlegg",
         "vedlegg.validering.duplikat": "Vedlegget {filnavn} finnes allerede.",
         "vedlegg.opplasting_feilmelding":
-            "Vi kan ikke sende vedleggene. Vennligst slett og korriger filene som feilet over",
+            "Vi kan ikke sende vedleggene. Vennligst fjern og korriger filene over",
         "vedlegg.opplasting_backend_feilmelding":
             "Vi klarte dessverre ikke å sende vedleggene. Vennligst forsøk igjen senere.",
         "vedlegg.opplasting_backend_virus_feilmelding":
@@ -118,20 +114,20 @@ export const tekster = {
         "vedlegg.opplasting_feilmelding_ILLEGAL_FILENAME": "Filnavn kan kun inneholde a-å, 0-9 og tegnene - _ . , ( )",
         "vedlegg.opplasting_feilmelding_FILE_TOO_LARGE": "Filer må være under 10 mb",
         "vedlegg.opplasting_feilmelding_COMBINED_FILES_TOO_LARGE":
-            "Vi kan ikke motta dokumentene dine hvis de tilsammen er over 150 Mb",
+            "Vi kan ikke motta vedleggene dine hvis de til sammen er over 150 Mb",
         "andre_vedlegg.type": "Send andre vedlegg",
         "andre_vedlegg.tilleggsinfo": "Hvis du har andre vedlegg du ønsker å sende oss kan de lastes opp her.",
         "andre_vedlegg.send_knapp_tittel": "Send vedlegg",
 
-        forelopigSvar: "Du har fått et brev om saksbehandlingstiden for søknaden din ",
+        forelopigSvar: "Du har fått ett brev om saksbehandlingstiden for søknaden din ",
 
         "driftsmelding.innsynDeaktivert":
-            "Vi kan dessverre ikke vise nyeste status for saken din. Du kan fortsatt sende inn dokumentasjon digitalt. Ta kontakt med NAV på telefon hvis du har spørsmål.",
+            "Vi kan dessverre ikke vise nyeste status for søknaden din. Du kan fortsatt sende inn opplysningene på nav.no. Har du spørsmål kan du ta kontakt med NAV-kontoret ditt.",
         "driftsmelding.ettersendelseDeaktivert":
-            "Vi kan ikke motta filer fra deg. Du kan enten prøve igjen senere, eller dra til NAV kontoret ditt.",
+            "Vi kan dessverre ikke motta vedlegg fra deg. Du kan enten prøve igjen senere, eller levere de på NAV-kontoret ditt.",
         "driftsmelding.innsynOgEttersendelseDeaktivert":
-            "Vi kan ikke vise deg nyeste status for saken din eller motta filer fra deg. Du kan enten prøve igjen senere, eller dra til NAV kontoret ditt.",
+            "Vi kan dessverre ikke vise deg nyeste status for søknaden din eller motta vedlegg fra deg. Du kan enten prøve igjen senere, eller ta kontakt med NAV-kontoret ditt.",
         "driftsmelding.kanIkkeSendeVedlegg":
-            "Vi har problemer med systemet og ber deg levere vedlegg på ditt NAV kontor",
+            "Vi har dessverre problemer med systemet og ber deg levere vedlegg på ditt NAV kontor",
     },
 };
