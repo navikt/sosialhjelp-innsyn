68cf757cf14007209fea797d732219052e3d0efc
joeldlin <59644512+joeldlin@users.noreply.github.com>
2022-01-05 12:31:41 +0100

    Viser feilmelding for alle serverfeil og log til amplitude (#363)
    Co-authored-by: Halvor Bjørn <halvor.grizzly.bjorn@nav.no>

---
+++ src/innsyn/LoadingResourcesFailedAlert.tsx
@@ -0,0 +1,41 @@
+import {Alert, BodyShort} from "@navikt/ds-react";
+import React, {useEffect, useState} from "react";
+import {REST_STATUS} from "../utils/restUtils";
+import {logServerfeil} from "../utils/amplitude";
+import {useSelector} from "react-redux";
+import {InnsynAppState} from "../redux/reduxTypes";
+
+const leserData = (restStatus: REST_STATUS): boolean => {
+    return restStatus === REST_STATUS.INITIALISERT || restStatus === REST_STATUS.PENDING;
+};
+
+export const LoadingResourcesFailedAlert = () => {
+    const {saksStatus, oppgaver, hendelser, vedlegg} = useSelector(
+        (state: InnsynAppState) => state.innsynsdata.restStatus
+    );
+    const [loadingResourcesFailed, setLoadingResourcesFailed] = useState(false);
+
+    useEffect(() => {
+        if (!leserData(saksStatus) && !leserData(oppgaver) && !leserData(hendelser) && !leserData(vedlegg)) {
+            if (
+                saksStatus === REST_STATUS.FEILET ||
+                oppgaver === REST_STATUS.FEILET ||
+                hendelser === REST_STATUS.FEILET ||
+                vedlegg === REST_STATUS.FEILET
+            ) {
+                logServerfeil({saksStatus, oppgaver, hendelser, vedlegg});
+                setLoadingResourcesFailed(true);
+            }
+        }
+    }, [saksStatus, oppgaver, hendelser, vedlegg]);
+    return (
+        <>
+            {loadingResourcesFailed && (
+                <Alert variant="warning" className="luft_over_16px">
+                    <BodyShort>Vi klarte ikke å hente inn all informasjonen på siden.</BodyShort>
+                    <BodyShort>Du kan forsøke å oppdatere siden, eller prøve igjen senere.</BodyShort>
+                </Alert>
+            )}
+        </>
+    );
+};
