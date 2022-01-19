5a37fc7f27f9b1cbc576e66dc81badd03b790cb0
jorel <jorgen.elden.lindgren@nav.no>
2022-01-19 14:35:08 +0100

    styleendring

--- src/utbetalinger/SavnerUtbetalingPanel.tsx
+++ src/utbetalinger/SavnerUtbetalingPanel.tsx
@@ -9,11 +9,9 @@ const SavnerUtbetalingPanel: React.FC = () => {
                 <InfoIkon />
             </span>
             <Label spacing>Er det en utbetaling du savner?</Label>
-            <BodyShort>
-                Oversikten over utbetalinger for økonomisk sosialhjelp er under utvikling. For øyeblikket kan vi kun
-                vise
-            </BodyShort>
-            <ul>
+            <BodyShort>Oversikten over utbetalinger for økonomisk sosialhjelp er under utvikling.</BodyShort>
+            <BodyShort style={{marginTop: "18px"}}>For øyeblikket kan vi kun vise</BodyShort>
+            <ul style={{marginTop: "0px"}}>
                 <li>
                     <BodyShort>utbetalinger for dine digitale søknader, ikke dine søknader på papir.</BodyShort>
                 </li>
