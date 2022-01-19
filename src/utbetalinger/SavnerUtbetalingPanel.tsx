17b8362838c926f5751b0ad8d477c7dd2d5a12e9
jorel <jorgen.elden.lindgren@nav.no>
2022-01-19 11:12:45 +0100

    Tekstendring + lenke

--- src/utbetalinger/SavnerUtbetalingPanel.tsx
+++ src/utbetalinger/SavnerUtbetalingPanel.tsx
@@ -8,24 +8,31 @@ const SavnerUtbetalingPanel: React.FC = () => {
             <span className="infoIkon">
                 <InfoIkon />
             </span>
-            <Label spacing>Savner du en utbetaling på denne siden?</Label>
-            <BodyShort>Utbetalingsoversikten er under utvikling og vi kan for øyeblikket bare vise:</BodyShort>
+            <Label spacing>Er det en utbetaling du savner?</Label>
+            <BodyShort>
+                Oversikten over utbetalinger for økonomisk sosialhjelp er under utvikling. For øyeblikket kan vi kun
+                vise
+            </BodyShort>
             <ul>
                 <li>
+                    <BodyShort>utbetalinger for dine digitale søknader, ikke dine søknader på papir.</BodyShort>
+                </li>
+                <li>
+                    <BodyShort>utbetalinger som er opp til ett år gamle.</BodyShort>
+                </li>
+                <li>
                     <BodyShort>
-                        utbetalinger av økonomisk sosialhjelp,{" "}
+                        utbetalinger for økonomisk sosialhjelp.{" "}
                         <Link href="https://tjenester.nav.no/utbetalingsoversikt">
                             andre utbetalinger finner du her
                         </Link>
                     </BodyShort>
                 </li>
-                <li>
-                    <BodyShort>utbetalinger fra digitale søknader, men ikke fra papirsøknader</BodyShort>
-                </li>
-                <li>
-                    <BodyShort>utbetalinger som er inntil ett år gamle</BodyShort>
-                </li>
             </ul>
+            <BodyShort>
+                Du kan se utbetalinger du skal få fremover i brevet med svar på søknaden din. Du finner dette under{" "}
+                <Link href="https://www.nav.no/sosialhjelp/innsyn">dine søknader.</Link>
+            </BodyShort>
         </div>
     );
 };
