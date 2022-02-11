f363e3593da355261f6d878b5fd97cbf900bd1eb
jorel <jorgen.elden.lindgren@nav.no>
2022-02-11 13:51:04 +0100

    flytter dok.etter og dok.krav accordion til nye komponenter

---
+++ src/components/oppgaver/accordions/DokumentasjonkravAccordion.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import {render, screen} from "../../../test/test-utils";
+import {DokumentasjonkravAccordion} from "./DokumentasjonkravAccordion";
+
+test("Rendrer Dokumentasjonkrav", async () => {
+    render(<DokumentasjonkravAccordion dokumentasjonkrav={[]} />);
+
+    expect(screen.getByText("Du må levere opplysninger")).toBeVisible();
+});
