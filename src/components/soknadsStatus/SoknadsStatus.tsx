beb1b0e8434084e1820a545e3590fef06cd35ab4
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-02-28 12:48:22 +0100

    wip

--- src/components/soknadsStatus/SoknadsStatus.tsx
+++ src/components/soknadsStatus/SoknadsStatus.tsx
@@ -12,15 +12,16 @@ import {Alert, BodyShort, Heading, Label, Panel, Tag} from "@navikt/ds-react";
 import {PlaceFilled} from "@navikt/ds-icons";
 import styled from "styled-components";
 
-const StyledContainer = styled.div`
+const Container = styled.div`
     padding-top: 3rem;
 `;
 
-const StyledPanel = styled(Panel)`
+const ContentPanel = styled(Panel)`
+    padding-top: 2rem;
     position: relative;
 `;
 
-const StyledIconContainer = styled.div`
+const Spot = styled.div`
     position: absolute;
     transform: translate(-50%, -50%);
     top: 0;
@@ -31,7 +32,7 @@ const StyledIconContainer = styled.div`
     width: 4rem;
 `;
 
-const StyledIcon = styled(PlaceFilled)`
+const SpotIcon = styled(PlaceFilled)`
     position: absolute;
     transform: translate(-50%, -50%);
     top: 50%;
@@ -40,8 +41,11 @@ const StyledIcon = styled(PlaceFilled)`
     width: 1.5rem;
 `;
 
-const StyledPanelContent = styled.div`
-    border-radius: 0;
+const ContentPanelHeading = styled(Heading)`
+    text-align: center;
+`;
+
+const ContentPanelBody = styled.div`
     @media screen and (min-width: 641px) {
         padding: 1rem 3.25rem 1rem 3.25rem;
     }
@@ -50,24 +54,30 @@ const StyledPanelContent = styled.div`
     }
 `;
 
-const StyledHeading = styled(Heading)`
-    text-align: center;
-    padding-bottom: 1rem;
-    border-bottom: 2px solid var(--navds-semantic-color-border-muted);
+interface ContentPanelBorderProps {
+    lightColor?: boolean;
+}
+
+const ContentPanelBorder = styled.div<ContentPanelBorderProps>`
+    border-bottom: 2px solid
+        var(
+            ${(props) =>
+                props.lightColor ? "--navds-semantic-color-border-inverted" : "--navds-semantic-color-border-muted"}
+        );
+    margin: 1rem 0;
 `;
 
-const StyledStatusBox = styled.div`
-    padding: 1rem;
-    border-bottom: 2px solid var(--navds-semantic-color-border-inverted);
+const StatusBox = styled.div`
+    margin: 0 1rem;
 `;
 
-const StyledStatusMessage = styled.div`
+const StatusMessage = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: normal;
 `;
 
-const StyledStatusMessageVedtak = styled.div`
+const StatusMessageVedtak = styled.div`
     line-height: 22px;
     margin-top: 6px;
 `;
@@ -98,17 +108,20 @@ const SoknadsStatus: React.FC<Props> = ({status, sak, restStatus}) => {
     };
 
     return (
-        <StyledContainer>
-            <StyledPanel>
-                <StyledIconContainer>
-                    <StyledIcon />
-                </StyledIconContainer>
-                <StyledPanelContent>
+        <Container>
+            <ContentPanel>
+                <Spot>
+                    <SpotIcon />
+                </Spot>
+                <ContentPanelBody>
                     {skalViseLastestripe(restStatus) && <Lastestriper linjer={1} />}
                     {restStatus !== REST_STATUS.FEILET && (
-                        <StyledHeading level="1" size="large">
-                            {soknadsStatusTittel(status, intl)}
-                        </StyledHeading>
+                        <>
+                            <ContentPanelHeading level="1" size="large" spacing>
+                                {soknadsStatusTittel(status, intl)}
+                            </ContentPanelHeading>
+                            <ContentPanelBorder />
+                        </>
                     )}
 
                     {status === SoknadsStatusEnum.BEHANDLES_IKKE && antallSaksElementer === 0 && (
@@ -129,65 +142,74 @@ const SoknadsStatus: React.FC<Props> = ({status, sak, restStatus}) => {
                             const sakBehandlesIkke = saksStatus === SaksStatus.BEHANDLES_IKKE;
                             const soknadBehandlesIkke = status === SoknadsStatusEnum.BEHANDLES_IKKE;
                             return (
-                                <StyledStatusBox key={index}>
-                                    <StyledStatusMessage>
-                                        <Label>{statusdetalj.tittel}</Label>
-                                        {!(sakBehandlesIkke || sakIkkeInnsyn) && (
-                                            <>
-                                                {saksStatus === SaksStatus.FERDIGBEHANDLET && (
-                                                    <Tag variant="success">
-                                                        <FormattedMessage id={hentSaksStatusTittel(saksStatus)} />
-                                                    </Tag>
-                                                )}
-                                                {saksStatus === SaksStatus.UNDER_BEHANDLING && (
-                                                    <Tag variant="warning">
-                                                        <FormattedMessage id={hentSaksStatusTittel(saksStatus)} />
-                                                    </Tag>
-                                                )}
-                                            </>
+                                <>
+                                    <StatusBox key={index}>
+                                        <StatusMessage>
+                                            <Label>{statusdetalj.tittel}</Label>
+                                            {!(sakBehandlesIkke || sakIkkeInnsyn) && (
+                                                <>
+                                                    {saksStatus === SaksStatus.FERDIGBEHANDLET && (
+                                                        <Tag variant="success">
+                                                            <FormattedMessage id={hentSaksStatusTittel(saksStatus)} />
+                                                        </Tag>
+                                                    )}
+                                                    {saksStatus === SaksStatus.UNDER_BEHANDLING && (
+                                                        <Tag variant="warning">
+                                                            <FormattedMessage id={hentSaksStatusTittel(saksStatus)} />
+                                                        </Tag>
+                                                    )}
+                                                </>
+                                            )}
+                                        </StatusMessage>
+                                        {statusdetalj.melding && statusdetalj.melding.length > 0 && (
+                                            <div>
+                                                <BodyShort>{statusdetalj.melding}</BodyShort>
+                                            </div>
                                         )}
-                                    </StyledStatusMessage>
-                                    {statusdetalj.melding && statusdetalj.melding.length > 0 && (
-                                        <div>
-                                            <BodyShort>{statusdetalj.melding}</BodyShort>
-                                        </div>
-                                    )}
-                                    {sakBehandlesIkke && !soknadBehandlesIkke && (
-                                        <div>
-                                            <BodyShort>
-                                                <FormattedMessage id="status.sak_behandles_ikke_ingress" />
-                                            </BodyShort>
-                                        </div>
-                                    )}
-                                    {sakIkkeInnsyn && !soknadBehandlesIkke && (
-                                        <div>
-                                            <BodyShort>
-                                                <FormattedMessage id="status.ikke_innsyn_ingress" />
-                                            </BodyShort>
-                                        </div>
-                                    )}
+                                        {sakBehandlesIkke && !soknadBehandlesIkke && (
+                                            <div>
+                                                <BodyShort>
+                                                    <FormattedMessage id="status.sak_behandles_ikke_ingress" />
+                                                </BodyShort>
+                                            </div>
+                                        )}
+                                        {sakIkkeInnsyn && !soknadBehandlesIkke && (
+                                            <div>
+                                                <BodyShort>
+                                                    <FormattedMessage id="status.ikke_innsyn_ingress" />
+                                                </BodyShort>
+                                            </div>
+                                        )}
 
-                                    {statusdetalj.vedtaksfilUrlList &&
-                                        statusdetalj.vedtaksfilUrlList.map((hendelse: VedtakFattet, index: number) => (
-                                            <StyledStatusMessage key={index}>
-                                                <StyledStatusMessageVedtak>
-                                                    <EksternLenke
-                                                        href={"" + hendelse.vedtaksfilUrl}
-                                                        target="_blank"
-                                                        onClick={onVisVedtak}
-                                                    >
-                                                        Vedtak (
-                                                        <DatoOgKlokkeslett bareDato={true} tidspunkt={hendelse.dato} />)
-                                                    </EksternLenke>
-                                                </StyledStatusMessageVedtak>
-                                            </StyledStatusMessage>
-                                        ))}
-                                </StyledStatusBox>
+                                        {statusdetalj.vedtaksfilUrlList &&
+                                            statusdetalj.vedtaksfilUrlList.map(
+                                                (hendelse: VedtakFattet, index: number) => (
+                                                    <StatusMessage key={index}>
+                                                        <StatusMessageVedtak>
+                                                            <EksternLenke
+                                                                href={"" + hendelse.vedtaksfilUrl}
+                                                                target="_blank"
+                                                                onClick={onVisVedtak}
+                                                            >
+                                                                Vedtak (
+                                                                <DatoOgKlokkeslett
+                                                                    bareDato={true}
+                                                                    tidspunkt={hendelse.dato}
+                                                                />
+                                                                )
+                                                            </EksternLenke>
+                                                        </StatusMessageVedtak>
+                                                    </StatusMessage>
+                                                )
+                                            )}
+                                    </StatusBox>
+                                    <ContentPanelBorder lightColor />
+                                </>
                             );
                         })}
-                </StyledPanelContent>
-            </StyledPanel>
-        </StyledContainer>
+                </ContentPanelBody>
+            </ContentPanel>
+        </Container>
     );
 };
 
