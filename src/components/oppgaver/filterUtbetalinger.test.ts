a8d8a5fb042ad0e9dab2f1496b10f74093a6b50d
jorel <jorgen.elden.lindgren@nav.no>
2022-03-30 09:12:52 +0200

    alle Vilkår og dokumentasjonskrav fjernes hvis alle utb.ref de har er passert utbetalingsperioden med mer enn 21 dager

--- src/components/oppgaver/filterUtbetalinger.test.ts
+++ src/components/oppgaver/filterUtbetalinger.test.ts
@@ -229,31 +229,24 @@ test("viser alle dok.krav hvis en mangler utb.ref", () => {
             dokumentasjonkravId: "1",
             dokumentasjonkravElementer: [
                 {
+                    hendelsetidspunkt: "",
                     hendelsetype: undefined,
                     dokumentasjonkravReferanse: "dokumentasjonkravReferanse",
-                    status: "UTBETALT",
+                    status: "RELEVANT",
                     utbetalingsReferanse: ["utbetalingsReferanse1"],
                 },
-            ],
-        },
-        {
-            dokumentasjonkravId: "2",
-            dokumentasjonkravElementer: [
                 {
+                    hendelsetidspunkt: "",
                     hendelsetype: undefined,
                     dokumentasjonkravReferanse: "dokumentasjonkravReferanse",
-                    status: "UTBETALT",
+                    status: "RELEVANT",
                     utbetalingsReferanse: ["utbetalingsReferanse2"],
                 },
-            ],
-        },
-        {
-            dokumentasjonkravId: "3",
-            dokumentasjonkravElementer: [
                 {
+                    hendelsetidspunkt: "",
                     hendelsetype: undefined,
                     dokumentasjonkravReferanse: "dokumentasjonkravReferanse",
-                    status: "UTBETALT",
+                    status: "RELEVANT",
                     utbetalingsReferanse: [],
                 },
             ],
@@ -302,4 +295,45 @@ test("viser alle vilkår hvis en mangler utb.ref", () => {
     const todaysDate = new Date("2022-03-31");
     const filtrerteUtbetalinger = filterVilkar(vilkar, saksutbetalinger, todaysDate);
     expect(filtrerteUtbetalinger).toBe(vilkar);
+});
+
+test("hvorfor ting fjernes", () => {
+    const dokumentasjonkrav: DokumentasjonKrav[] = [
+        {
+            dokumentasjonkravElementer: [
+                {
+                    hendelsetype: undefined,
+                    dokumentasjonkravReferanse: "f4rfd2c9-tmvy-3v9a-gund-mgam3ijm4plk",
+                    status: "RELEVANT",
+                    utbetalingsReferanse: ["2o9g9avk-kasl-1nja-d3s4-1j7knpdrjhuo"],
+                },
+                {
+                    hendelsetype: undefined,
+                    dokumentasjonkravReferanse: "42zzrcjz-39fk-qwgx-0uk2-qttnm4c73xu6",
+                    status: "RELEVANT",
+                    utbetalingsReferanse: ["gln4440d-azzt-bqcv-cpra-ur50f8hawfy9"],
+                },
+            ],
+            dokumentasjonkravId: "d3f9e91299c2f5635ca4669c0605a4e745f3eda833477cf1039cc9eb48dd0ff2",
+        },
+    ];
+
+    const utbetalinger: SaksUtbetaling = {
+        "2o9g9avk-kasl-1nja-d3s4-1j7knpdrjhuo": {
+            status: "UTBETALT",
+            fom: "2022-02-01",
+            tom: "2022-02-28",
+            utbetlingsreferanse: "2o9g9avk-kasl-1nja-d3s4-1j7knpdrjhuo",
+        },
+        "gln4440d-azzt-bqcv-cpra-ur50f8hawfy9": {
+            status: "UTBETALT",
+            fom: "2022-05-23",
+            tom: "2022-06-13",
+            utbetlingsreferanse: "gln4440d-azzt-bqcv-cpra-ur50f8hawfy9",
+        },
+    };
+
+    const todaysDate = new Date("2022-03-30");
+    const filterDokkrav = filterDokumentasjonkrav(dokumentasjonkrav, utbetalinger, todaysDate);
+    expect(filterDokkrav).toBe(dokumentasjonkrav);
 });
