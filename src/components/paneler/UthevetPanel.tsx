1c944c122aebfaaac063068d35d548a72e9657da
Ida <25682301+idaons@users.noreply.github.com>
2022-08-01 08:36:19 +0200

    fikser styling på feilsider (#463)

--- src/components/paneler/UthevetPanel.tsx
+++ src/components/paneler/UthevetPanel.tsx
@@ -7,18 +7,12 @@ export const UthevetPanel = styled(Panel)`
 
     @media screen and (min-width: 641px) {
         padding-top: 2rem;
-
-        .panel-uthevet-luft-under {
-            padding-bottom: 4rem;
-        }
+        padding-bottom: 2rem;
     }
 
     @media screen and (max-width: 640px) {
         padding-top: 1rem;
-
-        .panel-uthevet-luft-under {
-            padding-bottom: 1rem;
-        }
+        padding-bottom: 1rem;
     }
 `;
 
