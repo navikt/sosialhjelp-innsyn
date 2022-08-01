f2b904e9deab3973baab564fe38e1d15430091d4
Ida Onshus <25682301+idaons@users.noreply.github.com>
2022-08-01 09:31:15 +0200

    rydder i css og fikser feil syntax

--- src/components/Infopanel/InfoPanel.tsx
+++ src/components/Infopanel/InfoPanel.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-import "./infoPanel.css";
 import {LinkPanel} from "@navikt/ds-react";
+import styled from "styled-components";
 
-const InfoPanelContainer: React.FC<{children: React.ReactNode; className?: string}> = ({children, className}) => {
-    return <nav className={"infopanel_container " + className}>{children}</nav>;
-};
+const WRAP_WIDTH = "42em";
+export const InfoPanelWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: stretch;
+    gap: 1rem;
+    @media screen and (max-width: ${WRAP_WIDTH}) {
+        flex-direction: column;
+    }
+`;
 
+const StyledLinkPanel = styled(LinkPanel)`
+    align-items: stretch;
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+    flex-direction: column;
+
+    .navds-link-panel__chevron {
+        align-self: center;
+    }
+
+    @media screen and (max-width: ${WRAP_WIDTH}) {
+        width: 100%;
+        flex-direction: row;
+    }
+`;
 type Props = {
     children: React.ReactNode;
     tittel: React.ReactNode;
@@ -14,14 +38,11 @@ type Props = {
 
 const InfoPanel: React.FC<Props> = ({children, tittel, href}) => {
     return (
-        <LinkPanel href={href} className="infopanel" border={false}>
-            <div>
-                <LinkPanel.Title>{tittel}</LinkPanel.Title>
-                <LinkPanel.Description>{children}</LinkPanel.Description>
-            </div>
-        </LinkPanel>
+        <StyledLinkPanel href={href} border={false}>
+            <LinkPanel.Title>{tittel}</LinkPanel.Title>
+            <LinkPanel.Description>{children}</LinkPanel.Description>
+        </StyledLinkPanel>
     );
 };
 
-export {InfoPanelContainer};
 export default InfoPanel;
