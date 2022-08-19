ae09be23213e32502969700c8d98d5ef2697e3c9
Ida Onshus <25682301+idaons@users.noreply.github.com>
2022-08-19 12:19:29 +0200

    styler vedleggitems og feilmelding, skriver om til styledcomponents

--- src/components/oppgaver/FileItemView.tsx
+++ src/components/oppgaver/FileItemView.tsx
@@ -1,15 +1,86 @@
 import React, {useState} from "react";
 import PaperClipSlanted from "../ikoner/PaperClipSlanted";
-import TrashBin from "../ikoner/TrashBin";
 import {Fil} from "../../redux/innsynsdata/innsynsdataReducer";
 import {formatBytes} from "../../utils/formatting";
 import VedleggModal from "./VedleggModal";
 import {FormattedMessage} from "react-intl";
 import {REST_STATUS} from "../../utils/restUtils";
-import {Button, Label, Link} from "@navikt/ds-react";
+import {BodyShort, Button, Label, Link} from "@navikt/ds-react";
 import {ErrorMessage} from "../errors/ErrorMessage";
+import styled from "styled-components/macro";
+import {Delete} from "@navikt/ds-icons";
 
 type ClickEvent = React.MouseEvent<HTMLAnchorElement, MouseEvent> | React.MouseEvent<HTMLButtonElement, MouseEvent>;
+
+const MOBILE_MAXWIDTH = "40em";
+const StyledErrorMessage = styled(ErrorMessage)`
+    margin: 0.5rem 0 0.5rem;
+
+    @media screen and (max-width: ${MOBILE_MAXWIDTH}) {
+        margin: 0.5rem 0;
+    }
+`;
+
+const Wrapper = styled.div`
+    margin: 1rem 0;
+    max-width: 100%;
+    min-height: 38px;
+    padding-bottom: 1px;
+    border-bottom: 1px solid #c6c2bf;
+`;
+
+const StyledFilInfoOgKnapp = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+`;
+
+const StyledFilInfo = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    min-width: 0;
+    //flex-shrink: 1;
+    //flex-grow: 0;
+    //max-width: 66%;
+    white-space: nowrap;
+    flex-wrap: wrap;
+
+    .filikon {
+        height: 20px;
+        width: 20px;
+        margin-right: 8px;
+        margin-bottom: -2px;
+    }
+
+    a.filnavn {
+        display: inline;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        //min-width: 50%;
+        max-width: 20rem;
+    }
+
+    @media screen and (max-width: 1230px) {
+        a.filnavn {
+            // max-width: 25vw;
+        }
+    }
+`;
+
+const StyledDeleteButton = styled(Button)`
+    display: inline-block;
+    white-space: nowrap;
+    margin-left: auto;
+    @media screen and (max-width: ${MOBILE_MAXWIDTH}) {
+        svg {
+            display: none;
+        }
+    }
+`;
 
 const FileItemView: React.FC<{
     fil: Fil;
@@ -25,40 +96,45 @@ const FileItemView: React.FC<{
     };
 
     return (
-        <div className="vedlegg_liste_element" id={"app"}>
-            <div className="innhold">
-                <div className="filnavn_lenkeboks">
+        <Wrapper>
+            <StyledFilInfoOgKnapp>
+                <StyledFilInfo>
                     {fil.file && (
                         <VedleggModal file={fil.file} onRequestClose={() => setModalVises(false)} synlig={modalVises} />
                     )}
+                    <>
+                        <Link
+                            href="#"
+                            className="filnavn"
+                            onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => onVisVedlegg(event)}
+                        >
+                            <PaperClipSlanted className="filikon" />
 
-                    <PaperClipSlanted className="filikon" />
-                    <Link
-                        href="#"
-                        className="filnavn"
-                        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => onVisVedlegg(event)}
-                    >
-                        {fil.filnavn}
-                    </Link>
-                    <span className="filstorrelse">({storrelse})</span>
-                </div>
-                <div className="fjern_lenkeboks">
-                    <Button variant="tertiary" size="small" onClick={(event) => onDelete(event, fil)}>
-                        <Label>
-                            <FormattedMessage id="vedlegg.fjern" />
-                        </Label>
-                        <TrashBin className="klikkbar_soppelboette" />
-                    </Button>
-                </div>
-            </div>
+                            {fil.filnavn}
+                        </Link>
+                    </>
+                    <BodyShort as="span" className="filstorrelse">
+                        ({storrelse})
+                    </BodyShort>
+                </StyledFilInfo>
+                <StyledDeleteButton variant="tertiary" size="small" onClick={(event: any) => onDelete(event, fil)}>
+                    <Label>
+                        <FormattedMessage id="vedlegg.fjern" />
+                    </Label>
+                    <Delete />
+                </StyledDeleteButton>
+            </StyledFilInfoOgKnapp>
             {fil.status !== REST_STATUS.INITIALISERT &&
                 fil.status !== REST_STATUS.PENDING &&
                 fil.status !== REST_STATUS.OK && (
-                    <ErrorMessage className="oppgaver_vedlegg_feilmelding_rad">
-                        <FormattedMessage id={"vedlegg.opplasting_feilmelding_" + fil.status} />
-                    </ErrorMessage>
+                    <StyledErrorMessage>
+                        <FormattedMessage
+                            id={"vedlegg.opplasting_feilmelding_" + fil.status}
+                            defaultMessage={"Vi klarte dessverre ikke lese filen din. Årsaken er ukjent."}
+                        />
+                    </StyledErrorMessage>
                 )}
-        </div>
+        </Wrapper>
     );
 };
 
