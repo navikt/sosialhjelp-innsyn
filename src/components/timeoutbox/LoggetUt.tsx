b9c07fb911604c8a5a551b3472243d8fb600a174
Royce Nokham <royce.nokham@nav.no>
2022-10-06 09:46:18 +0200

    Legger til ny modal for timeout

---
+++ src/components/timeoutbox/LoggetUt.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import {FormattedMessage} from "react-intl";
+import {Innholdstittel, Normaltekst} from "nav-frontend-typografi";
+import {Button} from "@navikt/ds-react";
+
+interface Props {
+    onLoginAgainClick: () => void;
+}
+
+const LoggetUt = (props: Props) => {
+    return (
+        <div>
+            <Innholdstittel className="blokk-s timeoutbox__overskrift">
+                <FormattedMessage id="timeout.overskrift" />
+            </Innholdstittel>
+            <Normaltekst className="blokk-xxs">
+                <FormattedMessage id="timeout.utlopt" />
+            </Normaltekst>
+            <div className="timeoutbox__knapperad">
+                <Button variant="primary" onClick={props.onLoginAgainClick}>
+                    <FormattedMessage id="timeout.logginn" />
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default LoggetUt;
