4d110bc5fd330a19397d2931e46b65ed20935a3b
Ida Onshus <25682301+idaons@users.noreply.github.com>
2023-01-02 08:52:15 +0100

    fiks test

--- src/components/feilside/Feilside.tsx
+++ src/components/feilside/Feilside.tsx
@@ -10,7 +10,7 @@ import {FormattedMessage} from "react-intl";
 import styled from "styled-components/macro";
 import {setBreadcrumbs} from "../../utils/breadcrumbs";
 
-const FeilsideWrapper = styled.div`
+const FeilsideWrapper = styled.div.attrs({className: "blokk-center"})`
     margin-top: 2rem;
 `;
 export interface FeilsideProps {
--- src/components/linkside/Linkside.test.tsx
+++ src/components/linkside/Linkside.test.tsx
@@ -1,13 +1,13 @@
 import {getRedirectUrl} from "./Linkside";
 
 describe("getRedirectUrl", () => {
-    it("hentSaksStatusTittel should return correct title key for UNDER_BEHANDLING", () => {
+    it("getRedirectUrl should return correct url after login", () => {
         expect(getRedirectUrl("?goto=/sosialhjelp/innsyn/link&login_id=idporten_authentication_error")).toEqual(
-            "/innsyn/link"
+            "/link"
         );
     });
 
-    it("hentSaksStatusTittel should return correct title key for UNDER_BEHANDLING", () => {
-        expect(getRedirectUrl("")).toEqual("/innsyn");
+    it("getRedirectUrl should return correct url without any parameters", () => {
+        expect(getRedirectUrl("")).toEqual("/");
     });
 });
