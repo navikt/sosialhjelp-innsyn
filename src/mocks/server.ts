7e9dd5265ca85cd40794d8b078e1f63e85030e69
Martin Almvik <martin.almvik@nav.no>
2023-01-16 18:17:47 +0100

    Flytter over /utbetalinger til react-query

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,7 +1,3 @@
 import {setupServer} from "msw/node";
 
-import {getHendelseControllerMSW} from "../generated/hendelse-controller/hendelse-controller.msw";
-import {getKommuneControllerMSW} from "../generated/kommune-controller/kommune-controller.msw";
-
-const handlers = [...getKommuneControllerMSW(), ...getHendelseControllerMSW()];
-export const server = setupServer(...handlers);
+export const server = setupServer();
