69e4bf9e2e6f09557e9217de199ce7d2c988d3bb
Ida Onshus <25682301+idaons@users.noreply.github.com>
2023-02-10 16:21:05 +0100

    fikser klassenavn

--- src/components/oppgaver/vilkar/VilkarView.tsx
+++ src/components/oppgaver/vilkar/VilkarView.tsx
@@ -9,9 +9,9 @@ interface Props {
 
 export const VilkarView: React.FC<Props> = ({vilkar}) => {
     return (
-        <ul className={styles.oppgaver_liste}>
+        <ul className={styles.vilkar_liste}>
             {vilkar.map((element, index) => (
-                <li className={styles.oppgaver_detaljer} key={element.vilkarReferanse}>
+                <li className={styles.vilkar_detaljer} key={element.vilkarReferanse}>
                     <Label as="p">{element.tittel}</Label>
                     {element.beskrivelse && <BodyShort>{element.beskrivelse}</BodyShort>}
                 </li>
