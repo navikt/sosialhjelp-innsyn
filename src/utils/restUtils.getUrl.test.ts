445cce646e94ffc501432242ab615d3c1ba0c204
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-03-03 17:56:13 +0100

    fix test. bruker login-api i "midlertidig" ikke-nais løsning i dev-sbs og prod-sbs

--- src/utils/restUtils.getUrl.test.ts
+++ src/utils/restUtils.getUrl.test.ts
@@ -32,17 +32,17 @@ describe("getBaseUrl", () => {
             "http://localhost:8989/sosialhjelp/mock-alt-api/login-api/sosialhjelp/innsyn-api/api/v1"
         );
         //validateGetBaseUrl(localhostOrigins, "http://localhost:8080/sosialhjelp/innsyn-api/api/v1");//for idporten testing
-        validateGetBaseUrl(devSbsOrigins, "https://www-q0.dev.nav.no/sosialhjelp/innsyn-api/api/v1");
+        validateGetBaseUrl(devSbsOrigins, "https://www-q0.dev.nav.no/sosialhjelp/login-api/innsyn-api/api/v1");
         validateGetBaseUrl(devOrigins, "https://digisos.dev.nav.no/sosialhjelp/innsyn-api/api/v1");
         validateGetBaseUrl(
             mockOrigins,
             "https://digisos.ekstern.dev.nav.no/sosialhjelp/mock-alt-api/login-api/sosialhjelp/innsyn-api/api/v1"
         );
-        validateGetBaseUrl(prodNavnoOrigins, "https://www.nav.no/sosialhjelp/innsyn-api/api/v1");
+        validateGetBaseUrl(prodNavnoOrigins, "https://www.nav.no/sosialhjelp/login-api/innsyn-api/api/v1");
     });
 
     it("should default to prod when url is unknown correct backend-url", () => {
-        validateGetBaseUrl(unknownOrigins, "https://www.nav.no/sosialhjelp/innsyn-api/api/v1");
+        validateGetBaseUrl(unknownOrigins, "https://www.nav.no/sosialhjelp/login-api/innsyn-api/api/v1");
     });
 
     function validateGetBaseUrl(origins: string[], expected: string) {
