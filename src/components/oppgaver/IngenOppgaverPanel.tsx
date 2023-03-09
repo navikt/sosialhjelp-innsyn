95c305bfa67c24372d2f25f5c6f324337bc06e1d
Martin Almvik <martin.almvik@nav.no>
2023-03-09 15:32:10 +0100

    Kjører prettier

--- src/components/oppgaver/IngenOppgaverPanel.tsx
+++ src/components/oppgaver/IngenOppgaverPanel.tsx
@@ -18,27 +18,27 @@ interface Props {
 }
 
 const IngenOppgaverPanel: React.FC<Props> = ({leserData}) => {
-  const {t} = useTranslation();
+    const {t} = useTranslation();
     if (leserData) {
         return null;
     }
     return (
-      <StyledPanel>
-        <>
-          <Task width="1.5rem" height="1.5rem" style={{marginTop: "6px"}} aria-hidden title="oppgave" />
-          <div>
-            <Label as="p">{t("oppgaver.ingen_oppgaver")}</Label>
-            <BodyShort>{t("oppgaver.beskjed")}</BodyShort>
-          </div>
-        </>
-        <>
-          <Attachment width="1.5rem" height="1.5rem" style={{marginTop: "6px"}} aria-hidden title="vedlegg" />
-          <div>
-            <Label as="p">{t("oppgaver.andre_dokumenter")}</Label>
-            <BodyShort>{t("oppgaver.andre_dokumenter_beskjed")}</BodyShort>
-          </div>
-        </>
-      </StyledPanel>
+        <StyledPanel>
+            <>
+                <Task width="1.5rem" height="1.5rem" style={{marginTop: "6px"}} aria-hidden title="oppgave" />
+                <div>
+                    <Label as="p">{t("oppgaver.ingen_oppgaver")}</Label>
+                    <BodyShort>{t("oppgaver.beskjed")}</BodyShort>
+                </div>
+            </>
+            <>
+                <Attachment width="1.5rem" height="1.5rem" style={{marginTop: "6px"}} aria-hidden title="vedlegg" />
+                <div>
+                    <Label as="p">{t("oppgaver.andre_dokumenter")}</Label>
+                    <BodyShort>{t("oppgaver.andre_dokumenter_beskjed")}</BodyShort>
+                </div>
+            </>
+        </StyledPanel>
     );
 };
 
