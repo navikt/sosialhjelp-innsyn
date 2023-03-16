562e5ba180428bfcc4cdcf73afd7fe6caa4be15b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-03-16 12:30:35 +0100

    test

--- src/utils/restUtils.ts
+++ src/utils/restUtils.ts
@@ -325,14 +325,14 @@ export function getRedirectPath(loginUrl: string, id: string): string {
     const redirectOrigin = getRedirectOrigin();
     console.log("loginUrl: " + loginUrl);
     console.log("redirectOrigin: " + redirectOrigin);
-    if (loginUrl.indexOf("loginservice") === -1) {
-        // Ikke loginservice:
-        return "goto=" + redirectOrigin + window.location.pathname;
-    } else {
+    if (loginUrl.indexOf("digisos.dev.nav.no") === -1) {
         const gotoParameter = "goto=" + window.location.pathname;
         console.log("gotoParameter: " + gotoParameter);
         const redirectPath = redirectOrigin + "/sosialhjelp/innsyn/link?" + gotoParameter;
         console.log("redirectPath: " + redirectPath);
         return "redirect=" + redirectPath + "%26login_id=" + id;
+    } else {
+        // Ikke loginservice:
+        return "goto=" + redirectOrigin + window.location.pathname;
     }
 }
