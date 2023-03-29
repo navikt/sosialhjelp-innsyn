43babe277dc60a59f506654a909257010ba3e868
Ida Onshus <25682301+idaons@users.noreply.github.com>
2023-03-29 14:40:12 +0200

    locale

--- src/components/filopplasting/VedleggSuccess.tsx
+++ src/components/filopplasting/VedleggSuccess.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import {Alert} from "@navikt/ds-react";
+import {useTranslation} from "react-i18next";
 
 const StyledAlert = styled(Alert)`
     margin-top: 1rem;
 `;
 
 const VedleggSuccess = ({show}: {show: boolean}) => {
-    return show ? <StyledAlert variant="success">Takk! Dokumentasjonen er nå sendt inn.</StyledAlert> : null;
+    const {t} = useTranslation();
+    return show ? <StyledAlert variant="success">{t("vedlegg.suksess")}</StyledAlert> : null;
 };
 export default VedleggSuccess;
