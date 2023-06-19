483e9d6c1df36608b5abedb59641d3f7da46a5e0
Line Moseng <line.moseng@nav.no>
2023-06-19 14:01:14 +0200

    sammenligner på enum

--- src/components/historikk/Historikk.tsx
+++ src/components/historikk/Historikk.tsx
@@ -81,7 +81,7 @@ const HistorikkListe: React.FC<HistorikkListeProps> = ({hendelser, className, le
                 </BodyShort>
             );
         }
-        if (historikkEnumKey === "hendelse.sak_under_behandling_med_tittel" && tekstArgument) {
+        if (historikkEnumKey === HistorikkTekstEnum.SAK_UNDER_BEHANDLING_MED_TITTEL && tekstArgument) {
             return (
                 <BodyShort>
                     {t(enumValue, {tekstArgument: tekstArgument.charAt(0).toUpperCase() + tekstArgument.slice(1)})}
