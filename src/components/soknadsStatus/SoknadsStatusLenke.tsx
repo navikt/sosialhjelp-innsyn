ecfdc5af51d9396da166274a8ead2c75fad4bc0b
Ida Onshus <25682301+idaons@users.noreply.github.com>
2023-06-27 12:26:03 +0200

    fler språknøkler

--- src/components/soknadsStatus/SoknadsStatusLenke.tsx
+++ src/components/soknadsStatus/SoknadsStatusLenke.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import {Link} from "@navikt/ds-react";
 import {SoknadsStatusResponseStatus} from "../../generated/model";
+import {useTranslation} from "react-i18next";
 
 const SoknadsStatusLenke = (props: {status: SoknadsStatusResponseStatus | undefined}) => {
+    const {t} = useTranslation();
     switch (props.status) {
         case SoknadsStatusResponseStatus.SENDT:
         case SoknadsStatusResponseStatus.MOTTATT:
         case SoknadsStatusResponseStatus.UNDER_BEHANDLING:
             return (
                 <Link href="https://www.nav.no/okonomisk-sosialhjelp#saksbehandlingstider">
-                    Hvor lang tid tar det å behandle søknaden?
+                    {t("lenke.saksbehandling")}
                 </Link>
             );
 
         case SoknadsStatusResponseStatus.FERDIGBEHANDLET:
-            return <Link href="https://www.nav.no/okonomisk-sosialhjelp#klage">Hvordan sende klage?</Link>;
+            return <Link href="https://www.nav.no/okonomisk-sosialhjelp#klage">{t("lenke.klage")}</Link>;
     }
     return null;
 };
