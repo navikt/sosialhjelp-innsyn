39e76ce37a0641d4ebbb4fe700cd98e2b7bad06b
Martin Almvik <martin.almvik@nav.no>
2023-09-12 09:56:12 +0200

    WIP: Wonderwall

--- src/middleware.ts
+++ src/middleware.ts
@@ -44,24 +44,33 @@ export async function middleware(request: NextRequest) {
     }
 
     // Router bruker til login hvis vi får 401
-    try {
-        logger.info("Kaller /tilgang i innsyn-api");
-        const harTilgangResponse = await fetch(process.env.NEXT_INNSYN_API_BASE_URL + "/api/v1/innsyn/tilgang", {
-            headers: new Headers(request.headers),
-            credentials: "include",
-        });
-        logger.info(`Fikk ${harTilgangResponse.status} på kall til /tilgang`);
-        if (harTilgangResponse.status === 401) {
-            const json: AzureAdAuthenticationError = await harTilgangResponse.json();
-            const queryDivider = json.loginUrl.includes("?") ? "&" : "?";
+    if (process.env.NEXT_USE_WONDERWALL === "false") {
+        try {
+            logger.debug("Kaller /tilgang i innsyn-api");
+            const harTilgangResponse = await fetch(process.env.NEXT_INNSYN_API_BASE_URL + "/api/v1/innsyn/tilgang", {
+                headers: new Headers(request.headers),
+                credentials: "include",
+            });
+            logger.debug(`Fikk ${harTilgangResponse.status} på kall til /tilgang`);
+            if (harTilgangResponse.status === 401) {
+                const json: AzureAdAuthenticationError = await harTilgangResponse.json();
+                const queryDivider = json.loginUrl.includes("?") ? "&" : "?";
 
-            const redirectUrl = getRedirect(json.loginUrl, pathname, process.env.NEXT_INNSYN_REDIRECT_ORIGIN!, json.id);
-            logger.info(`Sender bruker til login: ${json.loginUrl + queryDivider + redirectUrl}`);
-            return NextResponse.redirect(json.loginUrl + queryDivider + redirectUrl);
+                const redirectUrl = getRedirect(
+                    json.loginUrl,
+                    pathname,
+                    process.env.NEXT_INNSYN_REDIRECT_ORIGIN!,
+                    json.id
+                );
+                logger.debug(`Sender bruker til login: ${json.loginUrl + queryDivider + redirectUrl}`);
+                return NextResponse.redirect(json.loginUrl + queryDivider + redirectUrl);
+            }
+        } catch (e) {
+            logger.warn("Feil i middleware fetch, sender bruker til 500");
+            return NextResponse.redirect(process.env.NEXT_INNSYN_REDIRECT_ORIGIN + "/sosialhjelp/innsyn/500");
         }
-    } catch (e) {
-        logger.warn("Feil i middleware fetch, sender bruker til 500");
-        return NextResponse.redirect(process.env.NEXT_INNSYN_REDIRECT_ORIGIN + "/sosialhjelp/innsyn/500");
+    } else {
+        logger.debug("Wonderwall er skrudd på. Skipper sjekk mot /tilgang");
     }
 }
 
