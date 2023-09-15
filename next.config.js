74ffdfb5f8169efa0761130d10feb5b396cfc2f8
Ida Onshus <25682301+idaons@users.noreply.github.com>
2023-09-15 13:14:18 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -51,6 +51,7 @@ const nextConfig = {
             return [
                 {
                     source: "/:path*",
+                    headers: [],
                 },
             ];
         }
