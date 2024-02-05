898aea7ddb903029093c7b081a1eb3ed4827f14f
jorel <jorgen.elden.lindgren@nav.no>
2024-02-05 14:44:25 +0100

    wip

--- src/utbetalinger/beta/tabs/UtbetalingAccordionItem.tsx
+++ src/utbetalinger/beta/tabs/UtbetalingAccordionItem.tsx
@@ -26,12 +26,12 @@ interface Props {
 }
 
 export const erDetteAapnet = (dagensDato: Date, utbetalingsdato?: string) => {
-    /** feb -         02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 - dato fremover - feb */
-    //                00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 - dager
-    /** feb til jan - 02 01 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 - dato tilbake - feb til jan*/
+    /** feb -         05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 |20| 21 22 23 - dato fremover - feb */
+    //                00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 |15| - dager
+    /** feb til jan - 05 04 03 02 01 31 30 29 28 27 26 25 24 23 22 |21| 20 19 18 - dato tilbake - feb til jan*/
 
     if (utbetalingsdato == "") return false;
-    const utbetalingsDato: Date = utbetalingsdato && new Date(new Date(utbetalingsdato).getTime());
+    const utbetalingsDato: Date = new Date(new Date(utbetalingsdato).getTime()) as Date;
     //const utbetalingsDato: Date = utbetalingsdato && new Date(utbetalingsdato);
 
     const femtenDagerSidenDate: Date = new Date(dagensDato.getTime() - 15 * 24 * 60 * 60 * 1000);
