e2995665466b2af80454cc687a3ce85eabd37dab
Martin <martinalmvik@gmail.com>
2024-02-09 14:14:01 +0100

    Bruker forfallsdato til kommende utbetaling hvis vi ikke har utbetali… (#872)
    * Bruker forfallsdato til kommende utbetaling hvis vi ikke har utbetalingsdato
    
    * Ta ned banner som forteller om problemer ved kommende utbetalinger

--- src/utbetalinger/beta/filter/useFiltrerteUtbetalinger.ts
+++ src/utbetalinger/beta/filter/useFiltrerteUtbetalinger.ts
@@ -19,23 +19,20 @@ export const filterMatch = (utbetaling: ManedUtbetaling, filter: FilterKey) => {
         matchMottaker = !utbetaling.annenMottaker;
     }
 
-    // Hvis vi ikke har dato-filter eller utbetalingsdato, trenger vi ikke sjekke datofilteret.
-    if (!utbetaling.utbetalingsdato || (!filter.tilDato && !filter.fraDato)) return matchMottaker;
+    // Hvis vi ikke har dato-filter eller utbetalingsdato/forfallsdato, trenger vi ikke sjekke datofilteret.
+    if ((!utbetaling.utbetalingsdato && !utbetaling.forfallsdato) || (!filter.tilDato && !filter.fraDato))
+        return matchMottaker;
 
-    const utbetalingsDato = stringToDateWithoutTimezone(utbetaling.utbetalingsdato);
-    let matchFra = filter.fraDato
-        ? isAfter(utbetalingsDato, filter.fraDato) || isEqual(utbetalingsDato, filter.fraDato)
-        : true;
-    let matchTil = filter.tilDato
-        ? isBefore(utbetalingsDato, filter.tilDato) || isEqual(utbetalingsDato, filter.tilDato)
-        : true;
+    const dato = stringToDateWithoutTimezone(utbetaling.utbetalingsdato ?? utbetaling.forfallsdato!);
+    let matchFra = filter.fraDato ? isAfter(dato, filter.fraDato) || isEqual(dato, filter.fraDato) : true;
+    let matchTil = filter.tilDato ? isBefore(dato, filter.tilDato) || isEqual(dato, filter.tilDato) : true;
 
     return matchMottaker && matchTil && matchFra;
 };
 const useFiltrerteUtbetalinger = (utbetalinger: UtbetalingerResponseMedId[]) => {
     const {filter} = useFilter();
 
-    const filtrerteUtbetalte = React.useMemo(() => {
+    return React.useMemo(() => {
         return utbetalinger
             .map((response) => {
                 const filtrertPerManed = response.utbetalingerForManed.filter((utbetaling) => {
@@ -45,8 +42,6 @@ const useFiltrerteUtbetalinger = (utbetalinger: UtbetalingerResponseMedId[]) => 
             })
             .filter((response) => response.utbetalingerForManed.length > 0);
     }, [utbetalinger, filter]);
-
-    return filtrerteUtbetalte;
 };
 
 export default useFiltrerteUtbetalinger;
