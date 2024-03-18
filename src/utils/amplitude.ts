7a38eaeb327183d90d02299921616b2e661b66e3
jorel <jorgen.elden.lindgren@nav.no>
2024-03-18 11:46:01 +0100

    wip
    Co-authored-by: Åshild Lobben Simonsen <ashild.lobben.simonsen@nav.no>

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -86,10 +86,10 @@ export const logSakBehandlingsTidUtenTittel = (hendelser: HendelseResponse[]) =>
     let groupSaksBasedOnSaksReferanse;
     if (hendelser && Object.keys(hendelser).length > 0) {
         groupSaksBasedOnSaksReferanse = hendelser.reduce((group: {[key: string]: HendelseResponse[]}, item, num) => {
-            if (!group[item.saksReferanse]) {
+            if (item && !group[item.saksReferanse]) {
                 group[item.saksReferanse] = [];
             }
-            group[item.saksReferanse].push(item);
+            item && group[item.saksReferanse].push(item);
             return group;
         }, {});
     }
@@ -138,10 +138,10 @@ export const logSakBehandlingsTidMedTittel = (hendelser: HendelseResponse[]) => 
     let groupSaksBasedOnSaksReferanse;
     if (hendelser && Object.keys(hendelser).length > 0) {
         groupSaksBasedOnSaksReferanse = hendelser.reduce((group: {[key: string]: HendelseResponse[]}, item, num) => {
-            if (!group[item.saksReferanse]) {
+            if (item && !group[item.saksReferanse]) {
                 group[item.saksReferanse] = [];
             }
-            group[item.saksReferanse].push(item);
+            item && group[item.saksReferanse].push(item);
             return group;
         }, {});
     }
