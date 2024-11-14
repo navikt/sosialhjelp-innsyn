d1b442665c3b05e721c6b52f0ba05b1194e7a1b2
Martin Almvik <martin.almvik@nav.no>
2024-11-14 15:58:45 +0100

    Legger til wonderwall

---
+++ src/pages/api/innsyn-api/[...slug].ts
@@ -0,0 +1,55 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+import {proxyApiRouteRequest} from "@navikt/next-api-proxy";
+import {getToken} from "@navikt/oasis";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<unknown>) => {
+    const {slug, ...params} = req.query;
+    let token = getToken(req);
+
+    console.log(process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT);
+    if (["local", "mock"].includes(process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT ?? "")) {
+        // Kommer herifra lokalt/i mock-miljø
+        const tokenCookie = req.cookies["localhost-idtoken"];
+        if (tokenCookie) {
+            token = tokenCookie;
+        }
+    }
+    if (!token) {
+        console.log("Fant ikke token, Redirecter til login");
+        return res.redirect("/oauth2/login");
+    }
+    console.log("Fant token: ", token);
+    if (!slug) {
+        res.status(400).json({message: "Manglende path"});
+        return;
+    }
+    const queryParams = Object.entries(params);
+    const queryString = queryParams
+        .map(([key, value]) => `${key}=${Array.isArray(value) ? value.join(",") : value}`)
+        .join("&");
+    const path =
+        "/sosialhjelp/innsyn-api/" +
+        (Array.isArray(slug) ? slug.join("/") : slug) +
+        (queryString !== "" ? `?${queryString}` : "");
+
+    console.log("Redirecter til ", path);
+    console.log("På hostname ", process.env.NEXT_INNSYN_API_HOSTNAME);
+    await proxyApiRouteRequest({
+        req,
+        res,
+        bearerToken: token,
+        hostname: process.env.NEXT_INNSYN_API_HOSTNAME ?? "",
+        path,
+        https: false,
+        port: process.env.NEXT_PUBLIG_RUNTIME_ENVIRONMENT === "local" ? 8080 : undefined,
+    });
+};
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    },
+};
+
+export default handler;
