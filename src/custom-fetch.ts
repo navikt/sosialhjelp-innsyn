74f9d42e11b12640073138157d417ab64b8a2704
Martin Almvik <martin.almvik@nav.no>
2025-01-11 11:10:13 +0100

    Proper error handling i custom-fetch

--- src/custom-fetch.ts
+++ src/custom-fetch.ts
@@ -1,5 +1,19 @@
 import {logger} from "@navikt/next-logger";
 
+const getBody = <T>(c: Response | Request): Promise<T> => {
+    const contentType = c.headers.get("content-type");
+
+    if (contentType && contentType.includes("application/json")) {
+        return c.json();
+    }
+
+    if (contentType && contentType.includes("application/pdf")) {
+        return c.blob() as Promise<T>;
+    }
+
+    return c.text() as Promise<T>;
+};
+
 export const customFetch = async <T>(url: string, options: RequestInit): Promise<T> => {
     const response = await fetch(url, {
         credentials: ["mock", "local"].includes(process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT ?? "")
@@ -10,15 +24,39 @@ export const customFetch = async <T>(url: string, options: RequestInit): Promise
     if (response.status === 204) {
         return [] as T;
     }
-    if (response.status >= 400) {
-        logger.error(`Error fetching ${url}: ${response.status} ${response.statusText}`);
+    if (!response.ok && !response.redirected) {
+        try {
+            const body = await getBody<string | any>(response);
+            const message = typeof body === "string" ? body : JSON.stringify(body);
+            logger.error(
+                `Non-ok response from ${url}: ${response.status} ${response.statusText}. Response: ${message}`
+            );
+        } catch (e) {
+            logger.error(
+                `error trying to get body from non-ok response from ${url}: ${response.status} ${response.statusText}`,
+                e
+            );
+            throw new Error(`error trying to get body from non-ok response. Exception: ${e}`);
+        }
+        throw new Error(`Non-ok response from ${url}: ${response.status} ${response.statusText}`);
     }
-    const data: T = await response.json();
-
-    // Trenger å få med statuskode på /tilgang
-    if (url.includes("/tilgang")) {
-        return {data, status: response.status} as T;
+    try {
+        const data = await getBody<T>(response);
+        // Kommer som application/json, og blir derfor parsa til json, aka "false" | "true". Konverterer derfor til boolean her.
+        if (data === "true") {
+            return true as T;
+        } else if (data === "false") {
+            return false as T;
+        }
+        // Trenger å få med statuskode på /tilgang
+        if (url.includes("/tilgang")) {
+            return {data, status: response.status} as T;
+        }
+        return data as T;
+    } catch (e) {
+        logger.error(
+            `error trying to get body from ok response from ${url}: ${response.status} ${response.statusText}`
+        );
+        throw new Error(`error trying to get body from response. Exception: ${e}`);
     }
-
-    return data as T;
 };
