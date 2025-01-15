42bde0b92bd79d16353fd2e5bd1fa048ec9cf05c
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-01-15 22:15:45 +0100

    Optional chainingˆ

--- src/components/driftsmelding/useFileUploadAllowed.ts
+++ src/components/driftsmelding/useFileUploadAllowed.ts
@@ -12,7 +12,7 @@ export const ettersendelseErDeaktivert = (kommuneInfo: KommuneResponse | undefin
 export const useFileUploadAllowed = (kommuneInfo: KommuneResponse | undefined, fiksDigisosId: string) => {
     let textKey = null;
     const { data } = useHentSoknadsStatus(fiksDigisosId);
-    if (data && data.isBroken) {
+    if (data?.isBroken) {
         textKey = "driftsmelding.vedlegg.vedleggMangler";
     } else if (ettersendelseErDeaktivert(kommuneInfo)) {
         textKey = "driftsmelding.kanIkkeSendeVedlegg";
