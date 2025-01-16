8df3ce270de4305018dce6cf2b7195a3bd969173
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-01-16 13:46:28 +0100

    Rydder i enhetstester

--- src/components/driftsmelding/useFileUploadError.test.ts
+++ src/components/driftsmelding/useFileUploadError.test.ts
@@ -1,47 +1,65 @@
-import { http, HttpResponse } from "msw";
 import { waitFor } from "@testing-library/react";
 
-import { SoknadsStatusResponse, SoknadsStatusResponseStatus } from "../../generated/model";
 import { server } from "../../mocks/server";
 import { renderHook } from "../../test/test-utils";
+import {
+    getHentKommuneInfoMockHandler,
+    getHentKommuneInfoResponseMock,
+} from "../../generated/kommune-controller/kommune-controller.msw";
+import {
+    getHentSoknadsStatusMockHandler,
+    getHentSoknadsStatusResponseMock,
+} from "../../generated/soknads-status-controller/soknads-status-controller.msw";
 
 import { useFileUploadError } from "./useFileUploadError";
-import { kommuneProblemer } from "./getDriftsmeldingByKommune.test";
 
-const broken: SoknadsStatusResponse = { isBroken: true, status: SoknadsStatusResponseStatus.SENDT };
-const notBroken: SoknadsStatusResponse = { isBroken: false, status: SoknadsStatusResponseStatus.SENDT };
+const KOMMUNE_INNSYN_INACTIVE_RESPONSE = getHentKommuneInfoResponseMock({
+    erInnsynDeaktivert: true,
+    erInnsynMidlertidigDeaktivert: true,
+    erInnsendingEttersendelseDeaktivert: false,
+    erInnsendingEttersendelseMidlertidigDeaktivert: false,
+});
 
-const brokenSoknadHandler = http.get(`/api/v1/innsyn/broken/soknadsStatus`, () => HttpResponse.json(broken));
-const notBrokenSoknadHandler = http.get(`/api/v1/innsyn/notBroken/soknadsStatus`, () => HttpResponse.json(notBroken));
+const KOMMUNE_ETTERSENDELSE_INACTIVE_RESPONSE = getHentKommuneInfoResponseMock({
+    erInnsynDeaktivert: false,
+    erInnsynMidlertidigDeaktivert: false,
+    erInnsendingEttersendelseDeaktivert: true,
+    erInnsendingEttersendelseMidlertidigDeaktivert: true,
+});
+
+const OK_SOKNAD_RESPONSE = getHentSoknadsStatusResponseMock({ isBroken: false });
+const BROKEN_SOKNAD_RESPONSE = getHentSoknadsStatusResponseMock({ isBroken: true });
 
 describe("useFileUploadAllowed", () => {
     beforeEach(() => {
         server.resetHandlers();
     });
 
-    it("Opplasting av vedlegg er lov om innsyn er nede, men ikke ettersendelse", async () => {
-        server.use(notBrokenSoknadHandler);
-        const { result } = renderHook(() => useFileUploadError(kommuneProblemer.utenInnsyn, "notBroken"));
+    it("skal tillate opplasting dersom innsyn er nede, men ikke ettersendelse", async () => {
+        server.use(
+            getHentKommuneInfoMockHandler(KOMMUNE_INNSYN_INACTIVE_RESPONSE),
+            getHentSoknadsStatusMockHandler(OK_SOKNAD_RESPONSE)
+        );
+
+        const { result } = renderHook(() => useFileUploadError("dummy-id"));
         await waitFor(() => expect(result.current).toEqual(null));
     });
 
-    it("Opplasting av vedlegg er disabled ved begge deaktivert", async () => {
-        server.use(notBrokenSoknadHandler);
-        const { result } = renderHook(() => useFileUploadError(kommuneProblemer.utenBegge, "notBroken"));
+    it("skal gi feilmelding dersom kommunens ettersendelse er inaktiv", async () => {
+        server.use(
+            getHentKommuneInfoMockHandler(KOMMUNE_ETTERSENDELSE_INACTIVE_RESPONSE),
+            getHentSoknadsStatusMockHandler(OK_SOKNAD_RESPONSE)
+        );
+        const { result } = renderHook(() => useFileUploadError("dummy-id"));
         await waitFor(() => expect(result.current).toEqual("driftsmelding.kanIkkeSendeVedlegg"));
     });
 
-    it("Opplasting av vedlegg er disabled ved ettersendelse deaktivert", async () => {
-        server.use(notBrokenSoknadHandler);
-        const { result } = renderHook(() => useFileUploadError(kommuneProblemer.utenEttersendelse, "notBroken"));
-        await waitFor(() => expect(result.current).toEqual("driftsmelding.kanIkkeSendeVedlegg"));
+    it("skal gi feilmelding dersom søknaden har isBroken: true pga vedleggmangel", async () => {
+        server.use(
+            getHentKommuneInfoMockHandler(KOMMUNE_ETTERSENDELSE_INACTIVE_RESPONSE),
+            getHentSoknadsStatusMockHandler(BROKEN_SOKNAD_RESPONSE)
+        );
+        const { result } = renderHook(() => useFileUploadError("dummy-id"));
+        await waitFor(() => expect(result.current).toEqual("driftsmelding.vedlegg.vedleggMangler"));
     });
-
-    it.skip("Opplasting av vedlegg er disabled ved broken soknad pga vedleggmangel", async () => {
-        server.use(brokenSoknadHandler);
-        const { result } = renderHook(() => useFileUploadError(kommuneProblemer.ok, "broken"));
-        await waitFor(() => expect(result.current).toEqual("driftsmelding.kanIkkeSendeVedlegg"), {
-            timeout: 5,
-        });
-    }, 99999);
 });
