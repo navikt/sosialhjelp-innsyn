3ccd36528a6a8bc10eb2dec139d96b12ebf9b9b3
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-01-17 13:55:32 +0100

    remove braces around arrow function body

--- src/utbetalinger/utbetalingerUtils.ts
+++ src/utbetalinger/utbetalingerUtils.ts
@@ -1,18 +1,15 @@
 import { i18n } from "i18next";
 import { set } from "date-fns";
 
-const hentUtbetalingTittel = (tittel: string, defaultTittel: string) => {
-    return tittel && tittel !== "default_utbetalinger_tittel" ? tittel : defaultTittel;
-};
+const hentUtbetalingTittel = (tittel: string, defaultTittel: string) =>
+    tittel && tittel !== "default_utbetalinger_tittel" ? tittel : defaultTittel;
 
-const hentTekstForUtbetalingsmetode = (utbetalingsmetode: string, i18n: i18n) => {
-    return i18n.exists(`utbetalingsmetode.${utbetalingsmetode.toLowerCase()}`)
+const hentTekstForUtbetalingsmetode = (utbetalingsmetode: string, i18n: i18n) =>
+    i18n.exists(`utbetalingsmetode.${utbetalingsmetode.toLowerCase()}`)
         ? i18n.t(`utbetalingsmetode.${utbetalingsmetode.toLowerCase()}`)
         : utbetalingsmetode;
-};
 
-const hentMaanedString = (maaned: number, i18n: i18n) => {
-    return set(new Date(0), { month: maaned - 1 }).toLocaleString(i18n.language, { month: "long" });
-};
+const hentMaanedString = (maaned: number, i18n: i18n) =>
+    set(new Date(0), { month: maaned - 1 }).toLocaleString(i18n.language, { month: "long" });
 
 export { hentUtbetalingTittel, hentMaanedString, hentTekstForUtbetalingsmetode };
