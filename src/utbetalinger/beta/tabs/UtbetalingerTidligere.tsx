b3a5e78d84b5a4bdd45a835bf1c1047901209d21
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-01-20 17:26:58 +0100

    Lager felles error/loading-komponent for nye og tidligere utbetalinger

--- src/utbetalinger/beta/tabs/UtbetalingerTidligere.tsx
+++ src/utbetalinger/beta/tabs/UtbetalingerTidligere.tsx
@@ -5,40 +5,29 @@ import { useTranslation } from "next-i18next";
 import { useHentTidligereUtbetalinger } from "../../../generated/utbetalinger-controller/utbetalinger-controller";
 import useFiltrerteUtbetalinger from "../filter/useFiltrerteUtbetalinger";
 import { useFilter } from "../filter/FilterContext";
-import Lastestriper from "../../../components/lastestriper/Lasterstriper";
 import { UtbetalingerResponseMedId } from "../UtbetalingerPanelBeta";
 import { addIdToUtbetalinger } from "../addIdToUtbetalinger";
 
 import { ManedGruppe } from "./ManedGruppe";
+import { UtbetalingerLoadingWrapper } from "./UtbetalingerLoadingWrapper";
 
-export const TidligereUtbetalinger = () => {
+export const UtbetalingerTidligere = () => {
     const { data, isLoading, isError } = useHentTidligereUtbetalinger({ query: { select: addIdToUtbetalinger } });
     const filtrerteTidligere = useFiltrerteUtbetalinger(data ?? []);
     const { isUsingFilter } = useFilter();
     const { t } = useTranslation("utbetalinger");
 
-    if (isLoading) {
-        return <Lastestriper />;
-    }
-    if (isError) {
-        return (
-            <Alert variant="error" inline>
-                {t("feil.fetch")}
-            </Alert>
-        );
-    }
-    if (filtrerteTidligere.length === 0)
-        return (
-            <Alert variant="info" inline>
-                {isUsingFilter ? t("feil.ingen.filter") : t("feil.ingen.default.tidligere")}
-            </Alert>
-        );
-
     return (
-        <>
-            {filtrerteTidligere.map((utbetalingSak: UtbetalingerResponseMedId) => (
-                <ManedGruppe utbetalingSak={utbetalingSak} key={`${utbetalingSak.maned}-${utbetalingSak.ar}`} />
-            ))}
-        </>
+        <UtbetalingerLoadingWrapper isLoading={isLoading} isError={isError}>
+            {filtrerteTidligere?.length ? (
+                filtrerteTidligere.map((utbetalingSak: UtbetalingerResponseMedId) => (
+                    <ManedGruppe utbetalingSak={utbetalingSak} key={`${utbetalingSak.maned}-${utbetalingSak.ar}`} />
+                ))
+            ) : (
+                <Alert variant="info" inline>
+                    {isUsingFilter ? t("feil.ingen.filter") : t("feil.ingen.default.tidligere")}
+                </Alert>
+            )}
+        </UtbetalingerLoadingWrapper>
     );
 };
