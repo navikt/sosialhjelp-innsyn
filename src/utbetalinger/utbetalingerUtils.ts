98eb57368f3de77f9d45c9b318472d72919206eb
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-01-20 18:52:47 +0100

    Refaktor av utbetalingsmetode for lesbarhet

--- src/utbetalinger/utbetalingerUtils.ts
+++ src/utbetalinger/utbetalingerUtils.ts
@@ -3,9 +3,11 @@ import { i18n } from "i18next";
 const hentUtbetalingTittel = (tittel: string, defaultTittel: string) =>
     tittel && tittel !== "default_utbetalinger_tittel" ? tittel : defaultTittel;
 
-const hentTekstForUtbetalingsmetode = (utbetalingsmetode: string, i18n: i18n) =>
-    i18n.exists(`utbetalingsmetode.${utbetalingsmetode.toLowerCase()}`)
-        ? i18n.t(`utbetalingsmetode.${utbetalingsmetode.toLowerCase()}`)
+const utbetalingsmetodeText = (utbetalingsmetode: string | undefined, i18n: i18n) => {
+    if (!utbetalingsmetode) return null;
+    return i18n.exists(`utbetalingsmetode.${utbetalingsmetode?.toLowerCase()}`)
+        ? i18n.t(`utbetalingsmetode.${utbetalingsmetode?.toLowerCase()}`)
         : utbetalingsmetode;
+};
 
-export { hentUtbetalingTittel, hentTekstForUtbetalingsmetode };
+export { hentUtbetalingTittel, utbetalingsmetodeText };
