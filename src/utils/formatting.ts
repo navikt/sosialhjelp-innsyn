2d6278e2f53b3a52d9dbdb552a5585351747db0d
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-01-20 17:47:15 +0100

    Refaktor av formattere
    * .replace er ikke nødvendig; DateTimeFormat legger punktum på ordinaltall
    * syntaksendringer (disse funksjonene kommer ikke til å leve lenge)

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -10,29 +10,28 @@ function formatBytes(bytes: number, decimals: number = 2): string {
 }
 
 // Eksempel: formatCurrency(12345) => 12.345
-function formatCurrency(amount: number, language: string): string {
-    return new Intl.NumberFormat(language).format(amount);
-}
+const formatCurrency = (amount: number, language: string): string => new Intl.NumberFormat(language).format(amount);
 
 // Eksempel: "2019-08-01" => "01. august 2019"
-function formatDato(isoDate: string, language: string) {
-    const dato: Date = new Date(isoDate);
-    const formatter = new Intl.DateTimeFormat(language, { day: "numeric", month: "long", year: "numeric" });
-    return formatter.format(dato).replace(/([0-9]) /, "$1. ");
-}
-// Eksempel "2022-04-11" => "11. april"
-export function getDayAndMonth(isoDate: string, language: string) {
-    const dato: Date = new Date(isoDate);
-    const formatter = new Intl.DateTimeFormat(language, { day: "numeric", month: "long" });
-    return formatter.format(dato).replace(/([0-9]) /, "$1. ");
-}
+export const formatDato = (isoDate: string, language: string) =>
+    new Intl.DateTimeFormat(language, {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    }).format(new Date(isoDate));
 
-export function dateToDDMMYYYY(language: string, dato: Date) {
-    return new Intl.DateTimeFormat(language, {
+// Eksempel "2022-04-11" => "11. april"
+export const getDayAndMonth = (isoDate: string, language: string) =>
+    new Intl.DateTimeFormat(language, {
+        day: "numeric",
+        month: "long",
+    }).format(new Date(isoDate));
+
+export const dateToDDMMYYYY = (language: string, dato: Date) =>
+    new Intl.DateTimeFormat(language, {
         day: "2-digit",
         month: "2-digit",
         year: "numeric",
     }).format(dato);
-}
 
-export { formatBytes, formatCurrency, formatDato };
+export { formatBytes, formatCurrency };
