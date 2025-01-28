fe361715ce70c232e261b253097f6808a372df2c
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-01-28 22:34:29 +0100

    Fjern styled-components

--- src/components/driftsmelding/DriftsmeldingKommune.tsx
+++ src/components/driftsmelding/DriftsmeldingKommune.tsx
@@ -1,16 +1,11 @@
 import * as React from "react";
 import { useTranslation } from "next-i18next";
 import { Alert, Label } from "@navikt/ds-react";
-import styled from "styled-components";
 
 import DatoOgKlokkeslett from "../tidspunkt/DatoOgKlokkeslett";
 import { KommuneResponse } from "../../generated/model";
 
 import { getDriftsmeldingByKommune } from "./lib/getDriftsmeldingByKommune";
-
-const StyledAlert = styled(Alert)`
-    margin-bottom: 1rem;
-`;
 
 export const DriftsmeldingKommune = ({ kommune }: { kommune: KommuneResponse | undefined }) => {
     const { t } = useTranslation();
@@ -20,11 +15,11 @@ export const DriftsmeldingKommune = ({ kommune }: { kommune: KommuneResponse | u
     if (!driftsmelding) return null;
 
     return (
-        <StyledAlert variant="error">
+        <Alert className="mb-4" variant="error">
             <Label as="p">
                 <DatoOgKlokkeslett tidspunkt={kommune?.tidspunkt?.toString() || ""} />
             </Label>
             {t(driftsmelding)}
-        </StyledAlert>
+        </Alert>
     );
 };
