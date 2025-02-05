cd5dd9dfa88e81719cde3f39df60031485a9a500
Martin Almvik <martin.almvik@nav.no>
2025-02-05 08:17:27 +0100

    Nedgrader /tilgang-error til warn
    Det spammes for mye i logs

--- src/components/Tilgangskontrollside/Tilgangskontrollside.tsx
+++ src/components/Tilgangskontrollside/Tilgangskontrollside.tsx
@@ -71,7 +71,7 @@ const Tilgangskontrollside = ({ children }: TilgangskontrollsideProps) => {
     }
 
     if (error) {
-        logger.error(`Fikk feilmelding fra harTilgang. Code: ${harTilgangData?.status}, message: ${error.message}`);
+        logger.warn(`Fikk feilmelding fra harTilgang. Code: ${harTilgangData?.status}, message: ${error.message}`);
     }
 
     const isAuthError = harTilgangData?.status === 401 || harTilgangData?.status === 403;
