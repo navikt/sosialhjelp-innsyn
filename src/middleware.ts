8d3ec4f39f61e41b8fcfaacd152e69657a7343fb
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-02-18 18:33:24 +0100

    Flytter noen miljøvariabler til env.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { logger } from "@navikt/next-logger";
 
+import { browserEnv, getServerEnv } from "./config/env";
+
 const PUBLIC_FILE = /\.(.*)$/;
 
 interface AzureAdAuthenticationError {
@@ -20,6 +22,10 @@ export async function middleware(request: NextRequest) {
 
     // Sett språk basert på decorator-language cookien
     const decoratorLocale = request.cookies.get("decorator-language")?.value ?? "nb";
+
+    const { NEXT_PUBLIC_BASE_PATH, NEXT_PUBLIC_INNSYN_ORIGIN, NEXT_PUBLIC_RUNTIME_ENVIRONMENT } = browserEnv;
+    const { NEXT_INNSYN_API_BASE_URL } = getServerEnv();
+
     if (decoratorLocale !== request.nextUrl.locale) {
         if (request.nextUrl.locale !== "nb") {
             const next = NextResponse.next();
@@ -27,7 +33,7 @@ export async function middleware(request: NextRequest) {
             return next;
         }
         const url = new URL(
-            `${process.env.NEXT_PUBLIC_INNSYN_ORIGIN}${process.env.NEXT_PUBLIC_BASE_PATH}/${
+            `${NEXT_PUBLIC_INNSYN_ORIGIN}${NEXT_PUBLIC_BASE_PATH}/${
                 decoratorLocale === "nb" ? "" : decoratorLocale
             }${pathname.replace("/sosialhjelp/innsyn", "")}`
         );
@@ -35,19 +41,18 @@ export async function middleware(request: NextRequest) {
     }
 
     // Router bruker til login hvis vi får 401. Dette gjelder bare for auth gjennom mock-alt. I prod/preprod gjelder dette for "vanlig" innlogging på login.nav.no
-    if (["mock", "local"].includes(process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT!)) {
+    if (["mock", "local"].includes(NEXT_PUBLIC_RUNTIME_ENVIRONMENT)) {
         // Reroute ved kall til /link. Brukes for redirect fra login-api
         if (pathname.startsWith("/link")) {
-            const searchParams = request.nextUrl.searchParams;
-            if (!searchParams.has("goto")) {
-                throw new Error("redirect mangler goto-parameter");
-            }
-            return NextResponse.redirect(new URL(searchParams.get("goto")!, process.env.NEXT_PUBLIC_INNSYN_ORIGIN));
+            const gotoParam = request.nextUrl.searchParams.get("goto");
+            if (!gotoParam) throw new Error("redirect mangler goto-parameter");
+            return NextResponse.redirect(new URL(gotoParam, NEXT_PUBLIC_INNSYN_ORIGIN));
         }
+
         try {
             const headers = new Headers(request.headers);
             headers.append("Authorization", `Bearer ${request.cookies.get("localhost-idtoken")?.value}`);
-            const harTilgangResponse = await fetch(process.env.NEXT_INNSYN_API_BASE_URL + "/api/v1/innsyn/tilgang", {
+            const harTilgangResponse = await fetch(`${NEXT_INNSYN_API_BASE_URL}/api/v1/innsyn/tilgang`, {
                 headers,
                 credentials: "include",
             });
@@ -55,30 +60,22 @@ export async function middleware(request: NextRequest) {
                 const json: AzureAdAuthenticationError = await harTilgangResponse.json();
                 const queryDivider = json.loginUrl.includes("?") ? "&" : "?";
 
-                const redirectUrl = getRedirect(
-                    json.loginUrl,
-                    pathname,
-                    process.env.NEXT_PUBLIC_INNSYN_ORIGIN!,
-                    json.id
-                );
+                const redirectUrl = getRedirect(json.loginUrl, pathname, NEXT_PUBLIC_INNSYN_ORIGIN, json.id);
                 return NextResponse.redirect(json.loginUrl + queryDivider + redirectUrl);
             }
         } catch (e) {
             // ikke logg eller redirect dersom feilen oppstod i forsøk på å rendre 500-siden
             if (pathname === "/500") return;
             logger.warn("Feil i middleware fetch, sender bruker til 500", e);
-            return NextResponse.redirect(process.env.NEXT_PUBLIC_INNSYN_ORIGIN + "/sosialhjelp/innsyn/500");
+            return NextResponse.redirect(`${NEXT_PUBLIC_INNSYN_ORIGIN}/sosialhjelp/innsyn/500`);
         }
     }
 }
 
 const getRedirect = (loginUrl: string, pathname: string, origin: string, id: string) => {
-    const _pathname = pathname.includes("/sosialhjelp/innsyn") ? pathname : "/sosialhjelp/innsyn" + pathname;
+    const _pathname = pathname.includes("/sosialhjelp/innsyn") ? pathname : `/sosialhjelp/innsyn${pathname}`;
     if (loginUrl.indexOf("digisos.intern.dev.nav.no") === -1) {
-        const gotoParameter = "goto=" + _pathname;
-        const redirectPath = origin + "/sosialhjelp/innsyn/link?" + gotoParameter;
-
-        return "redirect=" + redirectPath + "%26login_id=" + id;
+        return `redirect=${origin}/sosialhjelp/innsyn/link?goto=${_pathname}%26login_id=${id}`;
     } else {
         // ikke loginservice --> direkte-integrasjon med idporten i innsyn-api:
         return "goto=" + origin + _pathname;
