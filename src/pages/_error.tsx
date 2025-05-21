fc53365d0fea7f091b8141150b21ad4e57d07934
Ragnhild Neset <ragcecnes@gmail.com>
2025-05-21 15:29:59 +0200

    Remove 404 page.

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,12 +1,10 @@
 import { NextPageContext } from "next";
 import NextErrorComponent from "next/error";
 
-import SideIkkeFunnet from "./404";
 import ServerError from "./500";
 
 const Error = ({ statusCode }: { statusCode: number }) => {
     if (statusCode === 500) return <ServerError />;
-    if (statusCode === 404) return <SideIkkeFunnet />;
     return <NextErrorComponent statusCode={statusCode} />;
 };
 
