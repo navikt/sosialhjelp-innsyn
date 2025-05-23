c7727f04d42b90841349a09bc9daffcff53b6925
Martin Almvik <martin.almvik@nav.no>
2025-05-23 12:36:40 +0200

    Fiks api-proxy

--- src/app/api/innsyn-api/[...slug]/route.ts
+++ src/app/api/innsyn-api/[...slug]/route.ts
@@ -1,12 +1,14 @@
 import { proxyRouteHandler } from "@navikt/next-api-proxy";
 import { cookies } from "next/headers";
 
+import { isLocalhost, isMock } from "../../../../utils/restUtils";
+
 type RouteHandlerProxyTarget = { hostname: string; path: string; https: boolean; bearerToken?: string; port?: string };
 type ProxyRequestContext = { params: Promise<{ slug: string[] }> };
 type ProxyRequestHandler = (request: Request, context: ProxyRequestContext) => Promise<Response>;
 
 const getRouteHandlerProxyTarget = async (
-    headers: Headers,
+    bearerToken: string | undefined,
     requestPath: string[]
 ): Promise<RouteHandlerProxyTarget> => {
     const hostname = process.env.NEXT_INNSYN_API_HOSTNAME;
@@ -18,24 +20,25 @@ const getRouteHandlerProxyTarget = async (
 
     const path = `${basePath}/${requestPath.join("/")}`;
     const port = process.env.INNSYN_API_PORT;
-    let bearerToken;
-    if (process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "local") {
-        const cookieStore = await cookies();
-        if (!cookieStore.has("localhost-idtoken")) {
-            throw new Error("Missing localhost-idtoken cookie");
-        }
-        bearerToken = cookieStore.get("localhost-idtoken")?.value;
-    } else {
-        if (headers.has("Authorization")) {
-            throw new Error("Missing auth header");
-        }
-        bearerToken = `${headers.get("Authorization")?.split(" ")[1]}`;
-    }
     return { hostname, path: encodeURI(path), bearerToken, https, port };
 };
 
 const soknadApiProxy: ProxyRequestHandler = async (request, { params }): Promise<Response> => {
-    return proxyRouteHandler(request, await getRouteHandlerProxyTarget(request.headers, (await params).slug));
+    let bearerToken;
+    if (isLocalhost() || isMock()) {
+        const cookieStore = await cookies();
+        if (!cookieStore.has("localhost-idtoken")) {
+            return new Response("Missing auth header", { status: 401 });
+        }
+        bearerToken = cookieStore.get("localhost-idtoken")?.value;
+    } else {
+        const headers = request.headers;
+        if (!headers.has("Authorization")) {
+            return new Response("Missing auth header", { status: 401 });
+        }
+        bearerToken = `${headers.get("Authorization")?.split(" ")[1]}`;
+    }
+    return proxyRouteHandler(request, await getRouteHandlerProxyTarget(bearerToken, (await params).slug));
 };
 
 export const DELETE = soknadApiProxy;
