aadffacf2ff68dddeeb76529f5f885257ab523a0
Ragnhild Neset <ragcecnes@gmail.com>
2025-05-26 10:06:55 +0200

    Logg errors.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { logger } from "@navikt/next-logger";
 
 import ServerError from "../pages/500";
 
-export default function Error() {
+export default function Error({ error }: { error: Error }) {
+    logger.error(
+        `Uncaught clientside error: ${error.name}, error.tsx shown. ErrorInfo: ${JSON.stringify(error.message)}`
+    );
+
     useEffect(() => {
         document.title = "Teknisk feil | Økonomisk sosialhjelp";
     }, []);
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import Cookie from "js-cookie";
+import { logger } from "@navikt/next-logger";
 
 import { DECORATOR_LOCALE_COOKIE_NAME, isSupportedLocale } from "../i18n/common";
 import ServerError from "../pages/500";
 
-export default function GlobalError() {
+export default function GlobalError({ error }: { error: Error }) {
     const langCookie = Cookie.get(DECORATOR_LOCALE_COOKIE_NAME);
     const locale = langCookie && isSupportedLocale(langCookie) ? langCookie : "nb";
+
+    logger.error(
+        `Uncaught clientside error: ${error.name}, global-error.tsx shown. ErrorInfo: ${JSON.stringify(error.message)}`
+    );
 
     return (
         <html lang={locale}>
