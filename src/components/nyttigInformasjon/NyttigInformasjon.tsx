c73d0e012c1afc7e617989054030271b7f1c212a
Martin Almvik <martin.almvik@nav.no>
2025-06-23 11:35:54 +0200

    Fiks lenker i NyttigInformasjon

--- src/components/nyttigInformasjon/NyttigInformasjon.tsx
+++ src/components/nyttigInformasjon/NyttigInformasjon.tsx
@@ -1,17 +1,25 @@
 import { Box, Heading, Link, VStack } from "@navikt/ds-react";
-import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 
-const NyttigInformasjon = () => {
-    const t = useTranslations("NyttigInformasjon");
+import { getServerEnv } from "../../config/env";
+
+const NyttigInformasjon = async () => {
+    const t = await getTranslations("NyttigInformasjon");
     return (
         <Box>
             <VStack gap="4">
                 <Heading size="medium" level="2">
                     {t("tittel")}
                 </Heading>
-                <Link>{t("endringer")}</Link>
-                <Link>{t("klagerettigheter")}</Link>
-                <Link>{t("personopplysninger")}</Link>
+                <Link href={`${getServerEnv().NEXT_PUBLIC_INNSYN_ORIGIN}/okonomisk-sosialhjelp#melde`}>
+                    {t("endringer")}
+                </Link>
+                <Link href={`${getServerEnv().NEXT_PUBLIC_INNSYN_ORIGIN}/okonomisk-sosialhjelp#klage`}>
+                    {t("klagerettigheter")}
+                </Link>
+                <Link href={`${getServerEnv().NEXT_PUBLIC_INNSYN_ORIGIN}/personopplysninger-sosialhjelp`}>
+                    {t("personopplysninger")}
+                </Link>
             </VStack>
         </Box>
     );
