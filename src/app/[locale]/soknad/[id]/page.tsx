0beabdda8b36b6b10b6936998aa61d6e962317d5
Martin Almvik <martin.almvik@nav.no>
2025-06-25 15:26:28 +0200

    Fiks params på søknadsiden

--- src/app/[locale]/soknad/[id]/page.tsx
+++ src/app/[locale]/soknad/[id]/page.tsx
@@ -4,19 +4,20 @@ import { getFlag, getToggles } from "../../../../featuretoggles/unleash";
 import { StatusSendtPage } from "../../../../components/statusPage/StatusSendtPage";
 import { hentSoknadsStatus } from "../../../../generated/ssr/soknads-status-controller/soknads-status-controller";
 
-interface Props {
-    params: {
+const Page = async ({
+    params,
+}: {
+    params: Promise<{
         id: string;
-    };
-}
-
-const Page = async ({ params }: Props) => {
+    }>;
+}) => {
     const toggle = getFlag("sosialhjelp.innsyn.ny_soknadside", await getToggles());
     if (!toggle.enabled) {
         return notFound();
     }
+    const { id } = await params;
 
-    const soknadsStatusResponse = await hentSoknadsStatus(params.id);
+    const soknadsStatusResponse = await hentSoknadsStatus(id);
     if (soknadsStatusResponse.data.status === "SENDT") {
         return <StatusSendtPage navKontor={soknadsStatusResponse.data.navKontor ?? "et navkontor"} />;
     }
