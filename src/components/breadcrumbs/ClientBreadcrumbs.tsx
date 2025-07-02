b5027e41eb92b371f085cf51accb0fc24acc895a
Ragnhild Neset <ragcecnes@gmail.com>
2025-07-02 08:41:57 +0200

    Add breadrumbs to Klage-page. (#1174)
    * Add breadrumbs to Klage-page.
    And move breadcrumbs logic to each page.
    
    * Change last crumb to have no link.

---
+++ src/components/breadcrumbs/ClientBreadcrumbs.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Breadcrumb, LastCrumb } from "../../utils/breadcrumbs";
+import { useSetBreadcrumbs } from "../../hooks/useUpdateBreadcrumbs";
+
+interface ClientBreadcrumbsProps {
+    dynamicBreadcrumbs?: (Breadcrumb | LastCrumb)[];
+}
+const ClientBreadcrumbs = ({ dynamicBreadcrumbs }: ClientBreadcrumbsProps) => {
+    useSetBreadcrumbs(dynamicBreadcrumbs);
+
+    return <></>;
+};
+
+export default ClientBreadcrumbs;
