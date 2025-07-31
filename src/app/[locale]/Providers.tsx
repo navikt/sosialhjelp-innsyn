51c1a21570b76784a664f761b6726dd438388997
Ragnhild Neset <ragcecnes@gmail.com>
2025-07-31 15:29:03 +0200

    Add path aliases and rewrite files in app folder.

--- src/app/[locale]/Providers.tsx
+++ src/app/[locale]/Providers.tsx
@@ -8,11 +8,11 @@ import Cookies from "js-cookie";
 import { IToggle } from "@unleash/nextjs";
 import { usePathname, useRouter } from "next/navigation";
 
-import { FlagProvider } from "../../featuretoggles/context";
-import { logBrukerDefaultLanguage, logBrukerSpraakChange } from "../../utils/amplitude";
-import { getFaro, initInstrumentation, pinoLevelToFaroLevel } from "../../faro/faro";
-import { TilgangResponse } from "../../generated/model";
-import TilgangskontrollsideApp from "../../components/Tilgangskontrollside/TilgangskontrollsideApp";
+import { FlagProvider } from "@featuretoggles/context";
+import { logBrukerDefaultLanguage, logBrukerSpraakChange } from "@utils/amplitude";
+import { getFaro, initInstrumentation, pinoLevelToFaroLevel } from "@faro/faro";
+import { TilgangResponse } from "@generated/model";
+import TilgangskontrollsideApp from "@components/Tilgangskontrollside/TilgangskontrollsideApp";
 
 initInstrumentation();
 configureLogger({
--- src/app/[locale]/klage/status/[id]/[vedtakId]/page.tsx
+++ src/app/[locale]/klage/status/[id]/[vedtakId]/page.tsx
@@ -2,8 +2,8 @@ import { Heading } from "@navikt/ds-react";
 import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 
-import { getFlag, getToggles } from "../../../../../../featuretoggles/unleash";
-import ClientBreadcrumbs from "../../../../../../components/breadcrumbs/ClientBreadcrumbs";
+import { getFlag, getToggles } from "@featuretoggles/unleash";
+import ClientBreadcrumbs from "@components/breadcrumbs/ClientBreadcrumbs";
 
 const Page = async () => {
     const toggle = getFlag("sosialhjelp.innsyn.klage", await getToggles());
