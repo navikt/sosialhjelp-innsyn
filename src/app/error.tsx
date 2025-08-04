bb1085488cd22fb585ac80b87c3b1c2347066263
Martin Almvik <martin.almvik@nav.no>
2025-08-04 14:36:50 +0200

    Logg mer av error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,10 +5,8 @@ import { logger } from "@navikt/next-logger";
 
 import ServerError from "../pages/500";
 
-export default function Error({ error }: { error: Error }) {
-    logger.error(
-        `Uncaught clientside error: ${error.name}, error.tsx shown. ErrorInfo: ${JSON.stringify(error.message)}`
-    );
+export default function Error({ error }: { error: Error & { digest?: string } }) {
+    logger.error(`Uncaught clientside error: ${error.name}, error.tsx shown. Error: ${error}.`);
 
     useEffect(() => {
         document.title = "Teknisk feil | Økonomisk sosialhjelp";
