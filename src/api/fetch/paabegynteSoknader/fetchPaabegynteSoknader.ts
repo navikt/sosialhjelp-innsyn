cd8fda17884012e52f5058dfa0b8a08bbdd2350b
Martin Almvik <martin.almvik@nav.no>
2025-08-05 13:35:10 +0200

    Bedre error-håndtering på fetch av påbegynte søknader

--- src/api/fetch/paabegynteSoknader/fetchPaabegynteSoknader.ts
+++ src/api/fetch/paabegynteSoknader/fetchPaabegynteSoknader.ts
@@ -16,18 +16,16 @@ export interface PaabegyntSoknad {
 }
 
 const fetchPaabegynteSaker = async (): Promise<PaabegyntSoknad[]> => {
-    if (getServerEnv().NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "local") {
-        return Promise.resolve([]);
-    }
     try {
-        return exchangedFetch<PaabegyntSoknad[]>(
+        return await exchangedFetch<PaabegyntSoknad[]>(
             "/dittnav/pabegynte/aktive",
             getServerEnv().SOKNAD_API_HOSTNAME,
-            "/sosialhjelp/soknad-api"
+            "/sosialhjelp/soknad-api",
+            getServerEnv().SOKNAD_API_PORT
         );
     } catch (e: unknown) {
         logger.error(`Feil ved henting av paabegynte saker ${e}`);
-        return Promise.resolve([]);
+        return [];
     }
 };
 
