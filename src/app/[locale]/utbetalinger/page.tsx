0fd992a1bca0f760519518917b80aec4946dc488
joeldlin <jorgen.elden.lindgren@nav.no>
2025-08-15 13:06:31 +0200

    wip

--- src/app/[locale]/utbetalinger/page.tsx
+++ src/app/[locale]/utbetalinger/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { getTranslations } from "next-intl/server";
-import { VStack } from "@navikt/ds-react";
+import { Chips, Heading, VStack } from "@navikt/ds-react";
+import { ChipsToggle } from "@navikt/ds-react/Chips";
 
 import { getFlag, getToggles } from "@featuretoggles/unleash";
 
@@ -11,7 +12,19 @@ const Page = async () => {
         return notFound();
     }
 
-    return <VStack>{t("tittel")}</VStack>;
+    return (
+        <VStack gap="20" className="mt-20">
+            <Heading size="xlarge" level="1">
+                {t("tittel")}
+            </Heading>
+            <Chips>
+                <ChipsToggle checkmark={false}>hei 1</ChipsToggle>
+                <ChipsToggle checkmark={false}>hei 2</ChipsToggle>
+                <ChipsToggle checkmark={false}>hei 3</ChipsToggle>
+                <ChipsToggle checkmark={false}>hei 4</ChipsToggle>
+            </Chips>
+        </VStack>
+    );
 };
 
 export default Page;
