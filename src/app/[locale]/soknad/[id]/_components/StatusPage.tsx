bd5144a0ac547dae213fba725bc07ad7551352b7
Martin Almvik <martin.almvik@nav.no>
2025-08-15 12:18:49 +0200

    Flytt oppgaver inn i alle sider

--- src/app/[locale]/soknad/[id]/_components/StatusPage.tsx
+++ src/app/[locale]/soknad/[id]/_components/StatusPage.tsx
@@ -6,10 +6,13 @@ import { getTranslations } from "next-intl/server";
 import { prefetchHentHendelserBetaQuery } from "@generated/ssr/hendelse-controller/hendelse-controller";
 import { prefetchHentVedleggQuery } from "@generated/ssr/vedlegg-controller/vedlegg-controller";
 import { getQueryClient } from "@api/queryClient";
+import { prefetchGetOppgaverBetaQuery } from "@generated/ssr/oppgave-controller/oppgave-controller";
 
 import Oversikt, { OversiktSkeleton } from "./oversikt/Oversikt";
 import Dokumenter, { DokumenterSkeleton } from "./dokumenter/Dokumenter";
 import Filopplasting from "./dokumenter/Filopplasting";
+import Oppgaver from "./oppgaver/Oppgaver";
+import OppgaveAlert from "./oppgaver/OppgaveAlert";
 
 interface Props {
     heading: ReactNode;
@@ -21,17 +24,22 @@ export const StatusPage = async ({ id, heading, alert, children }: PropsWithChil
     const t = await getTranslations("StatusPage");
     const hendelserQueryClient = getQueryClient();
     const vedleggQueryClient = getQueryClient();
+    const oppgaverQueryClient = getQueryClient();
 
     // Prefetcher her og putter det i HydrationBoundary slik at det er tilgjengelig i browseren
     prefetchHentHendelserBetaQuery(hendelserQueryClient, id);
     prefetchHentVedleggQuery(vedleggQueryClient, id);
-
+    await prefetchGetOppgaverBetaQuery(oppgaverQueryClient, id);
     return (
         <VStack gap="20" className="mt-20">
             <Heading size="xlarge" level="1">
                 {heading}
             </Heading>
             {alert}
+            <HydrationBoundary state={dehydrate(oppgaverQueryClient)}>
+                <OppgaveAlert />
+                <Oppgaver />
+            </HydrationBoundary>
             {children}
             <VStack gap="2">
                 <Heading size="large" level="2">
--- src/app/[locale]/soknad/[id]/_components/StatusUnderBehandlingPage.tsx
+++ src/app/[locale]/soknad/[id]/_components/StatusUnderBehandlingPage.tsx
@@ -1,12 +1,6 @@
 import { getTranslations } from "next-intl/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-
-import { prefetchGetOppgaverBetaQuery } from "@generated/ssr/oppgave-controller/oppgave-controller";
-import { getQueryClient } from "@api/queryClient";
 
 import { StatusPage } from "./StatusPage";
-import Oppgaver from "./oppgaver/Oppgaver";
-import OppgaveAlert from "./oppgaver/OppgaveAlert";
 
 interface Props {
     id: string;
@@ -15,21 +9,5 @@ interface Props {
 export const StatusUnderBehandlingPage = async ({ id }: Props) => {
     const t = await getTranslations("StatusUnderBehandlingPage");
 
-    const queryClient = getQueryClient();
-    await prefetchGetOppgaverBetaQuery(queryClient, id);
-    return (
-        <StatusPage
-            id={id}
-            heading={t("tittel")}
-            alert={
-                <HydrationBoundary state={dehydrate(queryClient)}>
-                    <OppgaveAlert />
-                </HydrationBoundary>
-            }
-        >
-            <HydrationBoundary state={dehydrate(queryClient)}>
-                <Oppgaver />
-            </HydrationBoundary>
-        </StatusPage>
-    );
+    return <StatusPage id={id} heading={t("tittel")} />;
 };
