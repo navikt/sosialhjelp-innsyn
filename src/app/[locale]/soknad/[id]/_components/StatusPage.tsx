5b87b4a345e156f8a5de1c2bba4884bd0c64561b
Martin Almvik <martin.almvik@nav.no>
2025-08-18 13:47:18 +0200

    Dra ut oversikt i egen komponent

--- src/app/[locale]/soknad/[id]/_components/StatusPage.tsx
+++ src/app/[locale]/soknad/[id]/_components/StatusPage.tsx
@@ -1,14 +1,12 @@
 import React, { PropsWithChildren, ReactNode, Suspense } from "react";
 import { Heading, VStack } from "@navikt/ds-react";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getTranslations } from "next-intl/server";
 
-import { prefetchHentHendelserBetaQuery } from "@generated/ssr/hendelse-controller/hendelse-controller";
 import { prefetchHentVedleggQuery } from "@generated/ssr/vedlegg-controller/vedlegg-controller";
 import { getQueryClient } from "@api/queryClient";
 import { prefetchGetOppgaverBetaQuery } from "@generated/ssr/oppgave-controller/oppgave-controller";
 
-import Oversikt, { OversiktSkeleton } from "./oversikt/Oversikt";
+import Oversikt from "./oversikt/Oversikt";
 import Dokumenter, { DokumenterSkeleton } from "./dokumenter/Dokumenter";
 import Filopplasting from "./dokumenter/Filopplasting";
 import Oppgaver from "./oppgaver/Oppgaver";
@@ -21,13 +19,10 @@ interface Props {
 }
 
 export const StatusPage = async ({ id, heading, alert, children }: PropsWithChildren<Props>) => {
-    const t = await getTranslations("StatusPage");
-    const hendelserQueryClient = getQueryClient();
     const vedleggQueryClient = getQueryClient();
     const oppgaverQueryClient = getQueryClient();
 
     // Prefetcher her og putter det i HydrationBoundary slik at det er tilgjengelig i browseren
-    prefetchHentHendelserBetaQuery(hendelserQueryClient, id);
     prefetchHentVedleggQuery(vedleggQueryClient, id);
     await prefetchGetOppgaverBetaQuery(oppgaverQueryClient, id);
     return (
@@ -41,16 +36,7 @@ export const StatusPage = async ({ id, heading, alert, children }: PropsWithChil
                 <Oppgaver />
             </HydrationBoundary>
             {children}
-            <VStack gap="2">
-                <Heading size="large" level="2">
-                    {t("tittel")}
-                </Heading>
-                <Suspense fallback={<OversiktSkeleton />}>
-                    <HydrationBoundary state={dehydrate(hendelserQueryClient)}>
-                        <Oversikt />
-                    </HydrationBoundary>
-                </Suspense>
-            </VStack>
+            <Oversikt id={id} />
             <Filopplasting />
             <Suspense fallback={<DokumenterSkeleton />}>
                 <HydrationBoundary state={dehydrate(vedleggQueryClient)}>
