fa9f197c1d6b5606446752739f8078874432fc02
joeldlin <jorgen.elden.lindgren@nav.no>
2025-08-19 08:24:27 +0200

    søker får nu melding om søknaden deres er delt opp i flere saker

--- src/app/[locale]/soknad/[id]/_components/StatusUnderBehandlingPage.tsx
+++ src/app/[locale]/soknad/[id]/_components/StatusUnderBehandlingPage.tsx
@@ -1,25 +1,43 @@
 import { getTranslations } from "next-intl/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Heading, Tag } from "@navikt/ds-react";
+import { logger } from "@navikt/next-logger";
 
 import { getQueryClient } from "@api/queryClient";
 import { hentSaksStatuser } from "@generated/ssr/saks-status-controller/saks-status-controller";
 import { getTranslationKeyForUtfall, isVedtakUtfallKey } from "@components/vedtak/VedtakUtfallMap";
 import { VedtakUtfall } from "@components/vedtak/VedtakUtfall";
+import { hentAlleSaker } from "@generated/ssr/saks-oversikt-controller/saks-oversikt-controller";
+import fetchPaabegynteSaker from "@api/fetch/paabegynteSoknader/fetchPaabegynteSoknader";
+import fetchSoknadsdetaljer from "@api/fetch/saksdetaljer/fetchSoknadsdetaljer";
+import AlertWithCloseButton from "@components/alert/AlertWithCloseButton";
 
-import { StatusPage } from "./StatusPage";
-import Oppgaver from "./oppgaver/Oppgaver";
 import OppgaveAlert from "./oppgaver/OppgaveAlert";
+import Oppgaver from "./oppgaver/Oppgaver";
+import { StatusPage } from "./StatusPage";
 
 interface Props {
     id: string;
 }
 
+const fetchSaker = async () => {
+    try {
+        return hentAlleSaker();
+    } catch (e: unknown) {
+        logger.error(`Fikk feil under henting av alle saker. Feil: ${e}`);
+        return [];
+    }
+};
+
 export const StatusUnderBehandlingPage = async ({ id }: Props) => {
     const t = await getTranslations("StatusUnderBehandlingPage");
 
     const queryClient = getQueryClient();
-    const saker = await hentSaksStatuser(id);
+    const saksData = await hentSaksStatuser(id);
+
+    const [innsendteSoknader] = await Promise.all([fetchSaker(), fetchPaabegynteSaker()]);
+    const soknadsdetaljer = await Promise.all(fetchSoknadsdetaljer(innsendteSoknader));
+    const saker = soknadsdetaljer.find((sak) => sak.fiksDigisosId === id);
 
     return (
         <StatusPage
@@ -31,7 +49,14 @@ export const StatusUnderBehandlingPage = async ({ id }: Props) => {
                 </HydrationBoundary>
             }
         >
-            {saker.map(async (sak, index) => {
+            {saker && saker.saker.length > 1 && (
+                <AlertWithCloseButton variant="info">
+                    <Heading size="small">{t("deltSøknad.tittel")}</Heading>
+                    {t("deltSøknad.beskrivelse")}
+                </AlertWithCloseButton>
+            )}
+
+            {saksData.map(async (sak, index) => {
                 const key = sak.utfallVedtak;
                 if (key && isVedtakUtfallKey(key)) {
                     const utfallKey = getTranslationKeyForUtfall(key);
