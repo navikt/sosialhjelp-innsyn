1c2841b0b292c81de4a843d431eb288969234bd7
joeldlin <jorgen.elden.lindgren@nav.no>
2025-08-20 11:38:47 +0200

    wip

--- src/utbetalinger/UtbetalingerListe.tsx
+++ src/utbetalinger/UtbetalingerListe.tsx
@@ -1,13 +1,31 @@
 "use client";
 
 import React from "react";
+import { BoxNew, Heading, VStack } from "@navikt/ds-react";
 
 import { useUtbetalingerChip } from "./UtbetalingerProviderContext";
+//import {
+//    useHentNyeUtbetalinger,
+//    useHentTidligereUtbetalinger,
+//} from "@generated/utbetalinger-controller/utbetalinger-controller";
 
 const UtbetalingerListe = () => {
     const { selectedChip } = useUtbetalingerChip();
 
-    return <div>{selectedChip}</div>;
+    //const { data: nye } = useHentNyeUtbetalinger();
+    //const { data: tidligere } = useHentTidligereUtbetalinger();
+
+    return (
+        <VStack gap="20">
+            <Heading size="small" level="2">
+                {selectedChip}
+            </Heading>
+            <VStack gap="1">
+                <BoxNew background="info-moderate">måned</BoxNew>
+                <BoxNew background="info-moderate"> hade</BoxNew>
+            </VStack>
+        </VStack>
+    );
 };
 
 export default UtbetalingerListe;
