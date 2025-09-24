fd04e5bb2cd530c90622e959b09fbaf5df79eb53
joeldlin <jorgen.elden.lindgren@nav.no>
2025-09-24 15:23:43 +0200

    fjerner overfløddig kode

--- src/app/[locale]/klage/opprett/[id]/[vedtakId]/_components/KlageVedtak.tsx
+++ src/app/[locale]/klage/opprett/[id]/[vedtakId]/_components/KlageVedtak.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { VStack } from "@navikt/ds-react";
+import { BodyShort, VStack } from "@navikt/ds-react";
+import { useTranslations } from "next-intl";
+import { FilePdfIcon } from "@navikt/aksel-icons";
 
 import { useHentSakForVedtak } from "@generated/sak-controller/sak-controller";
+import DigisosLinkCard from "@components/statusCard/DigisosLinkCard";
 
-import KlageVedtakHeader from "./KlageVedtakHeader";
-import KlageVedtakContainer from "./KlageVedtakContainer";
+import Sakstittel from "../../../../../soknad/[id]/_components/saker/Sakstittel";
 
 interface Props {
     fiksDigisosId: string;
@@ -14,13 +16,26 @@ interface Props {
 
 const KlageVedtak = ({ fiksDigisosId, vedtakId }: Props) => {
     const { data: sak } = useHentSakForVedtak(fiksDigisosId, vedtakId);
+    const t = useTranslations("Vedtak");
 
     if (!sak) return null;
 
     return (
         <VStack>
-            <KlageVedtakHeader tittel={sak.tittel} vedtakUtfall={sak.utfallVedtak} />
-            <KlageVedtakContainer vedtakUtfall={sak.utfallVedtak!} vedtaksliste={sak.vedtaksfilUrlList} />
+            <Sakstittel tittel={sak.tittel} vedtakUtfall={sak.utfallVedtak} />
+            <BodyShort>{t(`beskrivelse.${sak.utfallVedtak}`)}</BodyShort>
+            {sak.vedtaksfilUrlList &&
+                sak.vedtaksfilUrlList.map((fil, index) => (
+                    <DigisosLinkCard
+                        downloadIcon={true}
+                        key={index}
+                        href={fil.url}
+                        icon={<FilePdfIcon />}
+                        description={fil.dato}
+                    >
+                        {t("vedtaksBrev")}
+                    </DigisosLinkCard>
+                ))}
         </VStack>
     );
 };
