588401a64895969c0576ee90b736ab8cdc0aabb2
Martin Almvik <martin.almvik@nav.no>
2025-09-26 07:09:59 +0200

    Logg ut progress på upload

--- src/components/filopplasting/new/utils/tusUploader.ts
+++ src/components/filopplasting/new/utils/tusUploader.ts
@@ -17,14 +17,12 @@ export const getTusUploader = ({
     const uploadOptions = (file: File): UploadOptions => ({
         endpoint: browserEnv.NEXT_PUBLIC_TUSD_URL,
         retryDelays: [0, 1000, 3000, 5000],
-        chunkSize: 150 * 1024 * 1024, // 150MB
         metadata: {
             filename: file.name,
             externalId: id,
         },
         uploadSize: file.size,
         onError: (error: unknown) => logger.error(`Upload failed: ${error}`),
-        // uploadId kan deriveres fra URL, så vi bruker denne som en callback for en primærnøkkel
         onUploadUrlAvailable,
         onProgress,
         onSuccess,
