73528ee1977d4f7de122ae967d499a7e1cece395
Ragnhild Neset <ragcecnes@gmail.com>
2025-10-10 12:39:01 +0200

    Hent ut og vis lenke til innsendt klage på søknadssiden.

--- src/app/[locale]/soknad/[id]/_components/saker/Saker.tsx
+++ src/app/[locale]/soknad/[id]/_components/saker/Saker.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import React, { use } from "react";
+import { useParams } from "next/navigation";
 
 import { SaksStatusResponse } from "@generated/model";
 import { VilkarResponse } from "@generated/ssr/model";
+import { useHentKlager } from "@generated/klage-controller/klage-controller";
 
 import Sak, { SakSkeleton } from "./Sak";
 
@@ -13,12 +15,23 @@ interface Props {
 }
 
 const Saker = ({ sakerPromise, vilkarPromise }: Props) => {
+    const { id: fiksDigisosId } = useParams<{ id: string }>();
     const saker = use(sakerPromise);
     const vilkar = use(vilkarPromise);
+
+    const { data: klageData } = useHentKlager(fiksDigisosId);
+
     return (
         <>
             {saker.map((sak, index) => (
-                <Sak key={index} sak={sak} vilkar={vilkar.filter((it) => it.saksReferanse === sak.referanse)} />
+                <Sak
+                    key={index}
+                    sak={sak}
+                    vilkar={vilkar.filter((it) => it.saksReferanse === sak.referanse)}
+                    innsendtKlage={klageData?.find((klage) =>
+                        sak.vedtaksfilUrlList?.some((vedtaksfil) => vedtaksfil.id === klage.vedtakId)
+                    )}
+                />
             ))}
         </>
     );
