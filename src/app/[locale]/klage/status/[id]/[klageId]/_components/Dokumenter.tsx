d81dad12165241fc734ddfe72f46c3d98246fb85
Ragnhild Neset <ragcecnes@gmail.com>
2025-10-17 09:17:44 +0200

    Vise ut klage.pdf. (#1264)
    * Vise ut klage.pdf.
    
    * Remove list on dokuments.

---
+++ src/app/[locale]/klage/status/[id]/[klageId]/_components/Dokumenter.tsx
@@ -0,0 +1,35 @@
+import { Heading, VStack } from "@navikt/ds-react";
+import { FileUploadItem } from "@navikt/ds-react/FileUpload";
+import { getTranslations } from "next-intl/server";
+
+import { VedleggResponse } from "@generated/model";
+
+type Props = {
+    klagePdf: VedleggResponse;
+};
+
+const Dokumenter = async ({ klagePdf }: Props) => {
+    const t = await getTranslations("KlageDokumenter");
+
+    return (
+        <div>
+            <Heading size="medium" level="2" className="mb-4">
+                {t("tittel")}
+            </Heading>
+            <VStack gap="2">
+                {klagePdf && (
+                    <FileUploadItem
+                        href={klagePdf.url}
+                        file={{
+                            name: klagePdf.filnavn?.length ? klagePdf.filnavn : t("klagePdfDefaultTittel"),
+                            size: klagePdf.storrelse,
+                        }}
+                        className="w-full"
+                    />
+                )}
+            </VStack>
+        </div>
+    );
+};
+
+export default Dokumenter;
