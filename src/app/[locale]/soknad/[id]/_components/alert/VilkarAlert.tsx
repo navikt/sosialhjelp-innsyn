5c0bcf9f98408790b254ae152ec545841ed64e59
Martin Almvik <martin.almvik@nav.no>
2025-10-20 15:00:25 +0200

    DSOS-332: Ved én sak, ikke vis sakliste

---
+++ src/app/[locale]/soknad/[id]/_components/alert/VilkarAlert.tsx
@@ -0,0 +1,20 @@
+import React, { use } from "react";
+import { useTranslations } from "next-intl";
+
+import { VilkarResponse } from "@generated/ssr/model";
+import StatusAlert from "@components/alert/StatusAlert";
+
+interface Props {
+    vilkarPromise: Promise<VilkarResponse[]>;
+}
+
+const VilkarAlert = ({ vilkarPromise }: Props): React.JSX.Element | null => {
+    const t = useTranslations("VilkarAlert");
+    const vilkar = use(vilkarPromise);
+    if (vilkar?.length) {
+        return <StatusAlert variant="warning" tittel={t("tittel")} beskrivelse={t("beskrivelse")} />;
+    }
+    return null;
+};
+
+export default VilkarAlert;
---
+++ src/app/[locale]/soknad/[id]/_components/saker/sak/SingleSak.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useParams } from "next/navigation";
+import { Heading, VStack } from "@navikt/ds-react";
+import { useTranslations } from "next-intl";
+
+import { KlageRef, SaksStatusResponse } from "@generated/model";
+import { VilkarResponse } from "@generated/ssr/model";
+import { useGetDokumentasjonkravBetaSuspense } from "@generated/oppgave-controller/oppgave-controller";
+
+import Vedtak from "../vedtak/Vedtak";
+import VilkarListe from "../vilkar/VilkarListe";
+import Dokumentasjonkrav from "../dokumentasjonkrav/Dokumentasjonkrav";
+
+import StatusTag from "./StatusTag";
+
+interface Props {
+    sak: SaksStatusResponse;
+    vilkar: VilkarResponse[];
+    innsendtKlage?: KlageRef;
+}
+
+const SingleSak = ({ sak, vilkar, innsendtKlage }: Props): React.JSX.Element | null => {
+    const t = useTranslations("SingleSak");
+    const { id } = useParams<{ id: string }>();
+    const { data } = useGetDokumentasjonkravBetaSuspense(id);
+    const dokumentasjonkrav = data.filter((it) => it.saksreferanse === sak.referanse);
+    const vedtakUtfall = sak.utfallVedtak;
+    if (!sak.utfallVedtak && vilkar.length === 0 && dokumentasjonkrav.length === 0) {
+        return null;
+    }
+    return (
+        <VStack gap="16">
+            <VStack gap="4">
+                {sak.utfallVedtak && (
+                    <>
+                        <Heading size="medium" level="2">
+                            {t("tittel")}
+                        </Heading>
+                        <StatusTag vedtakUtfall={vedtakUtfall} className="self-start" />
+                        <Vedtak
+                            vedtakUtfall={sak.utfallVedtak}
+                            vedtaksliste={sak.vedtaksfilUrlList}
+                            innsendtKlage={innsendtKlage}
+                        />
+                    </>
+                )}
+            </VStack>
+            {vilkar.length > 0 && <VilkarListe vilkar={vilkar} />}
+            {dokumentasjonkrav.length > 0 && <Dokumentasjonkrav dokumentasjonkrav={dokumentasjonkrav} />}
+        </VStack>
+    );
+};
+
+export default SingleSak;
