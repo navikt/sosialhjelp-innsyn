ac1ef27b157f31dbfecd24a68cd345ffacf7f04c
Martin Almvik <martin.almvik@nav.no>
2025-10-28 14:11:34 +0100

    Bare la ny utbetalingside være på

--- src/featuretoggles/utils.ts
+++ src/featuretoggles/utils.ts
@@ -6,7 +6,7 @@ export function localDevelopmentToggles(): IToggle[] {
     return EXPECTED_TOGGLES.map(
         (it): IToggle => ({
             name: it,
-            enabled: true,
+            enabled: it === "sosialhjelp.innsyn.ny_utbetalinger_side",
             impressionData: false,
             variant: {
                 name: "disabled",
