1851555b195848d742371d586f7b184c2fb7440b
Ragnhild Neset <ragcecnes@gmail.com>
2025-11-19 15:40:22 +0100

    Oppdatert design for paabegynte soknader.

--- src/components/soknaderList/list/soknadCard/status/PaabegyntCard.tsx
+++ src/components/soknaderList/list/soknadCard/status/PaabegyntCard.tsx
@@ -1,7 +1,7 @@
-import { NotePencilDashIcon } from "@navikt/aksel-icons";
+import { NotePencilDashIcon, FloppydiskIcon } from "@navikt/aksel-icons";
 import { useLocale, useTranslations } from "next-intl";
 
-import DigisosLinkCard from "@components/statusCard/DigisosLinkCard";
+import DigisosLinkCard, { Tag } from "@components/statusCard/DigisosLinkCard";
 import { browserEnv } from "@config/env";
 
 interface Props {
@@ -12,13 +12,18 @@ interface Props {
 const PaabegyntCard = ({ soknadId, keptUntil }: Props) => {
     const t = useTranslations("StatusCard.PaabegyntCard");
     const locale = useLocale();
+
+    const tags: Tag[] = [
+        { title: t("paabegynt"), variant: "neutral-moderate", icon: <NotePencilDashIcon /> },
+        { title: t("keptUntil", { date: keptUntil }), variant: "neutral-moderate", icon: <FloppydiskIcon /> },
+    ];
+
     return (
         <DigisosLinkCard
             href={`${browserEnv.NEXT_PUBLIC_INNSYN_ORIGIN}/sosialhjelp/soknad/${locale}/skjema/${soknadId}/1`}
-            description={t("description", { date: keptUntil })}
-            icon={<NotePencilDashIcon />}
             variant="info"
             dashed
+            tags={tags}
         >
             {t("title")}
         </DigisosLinkCard>
