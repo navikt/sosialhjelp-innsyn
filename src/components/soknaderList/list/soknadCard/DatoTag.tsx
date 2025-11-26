34906ddb30b15bf25c6f56b232d2402c43d913e6
Martin <martinalmvik@gmail.com>
2025-11-26 09:26:34 +0100

    DSOS-426: Mottattdato på søknadsliste (#1318)
    * DSOS-426: Mottattdato på søknadsliste
    
    * Flytt dato-tag ut i egen komponent

---
+++ src/components/soknaderList/list/soknadCard/DatoTag.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+import { Tag } from "@navikt/ds-react";
+import { useTranslations } from "next-intl";
+
+interface Props {
+    mottattDato?: Date;
+    sendtDato?: Date;
+}
+
+const DatoTag = ({ mottattDato, sendtDato }: Props): ReactNode => {
+    const t = useTranslations("DatoTag");
+    if (mottattDato) {
+        return (
+            <Tag variant="neutral-moderate" size="small">
+                {t("mottatt", { dato: mottattDato })}
+            </Tag>
+        );
+    }
+    if (sendtDato) {
+        return (
+            <Tag variant="neutral-moderate" size="small">
+                {t("sendt", { dato: sendtDato })}
+            </Tag>
+        );
+    }
+    return null;
+};
+
+export default DatoTag;
--- src/components/soknaderList/list/soknadCard/status/StatusCard.tsx
+++ src/components/soknaderList/list/soknadCard/status/StatusCard.tsx
@@ -1,49 +1,25 @@
 "use client";
 
-import { PropsWithChildren, ReactNode } from "react";
-import { useLocale, useTranslations } from "next-intl";
-import { LinkCardFooter } from "@navikt/ds-react/LinkCard";
-import { Tag } from "@navikt/ds-react";
+import { PropsWithChildren } from "react";
+import { useLocale } from "next-intl";
 
 import DigisosLinkCard, { Props } from "@components/statusCard/DigisosLinkCard";
 import { useFlag } from "@featuretoggles/context";
 
-import BehandlingsStatusTag, { BehandlingsStatus, VedtakProgress } from "./BehandlingStatusTag";
-
 type StatusCardProps = Omit<PropsWithChildren<Props>, "href"> & {
     id: string;
     tittel: string;
-    sendtDato?: Date;
-    behandlingsStatus?: BehandlingsStatus;
-    vedtakProgress?: VedtakProgress;
-    alertTexts?: string[];
-    extraTags?: ReactNode[];
 };
 
-const StatusCard = (props: StatusCardProps) => {
-    const { tittel, sendtDato, behandlingsStatus, vedtakProgress, extraTags } = props;
+const StatusCard = (props: PropsWithChildren<StatusCardProps>) => {
+    const { tittel, children } = props;
     const nySoknadSideToggle = useFlag("sosialhjelp.innsyn.ny_soknadside");
     const locale = useLocale();
-    const t = useTranslations("StatusCard");
 
     const href = nySoknadSideToggle.enabled ? `/${locale}/soknad/${props.id}` : `/${locale}/${props.id}/status`;
 
     return (
-        <DigisosLinkCard
-            href={href}
-            footer={
-                <LinkCardFooter>
-                    {sendtDato && (
-                        <Tag variant="neutral-moderate" size="small">
-                            {t("sendt", { dato: sendtDato })}
-                        </Tag>
-                    )}
-                    <BehandlingsStatusTag status={behandlingsStatus} vedtakProgress={vedtakProgress} />
-                    {extraTags}
-                </LinkCardFooter>
-            }
-            {...props}
-        >
+        <DigisosLinkCard href={href} footer={children} {...props}>
             <span lang="nb">{tittel}</span>
         </DigisosLinkCard>
     );
