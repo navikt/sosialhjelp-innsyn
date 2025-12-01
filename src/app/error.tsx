675f40782418cbd56c17c338cf287119a4a0788f
Martin <martinalmvik@gmail.com>
2025-12-01 14:22:51 +0100

    DSOS-439: Bedre global-error (#1322)
    * DSOS-439: Bedre global-error
    
    - Legger ved dekoratøren client side på global-error.tsx
    - Flytter error.tsx til toppen
    - Drar ut selve jsxen for errorsida i en egen komponent
    
    * Kast feil fra root layout
    
    * Fiks interaktivitet i dekoratøren på global-error
    
    * Fiks logging og instrumentation på global-error
    
    * Legg til css på global-error
    
    * Hard refresh på global-error ved språkbytte
    
    * Fjern kasting av error igjen
    
    * Fiks rendering av global-error ved språkbytte
    
    * Fiks bruk av feil env-variabel

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { logger } from "@navikt/next-logger";
 
-import ServerError from "../pages/500";
+import ErrorPage from "@components/error/ErrorPage";
 
 export default function Error({ error }: { error: Error & { digest?: string } }) {
     logger.error(`Uncaught clientside error: ${error.name}, error.tsx shown. Error: ${error}.`);
 
-    useEffect(() => {
-        document.title = "Teknisk feil | Økonomisk sosialhjelp";
-    }, []);
-
-    return (
-        <div className="min-h-[50vh]">
-            <ServerError />
-        </div>
-    );
+    return <ErrorPage />;
 }
