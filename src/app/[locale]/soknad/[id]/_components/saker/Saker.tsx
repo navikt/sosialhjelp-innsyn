569e36c50e2a0e4ed48aa5f7ef24f75ccec8c45f
joeldlin <jorgen.elden.lindgren@nav.no>
2025-12-01 15:00:02 +0100

    Fjerner unødvendiger filer og flytter koden fra de opp en hak

--- src/app/[locale]/soknad/[id]/_components/saker/Saker.tsx
+++ src/app/[locale]/soknad/[id]/_components/saker/Saker.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React, { use } from "react";
+import { BoxNew, Heading, VStack } from "@navikt/ds-react";
+import { useTranslations } from "next-intl";
 
 import { KlageRef, SaksStatusResponse } from "@generated/model";
 
-import SakListe from "./sak/SakListe";
+import Sak from "./sak/Sak";
 
 interface Props {
     sakerPromise: Promise<SaksStatusResponse[]>;
@@ -14,12 +16,51 @@ interface Props {
 const Saker = ({ sakerPromise, klagerPromise }: Props) => {
     const saker = use(sakerPromise);
     const klager = use(klagerPromise);
+    const t = useTranslations("Saker");
 
     if (!saker.length) {
         return null;
     }
 
-    return <SakListe saker={saker} klager={klager} />;
+    if (saker.length === 1) {
+        const sak = saker[0];
+        return (
+            <VStack gap="2">
+                <Heading size="large" level="2">
+                    {t("vedtak")}
+                </Heading>
+                <BoxNew borderWidth="1" borderRadius="xlarge" borderColor="neutral-subtle" padding="8">
+                    <Sak
+                        sak={sak}
+                        innsendtKlage={klager.find((klage) =>
+                            sak.vedtaksfilUrlList?.some((vedtaksfil) => vedtaksfil.id === klage.vedtakId)
+                        )}
+                    />
+                </BoxNew>
+            </VStack>
+        );
+    }
+
+    return (
+        <VStack gap="2">
+            <Heading size="large" level="2">
+                {t("dineSaker")}
+            </Heading>
+            <VStack gap="16">
+                {saker.map((sak, index) => (
+                    <BoxNew borderWidth="1" borderRadius="xlarge" borderColor="neutral-subtle" padding="8" key={index}>
+                        <Sak
+                            key={index}
+                            sak={sak}
+                            innsendtKlage={klager.find((klage) =>
+                                sak.vedtaksfilUrlList?.some((vedtaksfil) => vedtaksfil.id === klage.vedtakId)
+                            )}
+                        />
+                    </BoxNew>
+                ))}
+            </VStack>
+        </VStack>
+    );
 };
 
 export default Saker;
