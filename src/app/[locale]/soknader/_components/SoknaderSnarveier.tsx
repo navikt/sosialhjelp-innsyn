1a49ba680bbd8f30cb14d6857a1b06273dac025e
Martin <martinalmvik@gmail.com>
2025-12-08 13:28:03 +0100

    DSOS-429: Legg til snarvei utbetalinger på søknader-siden (#1323)

---
+++ src/app/[locale]/soknader/_components/SoknaderSnarveier.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ReactNode } from "react";
+
+import { useHentUtbetalinger } from "@generated/utbetalinger-controller-2/utbetalinger-controller-2";
+
+import UtbetalingerSnarvei from "../../landingsside/_components/snarveier/UtbetalingerSnarvei";
+
+const SoknaderSnarveier = (): ReactNode => {
+    const { data: utbetalinger } = useHentUtbetalinger();
+    if (!utbetalinger || utbetalinger.length === 0) {
+        return null;
+    }
+    return <UtbetalingerSnarvei />;
+};
+
+export default SoknaderSnarveier;
