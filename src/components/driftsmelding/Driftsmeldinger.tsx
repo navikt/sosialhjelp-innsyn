dfb244f76bc19bc75b3b50e5aa72e56ee9767be6
Martin Almvik <martin.almvik@nav.no>
2025-12-18 15:03:48 +0100

    Spacing mellom driftsmeldinger og playwright-tester

--- src/components/driftsmelding/Driftsmeldinger.tsx
+++ src/components/driftsmelding/Driftsmeldinger.tsx
@@ -1,13 +1,20 @@
-import { Alert } from "@navikt/ds-react";
+import { Alert, VStack } from "@navikt/ds-react";
 import Markdown from "markdown-to-jsx/react";
 
 import { getDriftsmeldinger } from "@components/driftsmelding/getDriftsmeldinger";
 
 export const Driftsmeldinger = async () => {
     const driftsmeldinger = await getDriftsmeldinger();
-    return driftsmeldinger?.map(({ severity, text, id }) => (
-        <Alert variant={severity} fullWidth key={id}>
-            <Markdown>{text}</Markdown>
-        </Alert>
-    ));
+    if (!driftsmeldinger.length) {
+        return null;
+    }
+    return (
+        <VStack gap="4" className="mt-4">
+            {driftsmeldinger?.map(({ severity, text, id }) => (
+                <Alert variant={severity} fullWidth key={id} data-testid={`driftsmelding-${id}`}>
+                    <Markdown>{text}</Markdown>
+                </Alert>
+            ))}
+        </VStack>
+    );
 };
--- src/components/driftsmelding/DriftsmeldingerPages.tsx
+++ src/components/driftsmelding/DriftsmeldingerPages.tsx
@@ -1,4 +1,4 @@
-import { Alert } from "@navikt/ds-react";
+import { Alert, VStack } from "@navikt/ds-react";
 import Markdown from "markdown-to-jsx/react";
 
 import { Driftsmelding } from "@components/driftsmelding/getDriftsmeldinger";
@@ -8,11 +8,15 @@ interface Props {
 }
 
 const DriftsmeldingerPages = ({ driftsmeldinger }: Props) => {
-    return driftsmeldinger?.map(({ severity, text, id }) => (
-        <Alert variant={severity} fullWidth key={id} className="mt-8">
-            <Markdown>{text}</Markdown>
-        </Alert>
-    ));
+    return (
+        <VStack gap="4">
+            {driftsmeldinger?.map(({ severity, text, id }) => (
+                <Alert variant={severity} fullWidth key={id} data-testid={`driftsmelding-${id}`}>
+                    <Markdown>{text}</Markdown>
+                </Alert>
+            ))}
+        </VStack>
+    );
 };
 
 export default DriftsmeldingerPages;
