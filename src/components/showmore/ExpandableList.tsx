df3869473994b708b4377c0d49cf957ce04ef8d2
Martin <martinalmvik@gmail.com>
2026-01-08 15:25:07 +0100

    Lag ExpandableList med UU-forbedringer (#1335)
    * Lag ExpandableList med UU-forbedringer
    
    * Disable eslint ref rule
    
    * Fiks kommentarer
    
    * Rename ref på kommende utbetalinger også

---
+++ src/components/showmore/ExpandableList.tsx
@@ -0,0 +1,39 @@
+import React, { RefObject, useRef } from "react";
+import { VStack } from "@navikt/ds-react";
+
+import useShowMore, { ITEMS_LIMIT } from "@components/showmore/useShowMore";
+import ShowMoreButton from "@components/showmore/ShowMoreButton";
+
+interface Props<T> {
+    items: T[];
+    children: (item: T, index: number, firstExpandedItemRef: RefObject<HTMLLIElement | null>) => React.JSX.Element;
+    id: string;
+    showMoreSuffix: string;
+    itemsLimit?: number;
+}
+
+const ExpandableList = <T,>({ itemsLimit, children, items, id, showMoreSuffix }: Props<T>): React.JSX.Element => {
+    const showMore = useShowMore(items, itemsLimit);
+    const { hasMore, showAll } = showMore;
+    const firstExpandedItemRef = useRef<HTMLLIElement>(null);
+    const visibleItems = showAll ? items : items.slice(0, ITEMS_LIMIT);
+    return (
+        <>
+            <VStack as="ul" gap="2" id={id}>
+                {/* eslint-disable-next-line react-hooks/refs */}
+                {visibleItems.map((item, index) => children(item, index, firstExpandedItemRef))}
+            </VStack>
+            {hasMore && (
+                <ShowMoreButton
+                    items={items}
+                    ref={firstExpandedItemRef}
+                    id={id}
+                    suffix={showMoreSuffix}
+                    {...showMore}
+                />
+            )}
+        </>
+    );
+};
+
+export default ExpandableList;
