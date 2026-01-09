c5734eeadeda5af65e69acead133e7adab60b9c7
Ragnhild Neset <ragcecnes@gmail.com>
2026-01-09 12:29:20 +0100

    Replace "paabegynt" with "draft" and use calendar icon.

--- src/components/soknaderList/list/soknadCard/status/PaabegyntCard.tsx
+++ src/components/soknaderList/list/soknadCard/status/PaabegyntCard.tsx
@@ -1,4 +1,4 @@
-import { NotePencilDashIcon, FloppydiskIcon } from "@navikt/aksel-icons";
+import { NotePencilDashIcon, CalendarIcon } from "@navikt/aksel-icons";
 import { useLocale, useTranslations } from "next-intl";
 import { LinkCardFooter } from "@navikt/ds-react/LinkCard";
 import { Tag } from "@navikt/ds-react";
@@ -19,17 +19,27 @@ const PaabegyntCard = ({ soknadId, keptUntil }: Props) => {
             href={`${browserEnv.NEXT_PUBLIC_INNSYN_ORIGIN}/sosialhjelp/soknad/${locale}/skjema/${soknadId}/1`}
             footer={
                 <LinkCardFooter>
-                    <Tag key="paabegynt" variant="neutral-moderate" icon={<NotePencilDashIcon />} size="small">
-                        {t("paabegynt")}
+                    <Tag
+                        key="paabegynt"
+                        variant="neutral-moderate"
+                        icon={<NotePencilDashIcon aria-hidden="true" />}
+                        size="small"
+                    >
+                        {t("utkast")}
                     </Tag>
 
-                    <Tag key="keptUntil" variant="neutral-moderate" icon={<FloppydiskIcon />} size="small">
+                    <Tag
+                        key="keptUntil"
+                        variant="neutral-moderate"
+                        icon={<CalendarIcon aria-hidden="true" />}
+                        size="small"
+                    >
                         {t("keptUntil", { date: keptUntil })}
                     </Tag>
                 </LinkCardFooter>
             }
         >
-            <span className="sr-only">{t("paabegynt") + " "}</span>
+            <span className="sr-only">{t("utkast") + " "}</span>
             {t("title")}
         </DigisosLinkCard>
     );
