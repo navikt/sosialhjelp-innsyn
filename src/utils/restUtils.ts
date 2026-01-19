a2d1f8327404fd945531bc0a350b1624b5905362
Ragnhild Neset <ragcecnes@gmail.com>
2026-01-19 14:37:38 +0100

    Add back stuff from knip that should be there.

--- src/utils/restUtils.ts
+++ src/utils/restUtils.ts
@@ -1,3 +1,7 @@
+export function isProd() {
+    return process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "prod";
+}
+
 export function isLocalhost() {
     return process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "local";
 }
