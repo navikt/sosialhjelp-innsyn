cf21c59a3077fd8593ea5f2856be6adfaa73c09b
joeldlin <59644512+joeldlin@users.noreply.github.com>
2026-02-09 11:57:20 +0100

    Revert "Feature/dsos 503 navigasjonsknapper oppdateringer (#1361)" (#1366)
    This reverts commit 1f975625373dadeff62bad7d6399abc79526e1fe.

--- src/app/[locale]/soknader/_components/SoknaderSnarveier.tsx
+++ src/app/[locale]/soknader/_components/SoknaderSnarveier.tsx
@@ -4,19 +4,13 @@ import { ReactNode } from "react";
 import { useHentUtbetalinger } from "@generated/utbetalinger-controller-2/utbetalinger-controller-2";
 
 import UtbetalingerSnarvei from "@components/snarveier/UtbetalingerSnarvei";
-import LandingssideSnarvei from "@components/snarveier/LandingssideSnarvei";
 
 const SoknaderSnarveier = (): ReactNode => {
     const { data: utbetalinger } = useHentUtbetalinger();
     if (!utbetalinger || utbetalinger.length === 0) {
         return null;
     }
-    return (
-        <>
-            <UtbetalingerSnarvei />
-            <LandingssideSnarvei />
-        </>
-    );
+    return <UtbetalingerSnarvei />;
 };
 
 export default SoknaderSnarveier;
