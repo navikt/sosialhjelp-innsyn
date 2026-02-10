61e3084e9608385d11b69885388bbd1e059d9b47
Martin Almvik <martin.almvik@nav.no>
2026-02-10 08:57:24 +0100

    Flytter klager inn i RQ
    Unngår prop drilling

--- src/app/[locale]/soknad/[id]/_components/saker/sak/sakpanel/SakPanel.tsx
+++ src/app/[locale]/soknad/[id]/_components/saker/sak/sakpanel/SakPanel.tsx
@@ -2,21 +2,26 @@ import React from "react";
 import Vedtak from "./Vedtak";
 import { BoxNew, VStack } from "@navikt/ds-react";
 import KlageInfo from "./KlageInfo";
-import { KlageRef, SaksStatusResponse } from "@generated/model";
+import { SaksStatusResponse } from "@generated/model";
 import Sakstittel from "../Sakstittel";
+import { useHentKlagerSuspense } from "@generated/klage-controller/klage-controller";
+import { useParams } from "next/navigation";
 
 interface Props {
     sak: SaksStatusResponse;
-    innsendtKlage?: KlageRef;
 }
 
-const SakPanel = ({ sak, innsendtKlage }: Props): React.JSX.Element => {
+const SakPanel = ({ sak }: Props): React.JSX.Element => {
+    const { id: fiksDigisosId } = useParams<{ id: string }>();
+    const { data: klager } = useHentKlagerSuspense(fiksDigisosId);
     const sortedVedtak = sak.vedtak.toSorted((a, b) => {
         const dateA = a.dato ? new Date(a.dato).getTime() : 0;
         const dateB = b.dato ? new Date(b.dato).getTime() : 0;
         return dateB - dateA;
     });
     const latestVedtak = sortedVedtak[0];
+    const allVedtakIds = sak.vedtak.map((vedtak) => vedtak.id);
+    const innsendtKlage = klager.find((klage) => allVedtakIds.includes(klage.vedtakId));
     return (
         <BoxNew borderWidth="1" borderColor="neutral-subtle" borderRadius="xlarge" overflow="hidden">
             <VStack gap="space-16" padding={{ xs: "space-16", md: "space-24" }}>
@@ -30,7 +35,7 @@ const SakPanel = ({ sak, innsendtKlage }: Props): React.JSX.Element => {
                     borderColor="neutral-subtle"
                     background="neutral-soft"
                 >
-                    <KlageInfo latestVedtak={latestVedtak} innsendtKlage={innsendtKlage} />
+                    <KlageInfo innsendtKlage={innsendtKlage} vedtakId={latestVedtak.id} />
                 </BoxNew>
             )}
         </BoxNew>
