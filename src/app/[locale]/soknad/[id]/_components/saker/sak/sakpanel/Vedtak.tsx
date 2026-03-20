22cab34aa9337fd7c5fc38f7ca7aa70b231a7ea0
Martin Almvik <martin.almvik@nav.no>
2026-03-20 14:49:51 +0100

    Autoscroll til første vedtakslenke ved #vedtak

--- src/app/[locale]/soknad/[id]/_components/saker/sak/sakpanel/Vedtak.tsx
+++ src/app/[locale]/soknad/[id]/_components/saker/sak/sakpanel/Vedtak.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import { BankNoteIcon, EnvelopeClosedIcon } from "@navikt/aksel-icons";
 import { BodyShort, VStack } from "@navikt/ds-react";
 import { useTranslations } from "next-intl";
 import DigisosLinkCard from "@components/statusCard/DigisosLinkCard";
 import { VedtakDto } from "@generated/model";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useSearchParams } from "next/navigation";
 
 interface Props {
     sortedVedtak: VedtakDto[];
@@ -13,6 +16,23 @@ interface Props {
 
 const Vedtak = ({ sortedVedtak, latestVedtak }: Props) => {
     const t = useTranslations("Vedtak");
+    const latestItemRef = useRef<HTMLAnchorElement>(null);
+    // Denne trigger rerendring på hash change (#vedtak), selv om den ikke brukes
+    const params = useSearchParams();
+
+    useEffect(() => {
+        const link = latestItemRef.current;
+        if (window.location.hash === "#vedtak" && link) {
+            if (link) {
+                requestAnimationFrame(() => {
+                    link.scrollIntoView({ behavior: "smooth", block: "center" });
+                    // focusVisible tvinger :focus-visible pseudoclass på elementet. Funker ikke i alle browsere
+                    link.focus({ preventScroll: true, focusVisible: true });
+                });
+            }
+        }
+    }, [params]);
+
     const isAnyInnvilget = sortedVedtak.some(
         (vedtak) => vedtak.utfall && ["INNVILGET", "DELVIS_INNVILGET"].includes(vedtak.utfall)
     );
@@ -23,11 +43,13 @@ const Vedtak = ({ sortedVedtak, latestVedtak }: Props) => {
                 <VStack gap="space-8" as="ol" aria-label={t("vedtaksbrev")}>
                     {sortedVedtak.map((vedtak, index) => {
                         const isNewest = index === 0 && sortedVedtak.length > 1;
+                        const isLatest = index === 0;
                         return (
                             <li key={vedtak.id}>
                                 <DigisosLinkCard
                                     cardIcon="external-link"
                                     openInNewTab
+                                    ref={isLatest ? latestItemRef : undefined}
                                     href={vedtak.vedtaksFilUrl ?? ""}
                                     icon={<EnvelopeClosedIcon aria-hidden />}
                                     description={
---
+++ src/global.d.ts
@@ -0,0 +1,3 @@
+interface FocusOptions {
+    focusVisible?: boolean;
+}
