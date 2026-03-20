0b62f977ef9df14d59a54e5eb1847e963206609b
joeldlin <jorgen.elden.lindgren@nav.no>
2026-03-20 11:28:08 +0100

    End DokKravReadMore til å være mer lik OppgaverReadMore

--- src/app/[locale]/soknad/[id]/_components/vilkar/readmore/DokKravReadMore.tsx
+++ src/app/[locale]/soknad/[id]/_components/vilkar/readmore/DokKravReadMore.tsx
@@ -1,4 +1,4 @@
-import { BodyLong, Heading, Link, List, ReadMore } from "@navikt/ds-react";
+import { BodyLong, Heading, Link, List, ReadMore, VStack } from "@navikt/ds-react";
 import { ListItem } from "@navikt/ds-react/List";
 import { useLocale, useTranslations } from "next-intl";
 
@@ -8,40 +8,58 @@ const DokKravReadMore = () => {
     const localeSuffix = locale === "nb" ? "" : `/${locale}`;
     return (
         <ReadMore header={t("header")}>
-            <Heading level="3" size="xsmall">
-                {t("papirHeading")}
-            </Heading>
-            <BodyLong spacing>{t("papirIntro")}</BodyLong>
-            <List as="ul" size="small">
-                <ListItem>{t("papirStep1")}</ListItem>
-                <ListItem>{t("papirStep2")}</ListItem>
-                <ListItem>{t("papirStep3")}</ListItem>
-                <ListItem>{t("papirStep4")}</ListItem>
-            </List>
-            <BodyLong spacing>{t("papirSkanner")}</BodyLong>
-            <Heading level="3" size="xsmall">
-                {t("nettsiderHeading")}
-            </Heading>
-            <BodyLong spacing>{t("nettsiderBody1")}</BodyLong>
-            <BodyLong spacing>{t("nettsiderBody2")}</BodyLong>
-            <Heading level="3" size="xsmall">
-                {t("problemerHeading")}
-            </Heading>
-            <BodyLong spacing>{t("problemerBody1")}</BodyLong>
-            <BodyLong>
-                {t.rich("problemerBody2", {
-                    ring: (chunks) => (
-                        <Link href="tel:+4755553333" inlineText>
-                            {chunks}
-                        </Link>
-                    ),
-                    kontor: (chunks) => (
-                        <Link href={`https://www.nav.no/sok-nav-kontor${localeSuffix}`} inlineText>
-                            {chunks}
-                        </Link>
-                    ),
-                })}
-            </BodyLong>
+            <VStack gap="space-32">
+                <VStack>
+                    <Heading level="3" size="xsmall">
+                        {t("papirHeading")}
+                    </Heading>
+                    <VStack gap="space-8">
+                        <BodyLong>{t("papirIntro")}</BodyLong>
+                        <List as="ul" size="small">
+                            <ListItem>
+                                <BodyLong>{t("papirStep1")}</BodyLong>
+                            </ListItem>
+                            <ListItem>
+                                <BodyLong>{t("papirStep2")}</BodyLong>
+                            </ListItem>
+                            <ListItem>
+                                <BodyLong>{t("papirStep3")}</BodyLong>
+                            </ListItem>
+                            <ListItem>
+                                <BodyLong>{t("papirStep4")}</BodyLong>
+                            </ListItem>
+                        </List>
+                        <BodyLong>{t("papirSkanner")}</BodyLong>
+                    </VStack>
+                </VStack>
+                <VStack>
+                    <Heading level="3" size="xsmall">
+                        {t("nettsiderHeading")}
+                    </Heading>
+                    <BodyLong spacing>{t("nettsiderBody1")}</BodyLong>
+                    <BodyLong>{t("nettsiderBody2")}</BodyLong>
+                </VStack>
+                <VStack>
+                    <Heading level="3" size="xsmall">
+                        {t("problemerHeading")}
+                    </Heading>
+                    <BodyLong spacing>{t("problemerBody1")}</BodyLong>
+                    <BodyLong>
+                        {t.rich("problemerBody2", {
+                            ring: (chunks) => (
+                                <Link href="tel:+4755553333" inlineText>
+                                    {chunks}
+                                </Link>
+                            ),
+                            kontor: (chunks) => (
+                                <Link href={`https://www.nav.no/sok-nav-kontor${localeSuffix}`} inlineText>
+                                    {chunks}
+                                </Link>
+                            ),
+                        })}
+                    </BodyLong>
+                </VStack>
+            </VStack>
         </ReadMore>
     );
 };
