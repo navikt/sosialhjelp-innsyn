eef02dc713aa88c5fd61b5661438d86b9e7edb9b
Martin Almvik <martin.almvik@nav.no>
2026-03-25 12:49:59 +0100

    slight oppgaver refactor

---
+++ src/app/[locale]/soknad/[id]/_components/oppgaver/InnsynsoppgaverSection.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Heading, HStack, Loader, Tag, VStack } from "@navikt/ds-react";
+import { NavigationGuardProvider } from "next-navigation-guard";
+import { useTranslations } from "next-intl";
+import { TasklistIcon } from "@navikt/aksel-icons";
+import ExpandableList from "@components/showmore/ExpandableList";
+import { OppgaveResponseBeta } from "@generated/model";
+import TipsReadMore from "../TipsReadMore";
+import OppgaveItem from "../oppgaver/OppgaveItem";
+
+interface Props {
+    oppgaver: OppgaveResponseBeta[];
+    isFetching: boolean;
+}
+
+const InnsynsoppgaverSection = ({ oppgaver, isFetching }: Props) => {
+    const t = useTranslations("InnsynsoppgaverSection");
+
+    const fullforteOppgaver = oppgaver.filter((oppgave) => oppgave.erLastetOpp);
+    const hasUncompletedOppgaver = oppgaver.length - fullforteOppgaver.length > 0;
+
+    return (
+        <VStack gap="space-8" as="section" aria-labelledby="oppgaver-tittel">
+            <HStack align="center" gap="space-8">
+                <Heading size="medium" level="2" id="oppgaver-tittel">
+                    {t("tittel")}
+                </Heading>
+                <Tag variant={hasUncompletedOppgaver ? "warning" : "success"} icon={<TasklistIcon aria-hidden />}>
+                    {hasUncompletedOppgaver
+                        ? t("xAvYFullfort", { fullfort: fullforteOppgaver.length, total: oppgaver.length })
+                        : t("alleFullfort")}
+                </Tag>
+                {isFetching && <Loader />}
+            </HStack>
+            {hasUncompletedOppgaver && <TipsReadMore />}
+            <NavigationGuardProvider>
+                <ExpandableList
+                    items={oppgaver}
+                    id="oppgaver"
+                    showMoreSuffix={t("suffix")}
+                    labelledById="oppgaver-tittel"
+                    itemsLimit={hasUncompletedOppgaver ? 3 : 1}
+                    gap={{ xs: "space-12", md: "space-16" }}
+                >
+                    {(oppgave, ref) => (
+                        <OppgaveItem
+                            ref={ref}
+                            key={`${oppgave.oppgaveId}-${oppgave.dokumenttype}-${oppgave.tilleggsinformasjon}`}
+                            oppgave={oppgave}
+                        />
+                    )}
+                </ExpandableList>
+            </NavigationGuardProvider>
+        </VStack>
+    );
+};
+
+export default InnsynsoppgaverSection;
---
+++ src/app/[locale]/soknad/[id]/_components/oppgaver/OppgaveItem.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { forwardRef, Ref } from "react";
+import Opplastingsboks from "@components/filopplasting/new/Opplastingsboks";
+import OpplastingsboksTus from "@components/filopplasting/new/OpplastingsboksTus";
+import { getVisningstekster } from "@utils/getVisningsteksterForVedlegg";
+import { useFlag } from "@featuretoggles/context";
+import { Metadata } from "@components/filopplasting/new/types";
+import { OppgaveResponseBeta } from "@generated/model";
+import TaskListItem from "../tasklistitem/TaskListItem";
+import OppgaveTag from "../tasklistitem/OppgaveTag";
+
+interface Props {
+    oppgave: OppgaveResponseBeta;
+}
+
+const withWarningColor = (text: string | undefined) => <span className="text-ax-text-warning">{text}</span>;
+
+const OppgaveItem = ({ oppgave }: Props, ref: Ref<HTMLLIElement>) => {
+    const toggle = useFlag("sosialhjelp.innsyn.ny_upload");
+    const newUploadEnabled = toggle?.enabled ?? false;
+
+    const { typeTekst, tilleggsinfoTekst } = getVisningstekster(oppgave.dokumenttype, oppgave.tilleggsinformasjon);
+    const metadata: Metadata = {
+        dokumentKontekst: "dokumentasjonetterspurt",
+        innsendelsesfrist: oppgave.innsendelsesfrist,
+        hendelsereferanse: oppgave.hendelsereferanse,
+        type: oppgave.dokumenttype,
+        tilleggsinfo: oppgave.tilleggsinformasjon,
+        hendelsetype: oppgave.hendelsetype,
+    };
+
+    return (
+        <TaskListItem ref={ref} variant={oppgave.erLastetOpp || !oppgave.erFraInnsyn ? "normal" : "warning"}>
+            {newUploadEnabled ? (
+                <OpplastingsboksTus
+                    id={oppgave.oppgaveId}
+                    completed={oppgave.erLastetOpp}
+                    label={typeTekst}
+                    description={tilleggsinfoTekst}
+                    tag={<OppgaveTag frist={oppgave.innsendelsesfrist} completed={oppgave.erLastetOpp} />}
+                    metadata={metadata}
+                />
+            ) : (
+                <Opplastingsboks
+                    metadata={metadata}
+                    completed={oppgave.erLastetOpp}
+                    label={!oppgave.erLastetOpp && oppgave.erFraInnsyn ? withWarningColor(typeTekst) : typeTekst}
+                    labelText={typeTekst}
+                    description={
+                        !oppgave.erLastetOpp && oppgave.erFraInnsyn
+                            ? withWarningColor(tilleggsinfoTekst)
+                            : tilleggsinfoTekst
+                    }
+                    tag={<OppgaveTag frist={oppgave.innsendelsesfrist} completed={oppgave.erLastetOpp} />}
+                />
+            )}
+        </TaskListItem>
+    );
+};
+
+export default forwardRef(OppgaveItem);
