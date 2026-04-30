7cdcc1cdcd9fa7475886c902901d696cbcacfdc8
joeldlin <jorgen.elden.lindgren@nav.no>
2026-04-30 12:07:47 +0200

    refaktorerer hvordan "tag" vises for dok.krav slik at det er lik hvordan "tag" blir brukt i dok.etter.

--- src/app/[locale]/soknad/[id]/_components/vilkar/dokumentasjonkrav/Dokumentasjonkrav.tsx
+++ src/app/[locale]/soknad/[id]/_components/vilkar/dokumentasjonkrav/Dokumentasjonkrav.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { Tag } from "@navikt/ds-react";
 import OpplastingsboksTus from "@components/filopplasting/OpplastingsboksTus";
 import TaskListItem from "../../tasklistitem/TaskListItem";
 import Opplastingsboks from "@components/filopplasting/Opplastingsboks";
-import { useTranslations } from "next-intl";
 import { DokumentasjonkravDto } from "@generated/model";
 import { useFlag } from "@featuretoggles/context";
 import OppgaveTag from "../../tasklistitem/OppgaveTag";
@@ -17,7 +15,6 @@ const withWarningColor = (text: string | undefined, isUncompleted: boolean) =>
     isUncompleted && text ? <span className="text-ax-text-warning">{text}</span> : text;
 
 const Dokumentasjonkrav = ({ dokKrav }: Props) => {
-    const t = useTranslations("Dokumentasjonkrav");
     const toggle = useFlag("sosialhjelp.innsyn.ny_upload");
     const newUploadEnabled = toggle?.enabled ?? false;
     return (
@@ -37,13 +34,7 @@ const Dokumentasjonkrav = ({ dokKrav }: Props) => {
                     description={dokKrav.beskrivelse}
                     completed={dokKrav.erLastetOpp}
                     variant={dokKrav.erLastetOpp ? undefined : "warning"}
-                    tag={
-                        dokKrav.opplastetDato ? (
-                            <Tag variant="success-moderate">{t("løst")}</Tag>
-                        ) : dokKrav.frist ? (
-                            <Tag variant="warning-moderate">{t("frist", { frist: new Date(dokKrav.frist) })}</Tag>
-                        ) : undefined
-                    }
+                    tag={<OppgaveTag frist={dokKrav.frist} completed={!!dokKrav.opplastetDato} />}
                 />
             ) : (
                 <Opplastingsboks
